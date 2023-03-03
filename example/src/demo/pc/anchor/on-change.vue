20dc7fdc30a83059ed18aebba79b6911de35b799
chenxi-20 <2465950588@qq.com>
2023-03-03 14:05:42 +0800

    feat(anchor): 添加锚点anchor组件onchange事件

---
+++ example/src/demo/pc/anchor/on-change.vue
@@ -0,0 +1,67 @@
+<template>
+  <tiny-row>
+    <tiny-col :span="10">
+      <div id="container" style="height: 30vh; border: 2px solid #333; overflow: auto">
+        <div id="basic-usage" style="height: 100vh; background: rgba(125, 0, 0, 0.1)">Basic Usage</div>
+        <div id="set-container" style="height: 100vh; background: rgba(0, 125, 0, 0.1)">Set Container</div>
+        <div id="event" style="background: rgba(0, 0, 125, 0.1)">
+          Event
+          <div id="on-change" style="height: 100vh; background: rgba(0, 0, 125, 0.2)">On Change</div>
+          <div id="link-click" style="height: 100vh; background: rgba(0, 0, 125, 0.3)">Link Click</div>
+        </div>
+      </div>
+    </tiny-col>
+    <tiny-col :span="2">
+      <tiny-anchor :links="links" @on-change="handleChange"></tiny-anchor>
+    </tiny-col>
+  </tiny-row>
+</template>
+
+<script>
+import { Anchor, Row, Col } from '@opentiny/vue'
+export default {
+  components: {
+    TinyAnchor: Anchor,
+    TinyRow: Row,
+    TinyCol: Col
+  },
+  data() {
+    return {
+      links: [
+        {
+          key: 'basic-usage',
+          link: '#basic-usage',
+          title: 'Basic Usage'
+        },
+        {
+          key: 'set-container',
+          link: '#set-container',
+          title: 'Set Container'
+        },
+        {
+          key: 'event',
+          link: '#event',
+          title: 'Event',
+          children: [
+            {
+              key: 'on-change',
+              link: '#on-change',
+              title: 'On Change'
+            },
+            {
+              key: 'link-click',
+              link: '#link-click',
+              title: 'Link Click'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    handleChange(link) {
+      console.log('change', link)
+    }
+  }
+}
+</script>
