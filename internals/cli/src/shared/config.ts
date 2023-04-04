bce50db720414ff4299a9e8241f8bfb9bf5c43ed
zzcr <894103554@qq.com>
2023-04-03 23:35:20 -0700

    fix(type): 修复@better-scroll没有排除导致报错问题

--- internals/cli/src/shared/config.ts
+++ internals/cli/src/shared/config.ts
@@ -11,7 +11,7 @@ const EXTENERAL = [
   'shepherd.js'
 ]
 const external = (deps) => {
-  return EXTENERAL.includes(deps) || /^@opentiny[\\/]|@originjs|echarts|cropperjs|crypto-js/.test(deps)
+  return EXTENERAL.includes(deps) || /^@opentiny[\\/]|@originjs|echarts|cropperjs|@better-scroll|crypto-js/.test(deps)
 }
 
 export { external }
