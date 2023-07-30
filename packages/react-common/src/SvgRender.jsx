47e4c5967a644c4dad5ed6e7c15ce93567a6a8cb
gxd3 <guoxiaodong167@gmail.com>
2023-07-30 19:22:03 +0800

    feat: react版本 button api 功能实现，多余注释删除

--- packages/react-common/src/SvgRender.jsx
+++ packages/react-common/src/SvgRender.jsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames'
 
 export default function SvgRender(Component, props) {
-  // const className = 'icon tiny-svg' + ' ' + props.className
   const className = classNames(
     'icon',
     'tiny-svg',
