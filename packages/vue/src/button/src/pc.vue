608592cbcaeb8087c4a433736d4f72be62744637
常浩-BJS21325 <changhao01@youdao>
2023-07-30 10:43:12 +0800

    package(rich-text-editor): add new components, rich-text-editor

--- packages/vue/src/button/src/pc.vue
+++ packages/vue/src/button/src/pc.vue
@@ -10,24 +10,28 @@
  *
  -->
 <template>
-  <button
-    class="tiny-button" 
-    @click="handleClick" 
-    :disabled="state.buttonDisabled || loading" 
-    :autofocus="autofocus"
-    :type="nativeType" :class="[
-      type ? 'tiny-button--' + type : '',
-      size ? 'tiny-button--' + size : '',
-      {
-        'is-disabled': state.buttonDisabled,
-        'is-loading': loading,
-        'is-plain': state.plain,
-        'is-round': round,
-        'is-circle': circle,
-        'is-icon': icon && !loading && (text || $slots.default),
-        'is-only-icon': icon && !loading && !(text || $slots.default)
-      }
-    ]" :tabindex="tabindex" v-bind="a($attrs, ['class', 'style'], true)">
+        <button 
+        class="tiny-button" 
+        @click="handleClick" 
+        :disabled="state.buttonDisabled || loading" 
+        :autofocus="autofocus"
+          :type="nativeType" 
+          :class="[
+            type ? 'tiny-button--' + type : '',
+            size ? 'tiny-button--' + size : '',
+            {
+              'is-disabled': state.buttonDisabled,
+              'is-loading': loading,
+              'is-plain': state.plain,
+              'is-round': round,
+              'is-circle': circle,
+              'is-icon': icon && !loading && (text || $slots.default),
+              'is-only-icon': icon && !loading && !(text || $slots.default)
+            }
+          ]" 
+        :tabindex="tabindex" 
+        v-bind="a($attrs, ['class', 'style'], true)"
+      >
     <icon-loading v-if="loading" class="tiny-icon-loading tiny-svg-size" />
     <component v-if="icon && !loading" :is="icon" :class="{ 'is-text': text || $slots.default }" />
     <slot>
