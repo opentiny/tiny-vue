9ef08b3abce5b811e41d39d7d61e42f17849eea1
GaoNeng-wWw <gaonenggg@outlook.com>
2023-08-06 14:32:04 +0800

    🐞 fix(color-picker): reactive bug
    fix some bug found during testing

--- examples/docs/resources/pc/demo/color-picker/base.vue
+++ examples/docs/resources/pc/demo/color-picker/base.vue
@@ -1,19 +1,27 @@
 <template>
-  <tiny-color-picker v-model="color" />
+  <tiny-color-picker v-model="color" :visible="true" />
+  <button @click="change">
+    Change
+  </button>
   <!-- <tiny-color-picker v-model="color2" /> -->
 </template>
 
 <script>
 import { ColorPicker } from '@opentiny/vue'
+import { ref } from 'vue'
 
 export default {
   components: {
     TinyColorPicker: ColorPicker
   },
   setup() {
+    const color = ref('#66ccff')
+    const change = () => {
+      color.value = color.value === '#66ccff' ? '#000' : '#66ccff'
+    }
     return {
-      color: '#66ccff',
-      color2: '#fff'
+      color,
+      change
     }
   }
 }
