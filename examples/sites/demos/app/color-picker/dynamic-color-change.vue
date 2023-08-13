f2bd339297825a5c6eeb67c7b5038a7521d3c913
GaoNeng-wWw <gaonenggg@outlook.com>
2023-08-13 14:18:14 +0800

    feat(color-picker): color-picker component

---
+++ examples/sites/demos/app/color-picker/dynamic-color-change.vue
@@ -0,0 +1,28 @@
+<template>
+  <tiny-color-picker v-model="color" />
+  <tiny-button @click="changeColor">
+    切换
+  </tiny-button>
+</template>
+
+<script>
+import { ColorPicker, Button } from '@opentiny/vue'
+import { ref } from 'vue'
+
+export default {
+  components: {
+    TinyColorPicker: ColorPicker,
+    TinyButton: Button
+  },
+  setup() {
+    const color = ref('#66ccff')
+    const changeColor = () => {
+      color.value = color.value === '#66ccff' ? '#000' : '#66ccff'
+    }
+    return {
+      color,
+      changeColor
+    }
+  }
+}
+</script>
