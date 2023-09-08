584b4b31ccc22436371934935fcee2fdea02a691
yuanningning <18943931897@163.com>
2023-09-08 02:15:17 -0700

    fix: count smb theme change fix

--- examples/sites/demos/app/link/custom-icon.vue
+++ examples/sites/demos/app/link/custom-icon.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <tiny-link>
+      <template #icon>
+        <icon-fillet-external-link class="tiny-svg-link"></icon-fillet-external-link>
+      </template>
+      链接按钮
+    </tiny-link>
+    <tiny-link disabled>
+      <template #icon>
+        <icon-fillet-external-link class="tiny-svg-size"></icon-fillet-external-link>
+      </template>
+      链接按钮
+    </tiny-link>
     <tiny-link :underline="false" :icon="IconEdit"> 编辑 </tiny-link>
     <tiny-link :underline="false" :icon="IconEdit" disabled> 禁用 </tiny-link>
     <tiny-link :underline="false"> 查看<icon-view class="tiny-svg-size"></icon-view> </tiny-link>
@@ -12,26 +24,32 @@
   </div>
 </template>
 
-<script lang="jsx">
-import { Link } from '@opentiny/vue'
-import { IconEdit, IconView, IconDel } from '@opentiny/vue-icon'
+<script>
+  import { Link } from '@opentiny/vue'
+  import { IconEdit, IconView, IconDel, IconFilletExternalLink } from '@opentiny/vue-icon'
 
-export default {
-  components: {
-    TinyLink: Link,
-    IconView: IconView(),
-    IconDel: IconDel()
-  },
-  data() {
-    return {
-      IconEdit: IconEdit()
+  export default {
+    components: {
+      TinyLink: Link,
+      IconView: IconView(),
+      IconDel: IconDel(),
+      IconFilletExternalLink: IconFilletExternalLink()
+    },
+    data() {
+      return {
+        IconEdit: IconEdit()
+      }
     }
   }
-}
 </script>
 
 <style>
-.tiny-link {
-  margin-right: 8px;
-}
+  .tiny-link {
+    margin-right: 8px;
+  }
+
+  .tiny-svg-link,
+  .tiny-svg-link g {
+    fill: var(--ti-link-default-text-color);
+  }
 </style>
