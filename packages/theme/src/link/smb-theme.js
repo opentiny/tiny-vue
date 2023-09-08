4164fddf6f7a83350bd7adcde29bed60e50bab05
Gimmy <975402925@qq.com>
2023-09-08 02:57:26 -0700

    fix(link): smb theme change fix

--- packages/theme/src/link/smb-theme.js
+++ packages/theme/src/link/smb-theme.js
@@ -1,15 +1,20 @@
 export const tinyLinkSmbTheme = {
-  'ti-link-display': 'none',
   'ti-link-svg-width': '16px',
   'ti-link-svg-height': '16px',
   'ti-link-svg-margin-right': '4px',
   'ti-link-font-weight': 'var(--ti-common-font-weight-4)',
-  'ti-link-default-border-display': 'none',
   'ti-link-default-hover-border-color': '#595959',
   'ti-link-default-hover-text-color': 'var(--ti-common-color-text-highlight)',
   'ti-link-default-text-color': 'var(--ti-common-color-text-highlight)',
   'ti-link-default-disabled-text-color': '#C2C2C2',
   'ti-link-icon-hover-text-weight': 'var(--ti-common-font-weight-4)',
   'ti-link-default-border-color': '#1476FF',
-  'ti-link-underline-width': 'calc(100% - 20px)'
+  'ti-link-has-icon-underline-width': 'calc(100% - 20px)',
+  'ti-link-has-svg-hover-text-color': '#1476FF',
+  'ti-link-danger-hover-text-color': 'var(--ti-common-color-danger-hover)',
+  'ti-link-primary-disabled-text-color': 'var(--ti-common-color-primary-disabled-text)',
+  'ti-link-danger-disabled-text-color': 'var(--ti-common-color-danger-disabled-text)',
+  'ti-link-success-disabled-text-color': 'var(--ti-common-color-success-disabled-text)',
+  'ti-link-warning-disabled-text-color': 'var(--ti-common-color-warning-disabled-text)',
+  'ti-link-info-disabled-text-color': 'var(--ti-common-color-info-disabled-text)'
 }
