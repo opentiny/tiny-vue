1d93f28abda5157ebfe971c05e493571e567110a
gimmyhehe <975402925@qq.com>
2023-09-08 03:00:25 -0700

    fix(link): smb theme change fix (#474)

--- packages/theme/src/link/aurora-theme.js
+++ packages/theme/src/link/aurora-theme.js
@@ -1,5 +1,6 @@
 export const tinyLinkAuroraTheme = {
   'ti-link-default-text-color': '#606266',
   'ti-link-default-hover-text-color': '#40a9ff',
-  'ti-link-has-svg-hover-text-color': '#40a9ff'
+  'ti-link-has-svg-hover-text-color': '#40a9ff',
+  'ti-link-primary-disabled-text-color': '#a0cfff'
 }
