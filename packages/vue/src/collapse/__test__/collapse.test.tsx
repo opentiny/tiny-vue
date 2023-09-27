0036fcb2037bfe20bac287f88d80df75e191390f
Kif <62132584+wkif@users.noreply.github.com>
2023-09-27 10:37:33 +0800

    fix(collapse): 修复collapse未深度监听导致Pc端tiny-collapse activeNames直接push无法响应的bug (#512)

--- packages/vue/src/collapse/__test__/collapse.test.tsx
+++ packages/vue/src/collapse/__test__/collapse.test.tsx
@@ -2,16 +2,21 @@ import { mountPcMode } from '@opentiny-internal/vue-test-utils'
 import { describe, test, expect, vi } from 'vitest'
 import Collapse from '@opentiny/vue-collapse'
 import CollapseItem from '@opentiny/vue-collapse-item'
-
+import { ref } from 'vue'
 describe('PC Mode', () => {
   const mount = mountPcMode
 
   const handleChange = vi.fn()
+  const activeNames = ref(['1'])
   const wrapper = mount(() => (
-    <Collapse modelValue="1" onChange={handleChange}>
+    <Collapse modelValue={activeNames.value} onChange={handleChange}>
       <CollapseItem title="一致性 Consistency" name="1">
         <div>面板内容1</div>
         <div>面板内容2</div>
+      </CollapseItem>
+      <CollapseItem title="反馈 Feedback" name="2">
+        <div>面板内容3</div>
+        <div>面板内容4</div>
       </CollapseItem>
     </Collapse>
   ))
@@ -33,5 +38,12 @@ describe('PC Mode', () => {
     expect(wrapper.find('.tiny-collapse').exists()).toBe(true)
     await wrapper.find('.tiny-collapse-item__header').trigger('click')
     expect(handleChange).toBeCalled()
+  })
+
+  // ref test
+  test('ref test', async () => {
+    activeNames.value.push('2')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('.is-active').exists()).toBeTruthy()
   })
 })
