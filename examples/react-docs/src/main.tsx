18f63ba58185e48cfc2b700dd669b67f4c5ad6fc
Mr.栋子 <guoxiaodong@MrdongzideMacBook-Pro.local>
2023-09-28 11:05:10 +0800

    fix: 开发链路调整

--- examples/react-docs/src/main.tsx
+++ examples/react-docs/src/main.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
