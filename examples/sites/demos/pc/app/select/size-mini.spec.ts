d7623d1e56c4e68bc420cf7dcbef82ac336c7952
Xppp0217 <82315158+Xppp0217@users.noreply.github.com>
2023-10-23 16:30:15 +0800

    fix（select-size）:select component medium，small，mini Type For One (#607)
    * fix:select组件medium，small，mini类型整合
    
    * fix（select-size）:select component medium，small，mini Type For One

--- examples/sites/demos/pc/app/select/size-mini.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('size-mini', async ({ page }) => {
-  await page.goto('http://localhost:7130/pc/select/size-mini')
-
-  const input = page.locator('#preview .tiny-input')
-  await expect(input).toHaveClass(/tiny-input-mini/)
-})
