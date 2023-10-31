cf51eb0f4ae59b7974bc0e987238798b515a00cc
Mr.栋子 <guoxiaodong167@gmail.com>
2023-10-31 14:39:24 +0800

    feat(react): use api of @vue/runtime-core in tiny-react

--- packages/react-common/src/index.ts
+++ packages/react-common/src/index.ts
@@ -1,28 +1,17 @@
-import * as hooks from 'react'
+import { useEffect } from 'react'
 import { Svg } from './svg-render.jsx'
-
-import { nextTick, ref, computed, readonly, watch, onBeforeUnmount, inject, provide } from './vue-hooks.js'
+import { generateVueHooks } from './vue-hooks.js'
 import { emit, on, off, once, emitEvent } from './event.js'
 import { If, Component, Slot, For, Transition } from './virtual-comp.jsx'
-import { filterAttrs, vc, getElementCssClass } from './utils.js'
+import { filterAttrs, vc, getElementCssClass, eventBus } from './utils.js'
 import { useFiber } from './fiber.js'
 import { useVm } from './vm.js'
-import { useReactive } from './reactive.js'
 import { twMerge } from 'tailwind-merge'
 import { stringifyCssClass } from './csscls.js'
+import { useExcuteOnce, useReload, useOnceResult, useVueLifeHooks } from './hooks.js'
+import { effectScope, nextTick } from '@vue/runtime-core'
 
 import '@opentiny/vue-theme/base/index.less'
-
-const vue_hooks = {
-  nextTick,
-  ref,
-  computed,
-  readonly,
-  watch,
-  onBeforeUnmount,
-  inject,
-  provide
-}
 
 // emitEvent, dispath, broadcast
 export const $prefix = 'Tiny'
@@ -41,73 +30,95 @@ export const useSetup = ({
   vm,
   parent
 }) => {
-  const render = typeof props.tiny_renderless === 'function' ? props.tiny_renderless : renderless
-  const { dispath, broadcast } = emitEvent(vm)
+  const $bus = useOnceResult(() => eventBus())
 
-  const utils = {
-    vm,
-    parent,
-    emit: emit(props),
-    constants,
-    nextTick,
-    dispath,
-    broadcast,
-    t() { },
-    mergeClass,
-    mode: props.tiny_mode
-  }
-  const sdk = render(
-    props,
-    {
-      ...hooks,
-      useReactive,
-      ...vue_hooks,
-      reactive: useReactive
-    },
-    utils,
-    extendOptions
-  )
-  const attrs = {
-    a: filterAttrs,
-    m: mergeClass,
-    vm: utils.vm,
-    gcls: (key) => getElementCssClass(classes, key),
-  }
+  // 刷新逻辑
+  const reload = useReload()
+  useExcuteOnce(() => {
+    // 1. 响应式出发 $bus 的事件
+    // 2. 事件响应出发组件更新
+    $bus.on('event:reload', reload)
+  })
 
-  if (Array.isArray(api)) {
-    api.forEach((name) => {
-      const value = sdk[name]
+  // 收集副作用，组件卸载自动清除副作用
+  const scope = useOnceResult(() => effectScope())
+  useEffect(() => {
+    return () => scope.stop()
+  }, [])
 
-      if (typeof value !== 'undefined') {
-        attrs[name] = value
-      }
+  // 执行一次 renderless
+  // renderless 作为 setup 的结果，最后要将结果挂在 vm 上
+  let setupResult = useExcuteOnce(() => {
+    const render = typeof props.tiny_renderless === 'function' ? props.tiny_renderless : renderless
+    const { dispath, broadcast } = emitEvent(vm)
+
+    const utils = {
+      vm,
+      parent,
+      emit: emit(props),
+      constants,
+      nextTick,
+      dispath,
+      broadcast,
+      t() { },
+      mergeClass,
+      mode: props.tiny_mode
+    }
+
+    let sdk
+    scope.run(() => {
+      sdk = render(
+        props,
+        {
+          ...generateVueHooks({
+            $bus
+          })
+        },
+        utils,
+        extendOptions
+      )
     })
-  }
 
-  return attrs
+    const attrs = {
+      a: filterAttrs,
+      m: mergeClass,
+      vm: utils.vm,
+      gcls: (key) => getElementCssClass(classes, key),
+    }
+
+    if (Array.isArray(api)) {
+      api.forEach((name) => {
+        const value = sdk[name]
+
+        if (typeof value !== 'undefined') {
+          attrs[name] = value
+        }
+      })
+    }
+
+    return attrs
+  })
+
+  useVueLifeHooks($bus)
+
+  return setupResult
 }
 
 export {
   Svg,
-  vc,
   If,
   Component,
   Slot,
   For,
   Transition,
+  vc,
   emit,
   on,
   off,
   once,
   emitEvent,
   useVm,
-  nextTick,
   useFiber,
-  ref,
-  computed,
-  readonly,
-  useReactive,
-  watch
 }
 
-export const reactive = useReactive
+export * from './vue-hooks.js'
