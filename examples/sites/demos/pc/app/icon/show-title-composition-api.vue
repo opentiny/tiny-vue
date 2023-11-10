07321e9f7e5eed8e539935259f3f7723fd5c1514
chenxi-20 <76168465+chenxi-20@users.noreply.github.com>
2023-11-10 14:10:21 +0800

    docs(icons): [icon] icon docs optimization (#779)

--- examples/sites/demos/pc/app/icon/show-title-composition-api.vue
+++ examples/sites/demos/pc/app/icon/show-title-composition-api.vue
@@ -1,6 +1,6 @@
 <template>
   <div title="复制" class="icon-demo">
-    <tiny-icon-copy class="tiny-svg-size icon-copy"></tiny-icon-copy>
+    <tiny-icon-copy></tiny-icon-copy>
   </div>
 </template>
 
@@ -11,12 +11,9 @@ const TinyIconCopy = iconCopy()
 </script>
 
 <style scoped>
-.icon-demo .tiny-svg-size {
+.icon-demo .tiny-svg {
   fill: #8994aa;
   margin: 20px 50px;
-}
-
-.icon-demo .icon-copy {
-  font-size: 1.8em;
+  font-size: 24px;
 }
 </style>
--- examples/sites/demos/pc/app/icon/webdoc/icon.en.md
+++ examples/sites/demos/pc/app/icon/webdoc/icon.en.md
@@ -4,4 +4,4 @@ title: Icon
 
 # Icon
 
-<div>Provides a set of common icons, which can be used by setting the class name `icon-iconName`.</div>
+<div>Common icon collection</div>
