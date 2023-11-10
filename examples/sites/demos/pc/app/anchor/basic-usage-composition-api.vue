131525efc3d00e4784c210f1e9f99bfc4f2c2e94
chenxi-20 <2465950588@qq.com>
2023-11-09 19:01:39 -0800

    docs(anchor): [anchor] anchor docs optimization

--- examples/sites/demos/pc/app/anchor/basic-usage-composition-api.vue
+++ examples/sites/demos/pc/app/anchor/basic-usage-composition-api.vue
@@ -1,7 +1,7 @@
 <template>
   <tiny-switch v-model="type" true-value="line" false-value="dot"> </tiny-switch>
   当前类型：{{ type }}
-  <tiny-anchor :links="links"></tiny-anchor>
+  <tiny-anchor :links="links" :type="type"></tiny-anchor>
 </template>
 
 <script setup>
