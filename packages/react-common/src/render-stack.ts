9ed64376d0b572701bcc121464a892c153598382
Mr.栋子 <guoxiaodong167@gmail.com>
2023-11-10 10:46:15 +0800

    feat(react): render-stack return empty string as valid react el

--- packages/react-common/src/render-stack.ts
+++ packages/react-common/src/render-stack.ts
@@ -1,7 +1,15 @@
 const renderStack = []
 
-export const getParent = () => renderStack[renderStack.length - 1]
+export const getParent = () => renderStack[renderStack.length - 1] || ({})
 
-export const EnterStack = (props) => renderStack.push(props)
+export const getRoot = () => renderStack[0] || ({})
 
-export const LeaveStack = () => renderStack.pop()
+export const EnterStack = (props) => {
+  renderStack.push(props)
+  return ''
+}
+
+export const LeaveStack = () => {
+  renderStack.pop()
+  return ''
+}
