91dc6be333330c7a79166a5592e2d80b889b4263
gimmyhehe <975402925@qq.com>
2023-11-09 22:08:25 -0800

    chore(docs): [switch] optimize switch demo and api (#782)

--- examples/sites/demos/pc/app/switch/mini-mode-composition-api.vue
+++ examples/sites/demos/pc/app/switch/mini-mode-composition-api.vue
@@ -2,8 +2,6 @@
   <tiny-switch mini></tiny-switch>
 </template>
 
-<script setup lang="jsx">
-import { Switch } from '@opentiny/vue'
-
-const TinySwitch = Switch
+<script setup>
+import { Switch as TinySwitch } from '@opentiny/vue'
 </script>
