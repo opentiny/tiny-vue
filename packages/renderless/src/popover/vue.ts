3535915f672981fa4609606a5311f99680dc741d
Gimmy <975402925@qq.com>
2023-11-13 00:51:06 -0800

    fix(components): [popover] fix popover offset error

--- packages/renderless/src/popover/vue.ts
+++ packages/renderless/src/popover/vue.ts
@@ -114,7 +114,7 @@ const initWatch = ({
         return
       }
       // mobile-first时，需要更新一下，arrow的位置才正确
-      if (mode === 'mobile-first' && val) {
+      if (val) {
         nextTick(() => updatePopper())
       }
       // 隐藏时，只冒一下事件，并没有调用doDestory();
