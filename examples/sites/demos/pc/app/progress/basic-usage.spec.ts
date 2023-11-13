0b0e8806585cd69192223d77800bbd1977007e47
chenxi-20 <76168465+chenxi-20@users.noreply.github.com>
2023-11-13 14:50:50 +0800

    docs(progress): [progress] progress docs optimization (#789)

--- examples/sites/demos/pc/app/progress/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/progress/basic-usage.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test'
 
-test('正常显示', async ({ page }) => {
+test('基础用法，是否可动态控制进度条', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).not.toBeNull())
   await page.goto('http://127.0.0.1:7130/pc/progress/basic-usage')
 
   const progress = page.getByRole('progressbar')
-  const outer = progress.locator('div.tiny-progress-bar__outer')
-  const inner = progress.locator('div.tiny-progress-bar__inner')
+  const progress1 = progress.nth(0).locator('.tiny-progress-bar__outer')
+  const progress2 = progress.nth(1).locator('.tiny-progress-bar__outer')
 
-  await expect(outer).toBeVisible()
-  await expect(outer).toHaveCSS('height', '6px')
-  await expect(inner).toBeVisible()
-  await expect(inner).toHaveCSS('border-radius', '3px')
+  await expect(progress1).toBeVisible()
+  await expect(progress2).toBeVisible()
+  await expect(progress1).toHaveCSS('height', '4px')
+  await expect(progress2).toHaveCSS('height', '24px')
+  await expect(progress2).toHaveCSS('border-radius', '12px')
   await expect(page.getByText('50%')).toBeVisible()
-  await expect(progress).toHaveAttribute('aria-valuemin', '0')
-  await expect(progress).toHaveAttribute('aria-valuemax', '100')
+
+  await expect(progress).toHaveAttribute('aria-valuenow', '20')
+  await page.locator('button').nth(3).click()
+  await expect(progress).toHaveAttribute('aria-valuenow', '10')
+  await page.locator('button').nth(4).click()
+  await expect(progress).toHaveAttribute('aria-valuenow', '20')
 })
