9194c3a51c8bd99ec40ccaa745087c68720c5841
zzcr <894103554@qq.com>
2023-11-15 00:08:30 -0800

    test(grid): [grid] update grid e2e test

--- examples/sites/demos/pc/app/grid/custom-style/body-style.spec.js
+++ examples/sites/demos/pc/app/grid/custom-style/body-style.spec.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('设置行样式', async ({ page }) => {
+test('自定义表体样式', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('grid-custom-style#custom-style-row-style')
-  await expect(page.getByText('WWW科技YX公司').first()).toHaveCSS('color', 'rgb(255, 0, 0)')
-  await expect(page.getByText('深圳', { exact: true }).first()).toHaveCSS('color', 'rgb(255, 0, 0)')
+  await page.goto('grid-custom-style#custom-style-body-style')
+  await expect(page.locator('.row__word--red').first()).toHaveCSS('color', 'rgb(219, 112, 147)')
+  await expect(page.locator('.col-orange').first()).toHaveCSS('background-color', 'rgb(255, 192, 203)')
 })
--- examples/vue3/playwright.config.js
+++ examples/vue3/playwright.config.js
@@ -2,5 +2,5 @@ import Config from '@opentiny-internal/playwright-config'
 
 export default Config({
   testDir: '../sites/demos',
-  baseURL: 'http://localhost:7130/pc/'
+  baseURL: 'http://localhost:3101/tiny-vue/zh-CN/os-theme/components/'
 })
