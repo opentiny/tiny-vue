a7418d1e596a33b2f55c5b8b1803d76296260dcb
Gimmy <975402925@qq.com>
2023-11-15 00:26:42 -0800

    docs(alert): [alert] optimize alert demo and api

--- examples/sites/demos/pc/app/alert/title-composition-api.vue
+++ examples/sites/demos/pc/app/alert/title-composition-api.vue
@@ -5,6 +5,14 @@
     <tiny-alert size="large">
       <template #title>通过 slot 设置自定义 title</template>
     </tiny-alert>
+    <br />
+    <tiny-alert size="large"></tiny-alert>
+    <br />
+    <tiny-alert size="large" type="success"></tiny-alert>
+    <br />
+    <tiny-alert size="large" type="error"></tiny-alert>
+    <br />
+    <tiny-alert size="large" type="warning"></tiny-alert>
   </div>
 </template>
 
