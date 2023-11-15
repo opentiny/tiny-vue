caa3414558b51f54010353aacbb9f1eb88b93442
yoyo <15014217605@163.com>
2023-11-15 09:25:25 +0800

    docs(site): [rate] Improve documentation (#814)
    * docs(site): [rate] Improve documentation
    
    * docs(site): [rate] Improve documentation 2

--- examples/sites/demos/pc/app/rate/basic-usage-composition-api.vue
+++ examples/sites/demos/pc/app/rate/basic-usage-composition-api.vue
@@ -2,7 +2,7 @@
   <tiny-rate v-model="rate1" show-text></tiny-rate>
 </template>
 
-<script setup lang="jsx">
+<script setup>
 import { ref } from 'vue'
 import { Rate as TinyRate } from '@opentiny/vue'
 
--- examples/sites/demos/pc/app/rate/texts-and-text-color.vue
+++ examples/sites/demos/pc/app/rate/texts-and-text-color.vue
@@ -2,7 +2,7 @@
   <tiny-rate v-model="rate" :texts="['很差', '差', '一般', '好', '很好']" show-text text-color="red"></tiny-rate>
 </template>
 
-<script lang="jsx">
+<script>
 import { Rate } from '@opentiny/vue'
 
 export default {
