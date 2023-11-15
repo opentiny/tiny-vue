4dba22c5e2872d3bf80f795d9ca19ca793dc3932
zhanyini <zhanyini666@163.com>
2023-11-14 18:32:49 -0800

    docs(select): [select] Select component documentation and demo optimization

--- examples/sites/demos/pc/app/select/show-alloption.vue
+++ examples/sites/demos/pc/app/select/show-alloption.vue
@@ -1,5 +1,5 @@
 <template>
-  <tiny-select v-model="value" placeholder="请选择" multiple :show-alloption="false">
+  <tiny-select v-model="value" multiple :show-alloption="false">
     <tiny-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </tiny-option>
   </tiny-select>
 </template>
@@ -26,3 +26,9 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.tiny-select {
+  width: 280px;
+}
+</style>
