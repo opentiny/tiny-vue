fec7dd71fad892351119b092ac05b5726d988eba
申君健 <40288193@qq.com>
2023-11-16 14:07:17 +0800

    docs(tooltip):   Optimize Tooltip's docs and demos (#838)
    * docs(tooltip):   Optimize Tooltip's docs and demos
    
    * docs(tooltip): fix

--- examples/sites/demos/pc/app/tooltip/delay-composition-api.vue
+++ examples/sites/demos/pc/app/tooltip/delay-composition-api.vue
@@ -3,6 +3,9 @@
     <tiny-tooltip content="延迟 2000ms 出现" :open-delay="2000">
       <button class="tiny-button tiny-button--primary">延迟 2s 出现</button>
     </tiny-tooltip>
+    <tiny-tooltip content="延迟 2000ms 关闭" :close-delay="2000">
+      <button class="tiny-button tiny-button--primary">延迟 2s 关闭</button>
+    </tiny-tooltip>
     <tiny-tooltip content="2000ms后自动隐藏" :hide-after="2000">
       <button class="tiny-button tiny-button--primary">2s 后自动隐藏</button>
     </tiny-tooltip>
