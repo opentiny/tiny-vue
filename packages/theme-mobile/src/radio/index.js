8c9b3086edc1bb72e66cb2b54c0e608482b60c31
jacknan <zhumaonan@aliyun.com>
2023-11-16 17:16:23 +0800

    feat(radio): 刷新设计规范
    Signed-off-by: jacknan <zhumaonan@aliyun.com>

--- packages/theme-mobile/src/radio/index.js
+++ packages/theme-mobile/src/radio/index.js
@@ -1,7 +1,11 @@
 export default {
-  'tiny-mobile-radio-size': '24px',
-  'tiny-mobile-radio-checked-color': '#f36f64',
-  'tiny-mobile-radio-disabled-color': '#ddd',
-  'tiny-mobile-radio-inner-border-color': '#fff',
-  'tiny-mobile-radio-label-font-size': '16px'
+  '--ti-mobile-radio-size': '24px',
+  '--ti-mobile-radio-size-outer': '20px',
+  '--ti-mobile-radio-size-inner': '10px',
+  '--ti-mobile-radio-checked-color': '#1476ff',
+  '--ti-mobile-radio-border-color-default': '#c2c2c2',
+  '--ti-mobile-radio-color-disabled': '#dbdbdb',
+  '--ti-mobile-radio-bg-color-disabled': '#fafafa',
+  '--ti-mobile-radio-label-font-size': '14px',
+  '--ti-mobile-radio-label-text-color': '#191919'
 }
