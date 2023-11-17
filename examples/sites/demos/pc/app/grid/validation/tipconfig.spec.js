ff0329852734eacd0f133a39a9afedfdb37773a4
zzcr <894103554@qq.com>
2023-11-16 23:58:42 -0800

    test(grid): [grid] update grid e2e test

--- examples/sites/demos/pc/app/grid/validation/tipconfig.spec.js
+++ examples/sites/demos/pc/app/grid/validation/tipconfig.spec.js
@@ -1,20 +1,21 @@
 import { test, expect } from '@playwright/test'
 
-test('隐藏列编辑时校验', async ({ page }) => {
+test('错误提示配置项', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('grid-validation#validation-editing-isvalidalways-validation')
+  await page.goto('grid-validation#validation-tipconfig')
+  await page.getByText('GFD科技YX公司').click()
   await page
     .getByRole('row', {
       name: '1 华东区 福州 公司技术和研发实力雄厚，是国家863项目的参与者，并被政府认定为“高新技术企业”。'
     })
     .getByRole('textbox')
-    .clear()
+    .click()
   await page
     .getByRole('row', {
       name: '1 华东区 福州 公司技术和研发实力雄厚，是国家863项目的参与者，并被政府认定为“高新技术企业”。'
     })
     .getByRole('textbox')
-    .fill('GF')
+    .fill('')
 
-  await expect(page.getByRole('tooltip', { name: '名称长度在 3 到 50 个字符' })).toBeVisible()
+  await expect(page.getByRole('tooltip', { name: '名称必填' })).toBeVisible()
 })
