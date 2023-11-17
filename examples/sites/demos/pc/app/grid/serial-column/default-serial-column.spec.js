b734f4da355337e5d0899c25a245a21e83fda643
zzcr <894103554@qq.com>
2023-11-16 22:49:35 -0800

    test(grid): [grid] update grid e2e test

--- examples/sites/demos/pc/app/grid/serial-column/default-serial-column.spec.js
+++ examples/sites/demos/pc/app/grid/serial-column/default-serial-column.spec.js
@@ -4,5 +4,5 @@ test('操作列基本用法', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-serial-column#serial-column-default-serial-column')
 
-  await expect(page.getByText('5')).toHaveText('5')
+  await expect(page.getByRole('cell', { name: '5' })).toBeVisible()
 })
