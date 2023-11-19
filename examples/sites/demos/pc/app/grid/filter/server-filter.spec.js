fbe1e9e57f792f70dc1027bf7750b07c7df7f927
zzcr <894103554@qq.com>
2023-11-19 00:29:45 -0800

    test(grid): [grid] update grid e2e test

--- examples/sites/demos/pc/app/grid/filter/server-filter.spec.js
+++ examples/sites/demos/pc/app/grid/filter/server-filter.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test'
 test('服务端过滤', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-filter#filter-server-filter')
+  await page.waitForTimeout(500)
   await page.getByRole('cell', { name: '城市' }).getByRole('img').first().click()
   await page.locator('li').filter({ hasText: '深圳' }).click()
   await page.getByRole('button', { name: '确定' }).click()
