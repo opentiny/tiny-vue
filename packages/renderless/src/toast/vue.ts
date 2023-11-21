5cf3917a8b3d204f0b5fec21136dd4d9d8622308
jacknan <zhumaonan@aliyun.com>
2023-11-21 10:02:41 +0800

    feat(toast): add mobile-toast (#751)
    * feat(toast): 增加toast组件
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * fix(toast): 删除日志打印
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 优化demo样式
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 刷新设计规范
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 删除无用代码
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 拆分特性demo
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 刷新token
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 刷新token定义
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(toast): 删除无用文件
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    ---------
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>

---
+++ packages/renderless/src/toast/vue.ts
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2022 - present TinyVue Authors.
+ * Copyright (c) 2022 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
+
+import { ISharedRenderlessParamHooks } from 'types/shared.type'
+import { timeout } from './index'
+import { IToastApi, IToastState, IToastProps } from 'types/toast.type'
+
+export const api = ['state', 'timeout']
+
+export const renderless = (props: IToastProps, { reactive }: ISharedRenderlessParamHooks, { vm, emit }): IToastApi => {
+  const state: IToastState = reactive({
+    text: props.text,
+    type: props.type,
+    time: props.time as number
+  })
+
+  const api: IToastApi = {
+    state,
+    timeout: timeout(emit)
+  }
+
+  return api
+}
---
+++ packages/vue/src/toast/src/index.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2022 - present TinyVue Authors.
+ * Copyright (c) 2022 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
+import { $props, $prefix, $setup, defineComponent } from '@opentiny/vue-common'
+import template from 'virtual-template?mobile'
+
+export const toastProps = {
+  ...$props,
+  type: {
+    type: String,
+    validator: (value: string) => Boolean(~['text', 'correct', 'error'].indexOf(value))
+  },
+  zIndex: {
+    type: Number,
+    default: 100
+  },
+  text: {
+    type: String,
+    default: 'text'
+  },
+  time: {
+    type: [String, Number],
+    default: 2000
+  },
+  timeout: {
+    type: Function,
+    default: null
+  }
+}
+
+export default defineComponent({
+  name: $prefix + 'Toast',
+  emits: ['timeout'],
+  props: toastProps,
+  setup(props, context) {
+    return $setup({ props, context, template })
+  }
+})
