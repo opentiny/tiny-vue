f0bae1e5466a359d91c38ea7b44e7ec6c85089e5
zzcr <894103554@qq.com>
2023-11-21 17:47:16 -0800

    fix(grid): [grid] fix errror E2E test

--- examples/sites/demos/pc/app/grid/fixed/right-fixed.spec.js
+++ examples/sites/demos/pc/app/grid/fixed/right-fixed.spec.js
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test'
 test('右冻结', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-fixed#fixed-right-fixed')
+  await page.waitForTimeout(100)
   await expect(page.getByRole('cell', { name: '城市' })).toHaveCSS('right', '4px')
 })
