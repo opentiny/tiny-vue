cf2a85d34cb06bd3c34a972a88412f34f9f5f871
yoyo <15014217605@163.com>
2023-11-22 16:59:40 +0800

    test(site/loading): [loading] e2e test case completion (#896)

--- examples/sites/demos/pc/app/loading/background.spec.ts
+++ examples/sites/demos/pc/app/loading/background.spec.ts
@@ -4,5 +4,5 @@ test('自定义遮罩背景色', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('loading#background')
   const loadingBg = page.locator('.tiny-loading__mask')
-  await expect(loadingBg).toHaveCSS('background-color', 'rgba(0, 0, 0, 0.3)')
+  await expect(loadingBg).toHaveCSS('background-color', 'rgba(0, 0, 0, 0.2)')
 })
