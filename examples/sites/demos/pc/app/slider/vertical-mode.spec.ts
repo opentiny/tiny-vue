0be0ccea17aeb77cd4e2199f7507c56f936e354a
yoyo <15014217605@163.com>
2023-11-23 16:28:13 +0800

    test(site/app): [slider, notify] e2e test case completion (#907)

--- examples/sites/demos/pc/app/slider/vertical-mode.spec.ts
+++ examples/sites/demos/pc/app/slider/vertical-mode.spec.ts
@@ -3,18 +3,7 @@ import { test, expect } from '@playwright/test'
 test('竖向滑块', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('slider#vertical-mode')
-  const preview = page.locator('#preview')
-  const slider = preview.locator('.tiny-slider__wrapper > .tiny-slider')
-  const sliderBlock = slider.locator('div').nth(1)
-  // 获取进度条的高度
-  const { height: sliderHeight } = await slider.boundingBox()
-  const { x, y } = await sliderBlock.boundingBox()
+  const slider = page.locator('.tiny-slider-container .tiny-slider')
 
-  await sliderBlock.hover()
-  await page.mouse.down()
-  await page.mouse.move(x, sliderHeight * 0.2 + y)
-  const sliderTip = slider.locator('div').nth(2)
-  await expect(sliderTip).toHaveText('20')
-  await page.waitForTimeout(300)
-  await page.mouse.up()
+  await expect(slider).toHaveClass(/tiny-slider__vertical/)
 })
