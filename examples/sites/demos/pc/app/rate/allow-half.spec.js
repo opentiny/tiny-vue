9c3759a70f13d1c86b85dedcd57eb396d9497e84
yoyo201626 <15014217605@163.com>
2023-11-24 16:16:17 +0800

    test(site/pc/app): [rate, tag-group, input] e2e test case completion 3

--- examples/sites/demos/pc/app/rate/allow-half.spec.js
+++ examples/sites/demos/pc/app/rate/allow-half.spec.js
@@ -9,6 +9,7 @@ test.describe('Rate 允许半选', () => {
     const icon = page.locator('.tiny-rate__star > .tiny-svg')
     const rate = await page.locator('.tiny-rate > span:nth-child(1)').boundingBox()
     await page.mouse.click(rate.x + rate.width / 2, rate.y + rate.height / 2)
+    await page.waitForTimeout(200)
     await expect(icon.nth(0)).toHaveClass(/ratehalf/)
   })
 })
