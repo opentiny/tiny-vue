415045514c1c8b50edf6b5aa74a755e952cbc157
ajaxzheng <894103554@qq.com>
2023-11-25 14:19:02 +0800

    docs(split):  [split] optimize split docs and e2e (#923)
    * docs(split):  [split] optimize split docs and e2e
    
    * docs(split):  [split] optimize split docs and e2e
    
    * docs(split):  [split] optimize split docs and e2e

--- examples/sites/demos/pc/app/split/disabled-drag.spec.ts
+++ examples/sites/demos/pc/app/split/disabled-drag.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('基本用法', async ({ page }) => {
+test('禁用拖拽', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('split#basic-usage')
+  await page.goto('split#disabled-drag')
   // 获取拖拽元素
   const triggerBtn = page.locator('.tiny-split-trigger')
   const leftPanel = page.locator('.left-pane')
@@ -16,6 +16,5 @@ test('基本用法', async ({ page }) => {
   // 移动之后的宽度为:移动之前的宽度+50-分割线宽度的一半
   const afterMove = leftPanelWidth + 100 - triggerBtnWidth / 2
   const { width: afterWidth } = await leftPanel.boundingBox()
-  // 小数点后一位相同即可
-  await expect(afterMove).toBeCloseTo(afterWidth, 1)
+  await expect(afterMove).not.toEqual(afterWidth)
 })
