9dc713243abaf5b7994a60eb63109ebea6e85423
MomoPoppy <125256456+MomoPoppy@users.noreply.github.com>
2023-11-25 18:04:25 +0800

    test(dropdown): [dropdown] Optimize dropdown component e2e test cases (#930)

--- examples/sites/demos/pc/app/dropdown/title.spec.ts
+++ examples/sites/demos/pc/app/dropdown/title.spec.ts
@@ -4,8 +4,8 @@ test('自定义文本', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('dropdown#title')
 
-  const preview = page.locator('#preview')
-  const dropDown = preview.locator('.tiny-dropdown')
+  const wrap = page.locator('#title')
+  const dropDown = wrap.locator('.tiny-dropdown')
 
   await expect(dropDown).toContainText('自定义文本')
 })
