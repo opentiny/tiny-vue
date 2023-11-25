a423791d51044fc463a89387aad5f041e1ec9a62
zhanyini <zhanyini666@163.com>
2023-11-25 02:14:47 -0800

    test(action-menu): [action-menu] Optimize action-menu component e2e test cases

--- examples/sites/demos/pc/app/action-menu/popper-class.spec.ts
+++ examples/sites/demos/pc/app/action-menu/popper-class.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-test('自定义下拉面板样式', async ({ page }) => {
+test('弹框样式', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('action-menu#popper-class')
 
-  const preview = page.locator('#preview')
-  const actionMenu = preview.locator('.tiny-action-menu')
-  const menuItem = actionMenu.locator('.tiny-action-menu__item')
+  const wrap = page.locator('#popper-class')
+  const actionMenu = wrap.locator('.tiny-action-menu')
+  const visibleItem = actionMenu.locator('.tiny-action-menu__item')
   const dropDownMenu = page.locator('body > .tiny-dropdown-menu.tiny-popper')
 
-  await menuItem.last().hover()
+  await page.waitForTimeout(300)
+  await visibleItem.last().hover()
   await expect(dropDownMenu).toHaveClass(/custom-action-menu/)
+  await expect(dropDownMenu).toHaveCSS('background-color', 'rgb(250, 235, 215)')
 })
