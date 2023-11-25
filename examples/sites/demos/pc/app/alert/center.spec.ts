270cc59f5fadc21cd016f9e429053ba4a078847b
Gimmy <975402925@qq.com>
2023-11-25 16:36:03 +0800

    test(alert): [alert] update alert E2E test

--- examples/sites/demos/pc/app/alert/center.spec.ts
+++ examples/sites/demos/pc/app/alert/center.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('测试 Alert 提示内容是否居中', async ({ page }) => {
+test('文字居中', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('alert#center')
 
--- examples/sites/demos/pc/app/alert/custom-close.spec.ts
+++ examples/sites/demos/pc/app/alert/custom-close.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test'
 
-test('自定义关闭图标，控制警告的再次显示或隐藏', async ({ page }) => {
+test('关闭按钮', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('alert#custom-close')
 
-  const alertSuccess = page.locator('.tiny-alert--success')
-  const defaultClose = alertSuccess.locator('.tiny-alert__close')
-  const customClose = alertSuccess.locator('.icon')
-  const btn = page.getByRole('button', { name: '显示或隐藏警告' })
+  const demo = page.locator('#custom-close')
+  const customAlert = demo.locator('.tiny-alert').first()
+  const defaultClose = customAlert.locator('.tiny-alert__close')
+  const customClose = customAlert.locator('.icon')
+  const switchBtn = demo.locator('.tiny-switch')
 
   // 自定义关闭图标
   await expect(defaultClose).not.toBeVisible()
@@ -15,40 +16,40 @@ test('自定义关闭图标，控制警告的再次显示或隐藏', async ({ pa
 
   // 点击自定义关闭按钮，警告消失
   await customClose.click()
-  await expect(alertSuccess).toBeHidden()
-  await btn.click()
+  await expect(customAlert).toBeHidden()
+  await switchBtn.click()
 
   // 点击按钮控制警告消失或隐藏
-  await expect(alertSuccess).toBeVisible()
-  await btn.click()
-  await expect(alertSuccess).toBeHidden()
-  await btn.click()
-  await expect(alertSuccess).toBeVisible()
-})
-
-test('关闭按钮事件', async ({ page }) => {
-  page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('alert#custom-close')
-
-  const alertWarning = page.locator('.tiny-alert')
-  const close = alertWarning.locator('.tiny-alert__close')
-
-  // 点击关闭后警告消失，自定义事件modalBox提示出现
-  await close.click()
-  await expect(alertWarning).toBeHidden()
-  await expect(page.locator('.tiny-modal__box')).toBeVisible()
+  await expect(customAlert).toBeVisible()
+  await switchBtn.click()
+  await expect(customAlert).toBeHidden()
+  await switchBtn.click()
+  await expect(customAlert).toBeVisible()
 })
 
 test('自定义关闭按钮文本', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('alert#custom-close')
 
-  const alertError = page.locator('.tiny-alert')
+  const alertError = page.locator('.tiny-alert').nth(1)
   const closeText = alertError.locator('.tiny-alert__close')
 
   // 自定义按钮文字
-  await expect(alertError.locator('.tiny-alert__close')).not.toBeVisible()
-  await expect(closeText).toBeVisible()
+  await expect(closeText).not.toBeVisible()
+  await expect(alertError.locator('.is-custom').getByText('关闭')).toBeVisible()
+})
+
+test('关闭按钮事件', async ({ page }) => {
+  page.on('pageerror', (exception) => expect(exception).toBeNull())
+  await page.goto('alert#custom-close')
+
+  const alertWarning = page.locator('.tiny-alert--warning')
+  const close = alertWarning.locator('.tiny-alert__close')
+
+  // 点击关闭后警告消失，自定义事件modalBox提示出现
+  await close.click()
+  await expect(alertWarning).not.toBeVisible()
+  await expect(page.locator('.tiny-modal__box').getByText('关闭了')).toBeVisible()
 })
 
 test('不可关闭警告', async ({ page }) => {
