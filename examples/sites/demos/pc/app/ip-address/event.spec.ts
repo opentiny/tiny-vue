9dfb06e94595a6792561fae01cf0a3e24d895811
Gimmy <975402925@qq.com>
2023-11-27 19:59:44 +0800

    test(ip-address): [ip-address] add timeout for run case

--- examples/sites/demos/pc/app/ip-address/event.spec.ts
+++ examples/sites/demos/pc/app/ip-address/event.spec.ts
@@ -7,7 +7,6 @@ test('ipAddress change事件', async ({ page }) => {
   const demo = page.locator('#event')
   const getIPAddressFirstInput = (index: number) =>
     demo.locator('.tiny-ip-address__input').nth(index).locator('input').first()
-  const input = demo.getByRole('textbox')
   const modal = page.locator('.tiny-modal.active')
 
   // 聚焦、失焦事件
@@ -17,15 +16,18 @@ test('ipAddress change事件', async ({ page }) => {
   await expect(modal.filter({ hasText: 'focus 事件触发了' })).toBeVisible()
   await getIPAddressFirstInput(0).blur()
   await expect(modal.filter({ hasText: 'blur 事件触发了' })).toBeVisible()
+  await page.waitForTimeout(500)
 
   // 内容改变事件
   await getIPAddressFirstInput(1).fill('111')
   await getIPAddressFirstInput(1).blur()
   await expect(modal.filter({ hasText: 'change 事件触发了' })).toBeVisible()
+  await page.waitForTimeout(500)
 
   // 输入事件事件
   await getIPAddressFirstInput(2).fill('111')
   await expect(modal.filter({ hasText: 'input 事件触发了' })).toBeVisible()
+  await page.waitForTimeout(500)
 
   // 选中事件
   const { x = 0, y = 0, width = 0 } = (await getIPAddressFirstInput(3).boundingBox()) || {}
