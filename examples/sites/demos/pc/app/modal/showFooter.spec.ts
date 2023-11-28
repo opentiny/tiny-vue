ec8096c532c347160171047a26295f7a182ad30e
gimmyhehe <975402925@qq.com>
2023-11-28 15:54:49 +0800

    test(modal): [modal] update modal E2E test (#939)

--- examples/sites/demos/pc/app/modal/showFooter.spec.ts
+++ examples/sites/demos/pc/app/modal/showFooter.spec.ts
@@ -4,7 +4,7 @@ test('不显示底部', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('modal#showFooter')
 
-  const modal = page.locator('.tiny-modal__box')
+  const modal = page.locator('.tiny-modal.active')
   await page.getByRole('button', { name: '不显示底部' }).click()
   await expect(modal.locator('.tiny-modal__footer')).not.toBeVisible()
 })
