5f1a16afd5a9bf6f3c2298eb7ce7a2641a04825b
chenxi-20 <76168465+chenxi-20@users.noreply.github.com>
2023-11-30 16:26:01 +0800

    test(search): [search] update search E2E test (#972)

--- examples/sites/demos/pc/app/search/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/search/basic-usage.spec.ts
@@ -2,20 +2,19 @@ import { expect, test } from '@playwright/test'
 
 test('是否正常渲染', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('http://127.0.0.1:7130/pc/search/basic-usage')
+  await page.goto('search#basic-usage')
 
-  const search = page.locator('#preview .tiny-search')
-  const first = search.locator('.tiny-search__line')
-  const selector = search.locator('.tiny-search__selector')
+  const first = page.locator('.tiny-search__line')
+  const selector = page.locator('.tiny-search__selector')
   const firstPresent = first.locator('.tiny-search__present')
   const firstInput = first.locator('.tiny-search__input')
   const icon = first.locator('a > svg')
 
-  await expect(first).toBeVisible()
-  await expect(icon).toBeVisible()
+  await expect(first).toHaveCount(1)
+  await expect(icon).toHaveCount(1)
   await expect(selector).not.toBeVisible()
   await expect(firstPresent).not.toBeVisible()
-  await expect(firstInput).toHaveValue('搜索')
+  await expect(firstInput).toHaveValue('默认值')
   await firstInput.fill('123')
   await expect(firstInput).toHaveValue('123')
 })
