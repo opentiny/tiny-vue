061a8918fddee7f5c46e0d040ef9b0a7adc6e8a4
shenjunjian <40288193@qq.com>
2023-11-30 19:29:13 -0800

    docs(e2e): Add tree /transfer e2e tests

--- examples/sites/demos/pc/app/tree/contextmenu.spec.ts
+++ examples/sites/demos/pc/app/tree/contextmenu.spec.ts
@@ -1,14 +1,9 @@
 import { test, expect } from '@playwright/test'
 
-test('可自定义右键菜单内容', async ({ page }) => {
+test('右键菜单', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('tree#contextmenu')
 
-  await page.getByText('三级 1-1-1').first().click({
-    button: 'right'
-  })
-  const menu = page.locator('#preview .tiny-tree-node__menu')
-  await expect(menu).toBeVisible()
-  await page.locator('.context-menu li').first().click()
-  await expect(menu).not.toBeVisible()
+  const tree = page.locator('.pc-demo-container')
+  await expect(tree.getByText('数据 1-1-1')).toHaveCount(1)
 })
--- examples/sites/demos/pc/app/tree/slot.spec.ts
+++ examples/sites/demos/pc/app/tree/slot.spec.ts
@@ -1,23 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('tree组件基本使用', async ({ page }) => {
+test('插槽', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('tree#basic-usage')
+  await page.goto('tree#slot')
 
-  const tree = page.locator('#preview .tiny-tree')
-  await tree.getByText('一级 1').click()
-  await expect(tree.locator('.tiny-tree-node').first()).toHaveClass(/is-current/)
-  await page.getByText('二级 1-1').click()
-  await expect(tree.locator('.tiny-tree-node').nth(1)).toHaveClass(/is-current/)
-  await page.getByRole('treeitem', { name: '三级 1-1-1' }).first().click()
-  await page.getByRole('treeitem', { name: '一级 3' }).locator('.tiny-svg').click()
-  await page
-    .getByRole('treeitem', { name: '一级 1' })
-    .first()
-    .locator('div')
-    .filter({ hasText: '一级 1' })
-    .first()
-    .click()
-  await expect(page.getByRole('treeitem', { name: '一级 1-1', includeHidden: true })).toBeHidden({ timeout: 0 })
-  await page.locator('.tiny-modal__box').first().click()
+  const tree = page.locator('.pc-demo-container')
+  const button = page.getByRole('button', { name: '加载数据' })
+
+  await button.click()
+  await expect(tree.getByText('数据 1-1-1')).toHaveCount(2)
 })
