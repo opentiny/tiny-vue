e85d5afc5c254fa3aa72a0cd6e87a64050e676c1
James-9696 <hujm969696@163.com>
2023-12-03 18:14:00 -0800

    test(fall-menu):[fall-menu]update fall-menu E2E test

--- examples/sites/demos/pc/app/fall-menu/custom-slider-icon.spec.ts
+++ examples/sites/demos/pc/app/fall-menu/custom-slider-icon.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test'
 test('自定义左、右侧图标', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('fall-menu#custom-slider-icon')
-  const preview = page.locator('#preview')
+  // const preview = page.locator('#preview')
   // 右侧插槽图标
-  const rightSlotIcon = preview.locator('.icon-slot-right')
+  const rightSlotIcon = page.locator('.icon-slot-right')
   // 父菜单列表
-  const fallMenuList = preview.locator('.tiny-fall-menu__list')
+  const fallMenuList = page.locator('.tiny-fall-menu__list')
   // 是否是默认图标
   await expect(rightSlotIcon.locator('svg path').getAttribute('d')).not.toContain(
     'M7 21c.2 0 .5-.1.6-.2l9.9-8c.2-.2.4-.5.4-.8'
@@ -17,7 +17,7 @@ test('自定义左、右侧图标', async ({ page }) => {
   // 向左移动
   await expect(fallMenuList).toHaveCSS('left', /^-\d+px/)
   // 左侧插槽图标
-  const leftSlotIcon = preview.locator('.icon-slot-left')
+  const leftSlotIcon = page.locator('.icon-slot-left')
   await expect(leftSlotIcon.locator('svg path').getAttribute('d')).not.toContain(
     'M17 21c-.2 0-.5-.1-.6-.2l-9.9-8c-.4-.2-.5-.5-.5-.8'
   )
