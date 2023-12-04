b392992db541ab06b4b64f1d1968a730e85f9a51
James-9696 <hujm969696@163.com>
2023-12-04 00:51:11 -0800

    test(fall-menu)[fall-menu] update fall-menu E2E test

--- examples/sites/demos/pc/app/fall-menu/custom-menuitem.spec.ts
+++ examples/sites/demos/pc/app/fall-menu/custom-menuitem.spec.ts
@@ -4,14 +4,11 @@ test('自定义内容', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('fall-menu#custom-menuitem')
   // 子菜单列表容器
-  const fallMenuBox = page.locator('.tiny-fall-menu__box')
   const preview = page.locator('.tiny-fall-menu__wrap')
   // 父菜单列表
   const fallMenuList = page.locator('.tiny-fall-menu__list')
-  // await preview.getByText(/首页/).hover()
-  await expect(fallMenuBox).toBeHidden()
-  await preview.getByText(`指南 level1 `, { exact: false }).click()
-  await expect(fallMenuBox).toBeVisible()
+  await preview.getByText('指南 level1').hover()
+  await page.waitForTimeout(1000)
   // 点击左、右侧图标切换列表
   await preview.locator('.tiny-fall-menu__nav > .tiny-fall-menu__subnav > .icon-slot-right').click()
   await expect(fallMenuList).toHaveCSS('left', /^-\d+px/)
--- examples/sites/demos/pc/app/fall-menu/data-resource.spec.ts
+++ examples/sites/demos/pc/app/fall-menu/data-resource.spec.ts
@@ -5,22 +5,18 @@ test('基础用法、数据源', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('fall-menu#data-resource')
   const preview = page.locator('.tiny-fall-menu__wrap')
-  // const fallMenuBox = page.locator('.tiny-fall-menu__box')
-  const rightSlotIcon = preview.locator('.tiny-fall-menu__nav > .tiny-fall-menu__subnav > .icon-slot-right')
-  const leftSlotIcon = preview.locator('.tiny-fall-menu__nav > .tiny-fall-menu__subnav > .icon-slot-left')
   // 父菜单列表
   const fallMenuList = page.locator('.tiny-fall-menu__list')
   // 菜单列表应该可见
-  // await expect(fallMenuBox).toBeHidden()
-  await preview.getByText(`指南`, { exact: false }).click()
-  // await expect(fallMenuBox).toBeVisible()
+  await preview.getByText(/^指南*$/i).hover()
+  await page.waitForTimeout(1000)
   // 菜单列表应该不可见
   // 点击右侧更多切换图标
-  await rightSlotIcon.click()
+  await preview.locator('.tiny-fall-menu__nav > .tiny-fall-menu__subnav > .icon-slot-right').click()
   // 向左移动
   await expect(fallMenuList).toHaveCSS('left', /^-\d+px/)
   // 点击左侧更多切换图标
-  await leftSlotIcon.click()
+  await preview.locator('.tiny-fall-menu__nav > .tiny-fall-menu__subnav > .icon-slot-left').click()
   // 向右移动
   await expect(fallMenuList).toHaveCSS('left', '0px')
 })
