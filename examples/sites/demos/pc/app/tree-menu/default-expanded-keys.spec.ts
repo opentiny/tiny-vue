884185001af3ae5e8c5c1e7421ad61514393d029
MomoPoppy <125256456+MomoPoppy@users.noreply.github.com>
2023-12-05 17:11:38 +0800

    test(tree-menu): [tree-menu] Optimizing e2e test cases for TreeMenu components (#1031)

--- examples/sites/demos/pc/app/tree-menu/default-expanded-keys.spec.ts
+++ examples/sites/demos/pc/app/tree-menu/default-expanded-keys.spec.ts
@@ -4,15 +4,19 @@ test('默认展开某节点', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('tree-menu#default-expanded-keys')
 
-  const treeMenu = page.locator('#preview .tiny-tree-menu')
+  const wrap = page.locator('#default-expanded-keys')
+  const treeMenu = wrap.locator('.tiny-tree-menu')
+  const treeNode = treeMenu.locator('.tiny-tree-node__wrapper > .tiny-tree-node')
+  const treeNodeContent = treeNode.locator('> .tiny-tree-node__content')
+
   // 默认展开节点
-  await expect(treeMenu.getByTitle('数据组件')).toBeVisible()
+  await expect(treeNodeContent.filter({ hasText: /^数据组件$/ })).toBeVisible()
   // 默认展开节点的祖先节点展开
-  await expect(treeMenu.getByTitle('组件', { exact: true }).first()).toBeVisible()
+  await expect(treeNodeContent.filter({ hasText: /^组件/ })).toBeVisible()
   // 默认展开节点兄弟节点展开
-  await expect(treeMenu.getByTitle('导航组件')).toBeVisible()
+  await expect(treeNodeContent.filter({ hasText: /^导航组件$/ })).toBeVisible()
   // 默认展开节点的下一层次节点不展开
-  await expect(treeMenu.getByTitle('Datepicker 日期')).not.toBeVisible()
+  await expect(treeNodeContent.filter({ hasText: /^Datepicker 日期$/ })).toBeVisible()
   // 非默认展开节点子的祖先节点的子节点不展开
-  await expect(treeMenu.getByTitle('页面布局')).not.toBeVisible()
+  await expect(treeNodeContent.filter({ hasText: /^页面布局/ })).toBeHidden()
 })
--- examples/sites/demos/pc/app/tree-menu/menu-collapsible.spec.ts
+++ examples/sites/demos/pc/app/tree-menu/menu-collapsible.spec.ts
@@ -4,8 +4,8 @@ test('菜单可折叠', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('tree-menu#menu-collapsible')
 
-  const preview = page.locator('#preview')
-  const treeMenu = preview.locator('.tiny-tree-menu')
+  const wrap = page.locator('#menu-collapsible')
+  const treeMenu = wrap.locator('.tiny-tree-menu')
   const toggleBtn = treeMenu.locator('.tiny-tree-menu__toggle-button')
 
   await expect(toggleBtn).toBeVisible()
@@ -13,4 +13,7 @@ test('菜单可折叠', async ({ page }) => {
   await toggleBtn.click()
   await expect(treeMenu).toHaveClass(/is-collapsed/)
   await expect(treeMenu).toHaveCSS('width', '0px')
+  await toggleBtn.click()
+  await expect(treeMenu).not.toHaveClass(/is-collapsed/)
+  await expect(treeMenu).toHaveCSS('width', '270px')
 })
