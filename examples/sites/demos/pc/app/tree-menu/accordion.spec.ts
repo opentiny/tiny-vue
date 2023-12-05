3192ad14d0e77d6c96d6d3d6a1c4a490956c5840
zhanyini <zhanyini666@163.com>
2023-12-04 23:25:33 -0800

    test(tree-menu): [tree-menu] Optimizing e2e test cases for TreeMenu components

--- examples/sites/demos/pc/app/tree-menu/accordion.spec.ts
+++ examples/sites/demos/pc/app/tree-menu/accordion.spec.ts
@@ -4,7 +4,9 @@ test('手风琴', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('tree-menu#accordion')
 
-  const treeMenu = page.locator('#preview .tiny-tree-menu')
+  const wrap = page.locator('#accordion')
+  const treeMenu = wrap.locator('.tiny-tree-menu')
+
   await treeMenu.getByTitle('指南').click()
   await expect(treeMenu.getByTitle('引入组件')).toBeVisible()
   await expect(treeMenu.getByTitle('构建部署')).toBeVisible()
--- examples/sites/demos/pc/app/tree-menu/empty-text.spec.ts
+++ examples/sites/demos/pc/app/tree-menu/empty-text.spec.ts
@@ -4,6 +4,6 @@ test('自定义空数据文本', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('tree-menu#empty-text')
 
-  const treeMenu = page.locator('#preview .tiny-tree-menu')
+  const treeMenu = page.locator('#empty-text .tiny-tree-menu')
   await expect(treeMenu.locator('.tiny-tree__empty-text')).toHaveText('空数据')
 })
