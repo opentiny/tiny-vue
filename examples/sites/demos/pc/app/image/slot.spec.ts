b4f097bd2627d8fc027fd2d07bd56920aa170662
shenjunjian <40288193@qq.com>
2023-12-06 22:25:58 -0800

    docs(image): add container/image's e2e test

--- examples/sites/demos/pc/app/image/slot.spec.ts
+++ examples/sites/demos/pc/app/image/slot.spec.ts
@@ -5,5 +5,8 @@ test('插槽', async ({ page }) => {
 
   await page.goto('image#slot')
   const errorSlot = page.locator('.tiny-image__error')
+  const customLost = page.getByText('图片已丢失')
+
   await expect(errorSlot).toBeVisible()
+  await expect(customLost).toBeVisible()
 })
