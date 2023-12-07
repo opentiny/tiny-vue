1b57fd5722cfce74c667468dd20e496b7c497da0
gimmyhehe <975402925@qq.com>
2023-12-07 20:23:18 +0800

    fix(form): fix from valid tooltip background color (#1059)

--- packages/theme/src/form/smb-theme.js
+++ packages/theme/src/form/smb-theme.js
@@ -1,4 +1,6 @@
 export const tinyFormSmbTheme = {
+  'ti-form-valid-error-bg-color': '#fff',
+  'ti-form-valid-error-border-color': '#fff',
   'ti-form-item-label-top-margin-bottom': '24px',
   'ti-form-item-label-top-label-line-height': '1.5', // smb要求行高为1.5
   'ti-form-item-label-top-label-height': 'calc(1.5em + 8px)' // border-box需要加上内边距
