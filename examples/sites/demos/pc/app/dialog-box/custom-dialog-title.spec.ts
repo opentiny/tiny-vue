f8d4c923565cd71bee127c12859fe4604ba7b0ba
James-9696 <hujm969696@163.com>
2023-12-08 00:33:28 -0800

    test[dialog-box,numeric]Update E2E TEST

--- examples/sites/demos/pc/app/dialog-box/custom-dialog-title.spec.ts
+++ examples/sites/demos/pc/app/dialog-box/custom-dialog-title.spec.ts
@@ -3,16 +3,7 @@ import { test, expect } from '@playwright/test'
 test('dialogBox 自定义标题', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('dialog-box#custom-dialog-title')
-
-  const preview = page.locator('#preview')
-  const button = preview.getByRole('button', { name: '弹出Dialog' })
-  const dialogBox = preview.locator('.tiny-dialog-box')
-  const header = dialogBox.locator('.tiny-dialog-box__header')
-  const customTitle = header.getByText('自定义title区域')
-  const defaultTitle = header.getByText('消息')
-
-  await button.click()
-  await expect(dialogBox).toBeVisible()
-  await expect(defaultTitle).toHaveCount(0)
-  await expect(customTitle).toBeVisible()
+  await page.getByRole('button', { name: '弹出Dialogfalse' }).click()
+  await expect(page.locator('.tiny-dialog-box__header > div')).toHaveText('自定义title区域')
+  await page.getByRole('button', { name: '确 定' }).click()
 })
--- examples/sites/demos/pc/app/dialog-box/draggable.spec.ts
+++ examples/sites/demos/pc/app/dialog-box/draggable.spec.ts
@@ -4,10 +4,9 @@ test('dialogBox 可拖拽的弹窗', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('dialog-box#draggable')
 
-  const preview = page.locator('#preview')
-  const button = preview.getByRole('button', { name: '可拖拽弹窗' })
+  const button = page.getByRole('button', { name: '可拖拽弹窗' })
   const notify = page.locator('.tiny-notify')
-  const dialogBox = preview.locator('.tiny-dialog-box')
+  const dialogBox = page.locator('.tiny-dialog-box')
   const title = dialogBox.locator('.tiny-dialog-box__title')
 
   await button.click()
@@ -19,9 +18,9 @@ test('dialogBox 可拖拽的弹窗', async ({ page }) => {
   // 开始拖拽
   await page.mouse.move(x + 2, y + 2)
   await page.mouse.down()
-  await page.mouse.move(x + 300, y + 300)
-  await expect(notify.filter({ hasText: /^拖拽开始$/ })).toBeVisible()
+  await page.mouse.move(x + 100, y + 100)
   await expect(notify.filter({ hasText: /^拖拽移动$/ })).toBeVisible()
+  await expect(notify.filter({ hasText: /^拖拽开始$/ })).toBeVisible()
   await page.mouse.up()
   await expect(notify.filter({ hasText: /^拖拽结束$/ })).toBeVisible()
 })
