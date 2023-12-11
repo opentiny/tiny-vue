b5867dd81f4dd0f7b24d8757bf006c10b4f25087
James <72028410+James-9696@users.noreply.github.com>
2023-12-11 18:57:27 +0800

    test:[dialog-box,numeric,modal] Add Or Update E2E TEST (#1095)
    * test[config-provider/user-head]Add E2E TEST
    
    * test[dialog-box,numeric]Update E2E TEST
    
    * test[dialog-box,numeric]Update E2E TEST
    
    * test(modal):[modal]update test E2E TEST

--- examples/sites/demos/pc/app/dialog-box/destroy-on-close.spec.ts
+++ examples/sites/demos/pc/app/dialog-box/destroy-on-close.spec.ts
@@ -2,13 +2,15 @@ import { test, expect } from '@playwright/test'
 
 test('关闭时销毁主体元素', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
+  const dialogBox = page.locator('.tiny-dialog-box')
   await page.goto('dialog-box#destroy-on-close')
-
-  const dialogBox = page.locator('#preview .tiny-dialog-box')
-
-  await page.getByRole('radio', { name: '关闭时销毁' }).click()
-  await expect(dialogBox).toHaveCount(0)
-
+  await page.getByRole('button', { name: '打开弹框' }).click()
+  await page.getByLabel('Close').click()
   await page.getByRole('radio', { name: '关闭时不销毁' }).click()
-  await expect(dialogBox).toHaveCount(1)
+  await page.getByRole('button', { name: '打开弹框' }).click()
+  await page.getByRole('button', { name: '确定' }).click()
+  await page.getByRole('radio', { name: '关闭时销毁' }).click()
+  await page.getByRole('button', { name: '打开弹框' }).click()
+  await page.getByRole('button', { name: '确定' }).click()
+  await expect(dialogBox).toBeHidden()
 })
--- examples/sites/demos/pc/app/dialog-box/form-in-dialog.spec.ts
+++ examples/sites/demos/pc/app/dialog-box/form-in-dialog.spec.ts
@@ -3,13 +3,14 @@ import { test, expect } from '@playwright/test'
 test('弹窗表单', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('dialog-box#form-in-dialog')
-
   const dialogBox = page.locator('.tiny-dialog-box')
   await page.getByRole('button', { name: '弹出表单' }).click()
   await expect(dialogBox.locator('.tiny-form')).toBeVisible()
-  await page.locator('#preview').getByRole('textbox').click()
-  await page.locator('#preview').getByRole('textbox').fill('name')
-  await page.getByText('消息').click()
+  await page.locator('#form-in-dialog').getByRole('textbox').click()
+  await page.locator('#form-in-dialog').getByRole('textbox').fill('123456')
   await page.locator('label').filter({ hasText: '全部' }).locator('span').nth(1).click()
+  await page.locator('label').filter({ hasText: '架构' }).locator('span').nth(1).click()
   await page.getByRole('button', { name: '确定' }).click()
+  await page.getByRole('button', { name: '弹出表单' }).click()
+  await expect(dialogBox.locator('.tiny-form')).toBeVisible()
 })
