71e767310d5afce5b48e88cc0c53db3175be8bca
申君健 <40288193@qq.com>
2023-12-11 14:55:10 +0800

    docs(tooltip): add tooltip's e2e test case (#1086)

--- examples/sites/demos/pc/app/tooltip/control.spec.js
+++ examples/sites/demos/pc/app/tooltip/control.spec.js
@@ -10,12 +10,30 @@ test('测试手动控制tooltip', async ({ page }) => {
   const disableSwitch = preview.locator('.tiny-switch').nth(2)
 
   const pop1 = page.getByText('智能提示的提示内容')
+  const pop2 = page.getByText('手动控制模式的提示内容')
+  const pop3 = page.getByText('禁用的提示内容')
 
+  // 测试visible
   await preview.getByText('内容不超长').hover()
   await expect(pop1).toBeVisible()
-  await page.waitForTimeout(50)
+  await page.waitForTimeout(20)
 
   await visibleSwitch.click()
   await preview.getByText('内容不超长').hover()
   await expect(pop1).toBeHidden()
+
+  await page.waitForTimeout(20)
+
+  // 测试manual
+  await expect(pop2).toBeHidden()
+  await manualSwitch.click()
+  await expect(pop2).toBeVisible()
+
+  await page.waitForTimeout(20)
+
+  // 测试禁用
+  await preview.getByText('我的内容很长很长').nth(2).hover()
+  await expect(pop3).toBeVisible()
+  await disableSwitch.click()
+  await expect(pop3).toBeHidden()
 })
