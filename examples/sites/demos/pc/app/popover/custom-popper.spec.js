6338e47220bbf2c0e490fb2436c12e26f0c6ac90
申君健 <40288193@qq.com>
2023-12-11 18:55:56 +0800

    docs(popover): [popover] add popover e2e test (#1096)
    * docs(popover): add popover e2e test
    
    * docs(popover): fix

--- examples/sites/demos/pc/app/popover/custom-popper.spec.js
+++ examples/sites/demos/pc/app/popover/custom-popper.spec.js
@@ -4,9 +4,30 @@ test('延迟隐藏', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('popover#custom-popper')
 
-  const button = page.getByRole('button', { name: '覆盖字体样式' })
-  const pop = page.locator('.custom-popover-demo-cls')
+  const button1 = page.getByRole('button', { name: '左侧位置弹出' })
+  const pop1 = page.getByRole('tooltip', { name: '我是一段内容' })
 
-  await button.hover()
-  await expect(pop).toBeVisible()
+  const button2 = page.getByRole('button', { name: '没有弹窗小箭头' })
+  const pop2 = page.getByRole('tooltip', { name: '我是一段内容' })
+
+  const button3 = page.getByRole('button', { name: '覆盖字体样式' })
+  const pop3 = page.locator('.custom-popover-demo-cls')
+
+  // 测试左侧弹出
+  await button1.hover()
+  await expect(pop1).toBeVisible()
+  let box1 = await button1.boundingBox()
+  let box2 = await pop1.boundingBox()
+  expect(box1.x > box2.x + box2.width)
+  await page.mouse.move(0, 0)
+  await page.waitForTimeout(20)
+
+  // 测试无箭头
+  await button2.hover()
+  await expect(pop2.locator('.popper__arrow')).toHaveCount(0)
+
+  // 测试字体
+  await button3.hover()
+  await expect(pop3).toBeVisible()
+  await expect(pop3).toHaveCSS('font-size', '16px')
 })
--- examples/sites/demos/pc/app/popover/offset.spec.js
+++ examples/sites/demos/pc/app/popover/offset.spec.js
@@ -4,7 +4,35 @@ test('箭头偏移', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('popover#offset')
 
-  // 默认偏移和50偏移
-  await expect(page.locator('.popper__arrow').nth(0)).toBeVisible()
-  await expect(page.locator('.popper__arrow').nth(1)).toBeVisible()
+  const preview = page.locator('.pc-demo-container')
+  const input1 = preview.locator('.tiny-slider__input input').nth(0)
+  const input2 = preview.locator('.tiny-slider__input input').nth(1)
+  const arrow = page.getByText('箭头偏移的内容').locator('.popper__arrow')
+  const panel = page.getByText('面板偏移的内容')
+
+  // 箭头偏移
+  await expect(arrow).toBeVisible()
+  let middle = await arrow.boundingBox()
+  await input1.fill('8')
+  await page.waitForTimeout(1000)
+  let left = await arrow.boundingBox()
+  await input1.fill('100')
+  await page.waitForTimeout(1000)
+  let right = await arrow.boundingBox()
+
+  expect(left.x < middle.x)
+  expect(middle.x < right.x)
+
+  // 面板偏移
+  await expect(panel).toBeVisible()
+  middle = await panel.boundingBox()
+  await input2.fill('-100')
+  await page.waitForTimeout(1000)
+  left = await panel.boundingBox()
+  await input2.fill('100')
+  await page.waitForTimeout(1000)
+  right = await panel.boundingBox()
+
+  expect(left.x < middle.x)
+  expect(middle.x < right.x)
 })
