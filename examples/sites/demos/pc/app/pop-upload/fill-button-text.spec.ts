1c875e159e3c614c72edfc69f73902d540df7419
chenxi-20 <2465950588@qq.com>
2023-12-11 01:31:33 -0800

    test(pop-upload): [pop-upload] Add e2e test cases

--- examples/sites/demos/pc/app/pop-upload/fill-button-text.spec.ts
+++ examples/sites/demos/pc/app/pop-upload/fill-button-text.spec.ts
@@ -1,39 +1,19 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('PopUpload 自定义按钮的文本', () => {
-  test('PopUpload 文件上传弹框中取消按钮的文本', async ({ page }) => {
-    page.on('pageerror', (exception) => expect(exception).toBeNull())
-    await page.goto('pop-upload#fill-button-text1')
+test('PopUpload 定义按钮和标题的文本', async ({ page }) => {
+  page.on('pageerror', (exception) => expect(exception).toBeNull())
+  await page.goto('pop-upload#fill-button-text')
 
-    const modalAppearBtn = page.getByRole('button', { name: '自定义按钮文本' })
-    const modal = page.locator('.tiny-modal')
-    const cancelBtn = modal.getByRole('button', { name: '自定义取消按钮' })
+  const modalAppearBtn = page.getByRole('button', { name: '自定义按钮文本' })
+  const modal = page.locator('.tiny-popupload__modal')
+  const cancelBtn = modal.getByRole('button', { name: '自定义取消按钮' })
+  const uploadsBtn = modal.getByRole('button', { name: '自定义提交按钮文本' })
+  const title = modal.getByRole('button', { name: '自定义提交按钮文本' })
+  const selectBtn = modal.getByRole('button', { name: '自定义按钮文本' })
 
-    await modalAppearBtn.click()
-    await expect(cancelBtn).toHaveText(/自定义取消按钮/)
-  })
-
-  test('PopUpload 文件上传弹框中提交按钮的文本', async ({ page }) => {
-    page.on('pageerror', (exception) => expect(exception).toBeNull())
-    await page.goto('pop-upload#fill-button-text2')
-
-    const modalAppearBtn = page.getByRole('button', { name: '自定义按钮文本' })
-    const modal = page.locator('.tiny-modal')
-    const uploadsBtn = modal.getByRole('button', { name: '自定义提交按钮文本' })
-
-    await modalAppearBtn.click()
-    await expect(uploadsBtn).toHaveText(/自定义提交按钮文本/)
-  })
-
-  test('PopUpload 打开弹出框的按钮的文本', async ({ page }) => {
-    page.on('pageerror', (exception) => expect(exception).toBeNull())
-    await page.goto('pop-upload#fill-button-text4')
-
-    const modalAppearBtn = page.getByRole('button', { name: '自定义按钮文本' })
-    const modal = page.locator('.tiny-modal')
-
-    await expect(modalAppearBtn).toHaveText(/自定义按钮文本/)
-    await modalAppearBtn.click()
-    await expect(modal).toBeVisible()
-  })
+  await modalAppearBtn.click()
+  await title.isVisible()
+  await cancelBtn.isVisible()
+  await uploadsBtn.isVisible()
+  await selectBtn.isVisible()
 })
