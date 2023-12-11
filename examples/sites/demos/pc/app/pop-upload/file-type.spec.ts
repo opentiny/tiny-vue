e20c7e84659b64ab0e8cac919ae550069964aace
chenxi-20 <76168465+chenxi-20@users.noreply.github.com>
2023-12-11 18:56:37 +0800

    fix(pop-upload): [pop-upload] Optimized the document and added e2e test cases. (#1079)
    * docs(pop-upload): [pop-upload] Optimization and supplementation of document examples
    
    * test(pop-upload): [pop-upload] Add e2e test cases

--- examples/sites/demos/pc/app/pop-upload/file-type.spec.ts
+++ examples/sites/demos/pc/app/pop-upload/file-type.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('PopUpload 可上传文件类型', async ({ page }) => {
+test('PopUpload 限制上传文件类型和大小', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('pop-upload#file-type')
 
-  const preview = page.locator('#preview')
+  const preview = page.locator('.all-demos-container')
   const modalAppearBtn = preview.getByRole('button', { name: '选择文件' })
-  const uploadModal = page.locator('.tiny-modal').nth(0)
+  const uploadModal = page.locator('.tiny-popupload__modal')
   const alert = uploadModal.locator('.tiny-alert')
   const selectFilesBtn = uploadModal.getByRole('button', { name: '选择文件' })
   const uploadsBtn = uploadModal.getByRole('button', { name: '开始上传' })
@@ -17,9 +17,12 @@ test('PopUpload 可上传文件类型', async ({ page }) => {
   const path3 = path.resolve(__dirname, '测试.svg')
 
   await modalAppearBtn.click()
+  await expect(alert.getByText('上传文件大小不超过:9KB')).toBeVisible()
   await expect(alert.getByText('上传文件类型限制为:.png,.jpg')).toBeVisible()
   const [fileChooser] = await Promise.all([page.waitForEvent('filechooser'), selectFilesBtn.click()])
   await fileChooser.setFiles(path1)
+  await uploadsBtn.click()
+  await expect(alert.getByText('测试.jpg:上传文件大小超出限制')).toBeVisible()
   await fileChooser.setFiles(path2)
   await fileChooser.setFiles(path3)
   await expect(lists).toHaveCount(3)
--- examples/sites/demos/pc/app/pop-upload/http-request.spec.ts
+++ examples/sites/demos/pc/app/pop-upload/http-request.spec.ts
@@ -4,10 +4,9 @@ test('PopUpload 覆盖默认请求', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('pop-upload#http-request')
 
-  const preview = page.locator('#preview')
+  const preview = page.locator('.all-demos-container')
   const modalAppearBtn = preview.getByRole('button', { name: '选择文件' })
-  const uploadModal = page.locator('.tiny-modal').nth(0)
-  const messageModal = page.locator('.tiny-modal').nth(1)
+  const uploadModal = page.locator('.tiny-popupload__modal')
   const selectFilesBtn = uploadModal.getByRole('button', { name: '选择文件' })
   const uploadsBtn = uploadModal.getByRole('button', { name: '开始上传' })
   const lists = uploadModal.locator('.tiny-popupload__dialog-table-item')
@@ -21,6 +20,5 @@ test('PopUpload 覆盖默认请求', async ({ page }) => {
 
   // 自定义上传行为
   await uploadsBtn.click()
-  await expect(messageModal).toBeVisible()
-  await expect(messageModal).toHaveText(/上传成功/)
+  await page.getByText('上传成功').isVisible()
 })
