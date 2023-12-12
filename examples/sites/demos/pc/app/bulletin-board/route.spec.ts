c0bd83bbcb0ec4114a2b8fc86e993cdc49c066da
wuyiping0628 <1106773985@qq.com>
2023-12-12 05:03:20 -0800

    test(bullentin-board):[bullentin-board] update bullentin-board E2E test

--- examples/sites/demos/pc/app/bulletin-board/route.spec.ts
+++ examples/sites/demos/pc/app/bulletin-board/route.spec.ts
@@ -4,7 +4,7 @@ test('BulletinBoard 基于Route的跳转(', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('bulletin-board#route')
 
-  const preview = page.locator('#preview')
+  const preview = page.locator('#route')
   const contentTitle = preview.locator('.tiny-bulletin-board__textTitle')
 
   // 判断a标签href属性值
--- examples/sites/demos/pc/app/bulletin-board/url.spec.ts
+++ examples/sites/demos/pc/app/bulletin-board/url.spec.ts
@@ -4,7 +4,7 @@ test('BulletinBoard 基于Url的跳转(', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('bulletin-board#url')
 
-  const preview = page.locator('#preview')
+  const preview = page.locator('#url')
   const contentTitle = preview.locator('.tiny-bulletin-board__textTitle')
 
   // a标签href属性值
