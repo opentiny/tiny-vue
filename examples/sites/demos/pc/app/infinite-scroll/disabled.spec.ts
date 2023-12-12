2a0cbb342779b2abccb81d05075b2c94ec6066e5
wuyiping0628 <1106773985@qq.com>
2023-12-12 00:11:51 -0800

    test(infinite-scroll):[infinite-scroll] update infinite-scroll E2E test

--- examples/sites/demos/pc/app/infinite-scroll/disabled.spec.ts
+++ examples/sites/demos/pc/app/infinite-scroll/disabled.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('基础用法', async ({ page }) => {
+test('禁用加载', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('infinite-scroll#disabled')
   const infiniteList = page.locator('#disabled .tiny-infinite-list-wrapper')
