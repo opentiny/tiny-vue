185eb8b84d748be1294e7a5b06b3c109cdf0bf17
wuyiping0628 <42107997+wuyiping0628@users.noreply.github.com>
2023-12-13 16:13:50 +0800

    test(text-popup):[text-popup] update text-popup E2E test (#1119)

--- examples/sites/demos/pc/app/text-popup/width.spec.ts
+++ examples/sites/demos/pc/app/text-popup/width.spec.ts
@@ -4,7 +4,7 @@ test('Textpopup 宽度', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('text-popup#width')
 
-  const preview = page.locator('#preview')
+  const preview = page.locator('#width')
   const textPopup = preview.locator('.tiny-text-popup')
 
   await expect(textPopup).toHaveCSS('width', '500px')
