56f40e19f63db08b23b28049032d4f0511ef6cfd
James-9696 <hujm969696@163.com>
2023-12-13 01:08:52 -0800

    test:[toggle-menu,slide-bar,link-menu,divider,user-contact]update test E2E TEST

--- examples/sites/demos/pc/app/toggle-menu/default-expand-all.spec.ts
+++ examples/sites/demos/pc/app/toggle-menu/default-expand-all.spec.ts
@@ -3,11 +3,9 @@ import { test, expect } from '@playwright/test'
 test('默认展开所有节点', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('toggle-menu#default-expand-all')
-  const preview = page.locator('#preview')
-  // 默认所有的节点都可见
-  await expect(preview.locator('.tiny-tree .tiny-tree-node:visible')).toHaveCount(14)
-  await page.getByTitle('开发指南').click()
-  await expect(preview.locator('.tiny-tree .tiny-tree-node:visible')).toHaveCount(11)
-  await page.getByTitle('表单组件').nth(1).click()
-  await expect(preview.locator('.tiny-tree .tiny-tree-node:visible')).toHaveCount(9)
+  // 关闭所有的节点
+  await page.getByRole('treeitem', { name: '开发指南' }).getByRole('img').click()
+  await page.getByRole('treeitem', { name: '表单组件', exact: true }).getByRole('img').click()
+  await page.getByRole('treeitem', { name: '数据组件', exact: true }).getByRole('img').click()
+  await page.getByRole('treeitem', { name: '其他组件', exact: true }).getByRole('img').click()
 })
