b65853e21796a13154c6cc4ea39c17b77e2cfe33
You-Hw-Y <1664677472@qq.com>
2023-12-18 23:30:43 -0800

    test(drop-tmies): [drop-times] amend drop-times e2e test

--- examples/sites/demos/pc/app/drop-times/size.spec.ts
+++ examples/sites/demos/pc/app/drop-times/size.spec.ts
@@ -4,6 +4,10 @@ test('设置组件尺寸', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('drop-times#size')
 
-  const droptimes = page.locator('.tiny-select')
+  const droptimes = page.locator('.drop-time-demo-size > div:nth-child(1)')
   await expect(droptimes).toHaveClass(/tiny-select--medium/)
+  const droptimesSmall = page.locator('.drop-time-demo-size > div:nth-child(2)')
+  await expect(droptimesSmall).toHaveClass(/tiny-select--small/)
+  const droptimesMini = page.locator('.drop-time-demo-size > div:nth-child(3)')
+  await expect(droptimesMini).toHaveClass(/tiny-select--mini/)
 })
