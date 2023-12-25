7ce48591528943e9c9f2adf23c0aa3a6be3de032
You-Hw-Y <1664677472@qq.com>
2023-12-25 01:18:25 -0800

    test(calendar): [calendar] amend e2e test

--- examples/sites/demos/pc/app/calendar/dynamic-add-schedule.spec.ts
+++ examples/sites/demos/pc/app/calendar/dynamic-add-schedule.spec.ts
@@ -6,16 +6,17 @@ test('添加日程事件', async ({ page }) => {
   const selectedDay4 = page.getByRole('cell', { name: '4 通知事项 通知事项A' }).getByRole('listitem').first()
   const dayFun4 = page.getByRole('tooltip', { name: '2019-9-4 19:42 通知事项 请注意该通知事项' })
   const selectedDay13 = page.getByRole('cell', { name: '13 这是一条警告' }).getByRole('listitem')
-  const dayFun13 = page.getByText('这是一条警告').nth(2)
+  const dayFun13 = page.getByRole('tooltip', { name: '2019-9-13 9:0 这是一条警告 这是一条警告消息这是一条警告' })
   const selectedDay14 = page.getByRole('listitem').filter({ hasText: '这是一条错误' })
-  const datFun14 = page.getByText('这是一条错误').nth(2)
+  const datFun14 = page.getByRole('tooltip', { name: '2019-9-14 9:0 这是一条错误 这是一条错误，还有错误' })
 
+  await page.waitForTimeout(2000)
   await selectedDay4.hover()
-  await expect(dayFun4).not.toBeDisabled()
+  await expect(dayFun4).toBeVisible()
 
   await selectedDay13.hover()
-  await expect(dayFun13).not.toBeDisabled()
+  await expect(dayFun13).toBeVisible()
 
   await selectedDay14.hover()
-  await expect(datFun14).not.toBeDisabled()
+  await expect(datFun14).toBeVisible()
 })
