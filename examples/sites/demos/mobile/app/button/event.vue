4116bd232ddb93705b33819fd0b33bbdd6473ad3
jacknan <zhumaonan@aliyun.com>
2023-12-25 14:46:11 +0800

    feat(button): refresh button ui (#1171)
    * feat(button): 刷新移动端设计
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(button): 刷新移动端设计
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(button): 刷新demo
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(button): 删除无用文件
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(button): 优化样式
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * fix(pull-refresh): 去除影响pc模式的修改
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    * feat(button): 优化样式
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>
    
    ---------
    
    Signed-off-by: jacknan <zhumaonan@aliyun.com>

--- examples/sites/demos/mobile/app/button/event.vue
+++ examples/sites/demos/mobile/app/button/event.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="button-wrap">
-    <h3>按钮小尺寸</h3>
-    <tiny-button type="primary" size="small">主要按钮</tiny-button>
-    <tiny-button type="secondary" size="small">次要按钮</tiny-button>
-    <tiny-button type="danger" size="small">危险按钮</tiny-button>
-    <tiny-button type="text" size="small">文字按钮</tiny-button>
-    <tiny-button type="icon" :icon="IconMail" size="small"></tiny-button>
-    <tiny-button type="icon" :icon="IconMail" text="图标按钮" size="small"></tiny-button>
-    <tiny-button type="primary" loading size="small">主要按钮-加载</tiny-button>
+    <p>点击事件</p>
     <tiny-button type="secondary" :loading="loading" @click="btnClick" size="small">点我加载</tiny-button>
   </div>
 </template>
--- examples/sites/demos/mobile/app/button/reset-time.vue
+++ examples/sites/demos/mobile/app/button/reset-time.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="button-wrap">
-    <tiny-button round>默认1秒</tiny-button>
-    <tiny-button type="primary" round :reset-time="2000">禁用2秒</tiny-button>
-    <tiny-button type="success" round :reset-time="5000">禁用5秒</tiny-button>
-    <tiny-button round>圆角按钮</tiny-button>
-    <tiny-button type="primary" round disabled>主要按钮</tiny-button>
-    <tiny-button type="success" round disabled>成功按钮</tiny-button>
-    <tiny-button type="info" round disabled>信息按钮</tiny-button>
-    <tiny-button type="warning" round disabled>警告按钮</tiny-button>
-    <tiny-button type="danger" round disabled>危险按钮</tiny-button>
-    <tiny-button type="secondary" round disabled>次要按钮</tiny-button>
-    <tiny-button round disabled>弱按钮</tiny-button>
+    <p>按钮类型</p>
+    <tiny-button type="primary" reset-time="500">重置时间</tiny-button>
   </div>
 </template>
 
-<script lang="jsx">
+<script>
 import { Button } from '@opentiny/vue'
 
 export default {
   components: {
     TinyButton: Button
-  },
-  data() {
-    return {}
   }
 }
 </script>
 
 <style scoped>
+body {
+  overflow: hidden;
+}
 .button-wrap {
-  padding: 10px 20px;
+  padding: 0 10px;
+  overflow-y: scroll;
+  height: 100%;
 }
 .button-wrap .tiny-mobile-button {
   margin-right: 16px;
