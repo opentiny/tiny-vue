276393123f3909d20739b8ebcbbbdcdb82a0ca4a
You-Hw-Y <1664677472@qq.com>
2023-12-25 02:23:18 -0800

    test(calendar): [calendar] amend e2e test

--- examples/sites/demos/pc/app/calendar/dynamic-add-schedule.spec.ts
+++ examples/sites/demos/pc/app/calendar/dynamic-add-schedule.spec.ts
@@ -3,12 +3,14 @@ import { test, expect } from '@playwright/test'
 test('添加日程事件', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('calendar#dynamic-add-schedule')
-  const selectedDay4 = page.getByRole('cell', { name: '4 通知事项 通知事项A' }).getByRole('listitem').first()
-  const dayFun4 = page.getByRole('tooltip', { name: '2019-9-4 19:42 通知事项 请注意该通知事项' })
-  const selectedDay13 = page.getByRole('cell', { name: '13 这是一条警告' }).getByRole('listitem')
-  const dayFun13 = page.getByRole('tooltip', { name: '2019-9-13 9:0 这是一条警告 这是一条警告消息这是一条警告' })
+  const selectedDay4 = page.getByText('通知事项通知事项A', { exact: true })
+  const dayFun4 = page.locator('.tiny-calendar__tip-content').filter({ hasText: '请注意该通知事项A' })
+
+  const selectedDay13 = page.getByRole('listitem').filter({ hasText: '这是一条警告' })
+  const dayFun13 = page.locator('.tiny-calendar__tip-content').filter({ hasText: '这是一条警告消息这是一条警告' })
+
   const selectedDay14 = page.getByRole('listitem').filter({ hasText: '这是一条错误' })
-  const datFun14 = page.getByRole('tooltip', { name: '2019-9-14 9:0 这是一条错误 这是一条错误，还有错误' })
+  const datFun14 = page.locator('.tiny-calendar__tip-content').filter({ hasText: '这是一条错误，还有错误' })
 
   await selectedDay4.hover()
   await page.waitForTimeout(200)
