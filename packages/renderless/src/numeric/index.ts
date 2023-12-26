3489934ff99d6ab86ad71e3c0af80b2c0b518a43
张烟铃 <2581932570@qq.com>
2023-12-26 11:57:12 +0800

    fix(components):[numeric]+ change to Number

--- packages/renderless/src/numeric/index.ts
+++ packages/renderless/src/numeric/index.ts
@@ -341,7 +341,7 @@ export const handleInputChange =
     const value = event.target?.value === '-' ? 0 : event.target?.value
     if (props.stepStrictly) {
         const previousValue = Number((props.mouseWheel ? state.displayValue : props.modelValue) || 0)
-        if (Math.abs(previousValue - value) % +props.step === 0) return api.setCurrentValue(value)
+        if (Math.abs(previousValue - value) %  Number(props.step) === 0) return api.setCurrentValue(value)
 
         if (previousValue < 0 || value < 0)
             return api.setCurrentValue(
