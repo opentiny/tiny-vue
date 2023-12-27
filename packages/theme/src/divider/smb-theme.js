1244b51f89cbbfd764659187ce042d86585d1c11
hwx1309243 <hujm969696@163.com>
2023-12-27 02:33:18 -0800

    fix(theme): update divider smb theme

---
+++ packages/theme/src/divider/smb-theme.js
@@ -0,0 +1,7 @@
+export const tinyDialogSelectAuroraTheme = {
+  'ti-divider-dividing-line': 'var(--ti-common-color-dark)',
+  'ti-divider-horizontal-opacity': 0.5,
+  'ti-divider-vertical-opacity': 0.5,
+  'ti-divider-text-color': 'var(--ti-common-color-bg-dark-text)',
+  'ti-divider-text-bg-color': 'var(--ti-base-color-white)'
+}
--- packages/theme/src/transfer/smb-theme.js
+++ packages/theme/src/transfer/smb-theme.js
@@ -1,9 +1,9 @@
 export const tinyTransferSmbTheme = {
   // transfer-SMB主题
-  'ti-transfer-panel-item-text-color': 'var(--ti-common-color-info-normal, #191919)',
-  'ti-transfer-button-bg-color': 'var(--ti-common-color-bg-light-normal, #1476ff)',
+  'ti-transfer-panel-item-text-color': 'var(--ti-common-color-info-normal)',
+  'ti-transfer-button-bg-color': 'var(--ti-common-color-prompt)',
   'ti-transfer-font-size': 'var(--ti-common-font-size-1)',
-  'ti-transfer-button-border-radius': 'var(--ti-common-border-radius-normal, 6px)',
-  'ti-transfer-panel-border-color': 'var(--ti-common-color-line-normal, #dbdbdb)',
-  'ti-transfer-panel-item-hover-bg-color': 'var(--ti-common-color-hover-background, #f5f5f5)'
+  'ti-transfer-button-border-radius': 'var(--ti-common-space-6)',
+  'ti-transfer-panel-border-color': 'var(--ti-common-color-line-disabled)',
+  'ti-transfer-panel-item-hover-bg-color': 'var(--ti-common-color-hover-background)'
 }
