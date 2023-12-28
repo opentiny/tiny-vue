3986c1f86951a252f1513162d016bedb93430ec1
hwx1309243 <hujm969696@163.com>
2023-12-27 22:59:20 -0800

    fix: update theme

--- packages/theme/src/divider/smb-theme.js
+++ packages/theme/src/divider/smb-theme.js
@@ -2,6 +2,6 @@ export const tinyDialogSelectAuroraTheme = {
   'ti-divider-dividing-line': 'var(--ti-common-color-dark)',
   'ti-divider-horizontal-opacity': 0.5,
   'ti-divider-vertical-opacity': 0.5,
-  'ti-divider-text-color': 'var(--ti-common-color-bg-dark-text)',
+  'ti-divider-text-color': 'var(--ti-common-color-text-primary)',
   'ti-divider-text-bg-color': 'var(--ti-base-color-white)'
 }
