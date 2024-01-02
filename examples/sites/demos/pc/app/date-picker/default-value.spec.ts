d52e03f6cb4c48b05a8be61d2b391c42b70c1f12
Kagol <kagol@sina.com>
2024-01-02 19:57:43 +0800

    test(date-picker): [date-picker] optimize date-picker e2e test (#1076)

--- examples/sites/demos/pc/app/date-picker/default-value.spec.ts
+++ examples/sites/demos/pc/app/date-picker/default-value.spec.ts
@@ -3,9 +3,24 @@ import { test, expect } from '@playwright/test'
 test('[DatePicker] 测试选择器打开时默认时间设置', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('date-picker#default-value')
-  const dateInput = page.getByRole('textbox', { name: '2000-11-11' })
 
-  await page.getByRole('textbox', { name: '2000-11-10' }).click()
-  await page.getByText('11', { exact: true }).click()
-  await expect(dateInput).toBeVisible()
+  // default-value: 打开日期面板，默认显示的日期
+  await page.getByRole('textbox').nth(1).click()
+  await expect(page.getByRole('button', { name: '2000 年' })).toBeVisible()
+  await expect(page.getByRole('button', { name: '11 月' })).toBeVisible()
+
+  // default-time: 日期时间(范围)，选择日期之后默认显示的时间(范围)
+  const dateInputDefaultTime = page.getByRole('textbox').nth(2)
+  await dateInputDefaultTime.fill('2023-05-20 09:00:00')
+  await dateInputDefaultTime.press('Enter')
+
+  await page.getByRole('textbox', { name: '2023-05-20 09:00:00' }).click()
+  await page.getByRole('cell', { name: '15' }).getByText('15').click()
+  await expect(page.getByRole('textbox', { name: '选择时间' })).toHaveValue('09:00:00')
+
+  await page.getByRole('textbox').nth(3).click()
+  await page.getByRole('cell', { name: '10' }).getByText('10').first().click()
+  await page.getByRole('cell', { name: '10' }).getByText('10').nth(1).click()
+  await expect(page.getByRole('textbox', { name: '开始时间' })).toHaveValue('09:00:00')
+  await expect(page.getByRole('textbox', { name: '结束时间' })).toHaveValue('18:00:00')
 })
--- examples/sites/demos/pc/app/date-picker/events.spec.ts
+++ examples/sites/demos/pc/app/date-picker/events.spec.ts
@@ -4,20 +4,34 @@ test('[DatePicker] 测试事件', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('date-picker#events')
 
+  // focus 事件
+  await page.locator('input[type="text"]').nth(1).click()
+  const focusEventMessageDom = page.locator('div').filter({ hasText: '触发 focus 事件' }).nth(1)
+  await expect(focusEventMessageDom).toBeVisible()
+
   // blur 事件
-  await page.getByRole('textbox').nth(1).click()
-  await page.getByText('blur：').click()
-  const blurEventMessageDom = page.locator('div').filter({ hasText: '可获取组件实例' }).nth(1)
+  const dateInputBlur = page.locator('input[type="text"]').nth(2)
+  await dateInputBlur.fill('2023-05-20')
+  await dateInputBlur.press('Enter')
+
+  await page.getByRole('textbox', { name: '2023-05-20' }).click()
+  await page.getByRole('cell', { name: '15' }).getByText('15').click()
+  const blurEventMessageDom = page.locator('div').filter({ hasText: '触发 blur 事件' }).nth(1)
   await expect(blurEventMessageDom).toBeVisible()
 
   // change 事件
-  await page.getByRole('textbox').nth(2).click()
-  await page.getByRole('cell', { name: '20' }).getByText('20').last().click()
-  const changeEventMessageDom = page.locator('div').filter({ hasText: '组件绑定值' }).nth(1)
+  const dateInputChange = page.locator('input[type="text"]').nth(3)
+  await dateInputChange.fill('2023-05-20')
+  await dateInputChange.press('Enter')
+
+  await page.getByRole('textbox', { name: '2023-05-20' }).click()
+  await page.getByRole('cell', { name: '15' }).getByText('15').click()
+  const changeEventMessageDom = page.locator('div').filter({ hasText: '触发 change 事件，组件绑定值为：' }).nth(1)
   await expect(changeEventMessageDom).toBeVisible()
 
-  // focus 事件
-  await page.getByRole('textbox').nth(3).click()
-  const focusEventMessageDom = page.locator('div').filter({ hasText: '回调参数为组件实例' }).nth(1)
-  await expect(focusEventMessageDom).toBeVisible()
+  // onPick 事件
+  await page.locator('#events').getByRole('textbox').nth(3).click()
+  await page.getByRole('cell', { name: '10' }).getByText('10').first().click()
+  const onPickEventMessageDom = page.locator('div').filter({ hasText: '触发 onPick 事件，开始日期为：' }).nth(1)
+  await expect(onPickEventMessageDom).toBeVisible()
 })
