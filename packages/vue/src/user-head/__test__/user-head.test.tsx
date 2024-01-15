6db36200ce88092a56bfaec77c32103195ea26f3
Floyd <59243597+Floyd-bit@users.noreply.github.com>
2024-01-15 16:43:25 +0800

    test(user-head): [user-head] improve unit testing of components (#1296)

--- packages/vue/src/user-head/__test__/user-head.test.tsx
+++ packages/vue/src/user-head/__test__/user-head.test.tsx
@@ -2,6 +2,7 @@ import { mountPcMode } from '@opentiny-internal/vue-test-utils'
 import { nextTick } from 'vue'
 import { describe, expect, test } from 'vitest'
 import UserHead from '@opentiny/vue-user-head'
+import { iconSmile } from '@opentiny/vue-icon'
 
 describe('PC Mode', () => {
   const mount = mountPcMode
@@ -14,22 +15,66 @@ describe('PC Mode', () => {
     const userHeadEl = wrapper.find('.tiny-user-head__portrait').element as HTMLElement
     expect(userHeadEl.style.getPropertyValue('background-color')).toBe('rgb(194, 194, 194)')
   })
-  test.todo('color 设置文字颜色')
 
-  test.todo('message-total 设置消息计数')
+  test('color', async () => {
+    const wrapper = mount(() => <UserHead color="rgb(194, 194, 194)"></UserHead>)
+    expect(wrapper.find('.tiny-user-head__portrait').attributes().style).contains('color: rgb(194, 194, 194)')
+  })
 
-  test.todo('message-type 规定消息类型 details|basic 可选 。;该属性的可选值为 details / basic')
+  test('message-total', async () => {
+    const wrapper = mount(() => <UserHead message-total={100}></UserHead>)
+    expect(wrapper.find('.tiny-user-head__message').text()).toEqual('100')
+  })
 
-  test.todo('message-upper-limit 设置消息显示上限')
+  test('message-type', async () => {
+    const wrapper = mount(() => <UserHead message-total={100} message-type="basic"></UserHead>)
+    expect(wrapper.find('.tiny-user-head__message').classes()).toContain('basic')
+  })
 
-  test.todo('min 设置小尺寸样式')
+  test('message-upper-limit', async () => {
+    // 消息总数大于上限
+    const overLimitWrapper = mount(() => <UserHead message-total={100} message-upper-limit={99}></UserHead>)
+    expect(overLimitWrapper.find('.tiny-user-head__message').text()).toBe('99+')
+    // 消息总数小于上限
+    const underLimitWrapper = mount(() => <UserHead message-total={50} message-upper-limit={99}></UserHead>)
+    expect(underLimitWrapper.find('.tiny-user-head__message').text()).toBe('50')
+  })
 
-  test.todo('round 设置圆形模式')
+  test('min', async () => {
+    const wrapper = mount(() => <UserHead min></UserHead>)
+    expect(wrapper.find('.tiny-user-head__portrait').classes()).toContain('min')
+  })
 
-  test.todo('type 设置头像类型，icon|image|label 可选。;该属性的可选值为 icon / image / label')
+  test('round', async () => {
+    const wrapper = mount(() => <UserHead round></UserHead>)
+    expect(wrapper.find('.tiny-user-head__portrait').classes()).toContain('round')
+  })
 
-  test.todo('value 设置type=icon 时为图标类名，type=label时为字体串，type=image时为资源路径')
+  // type 设置头像类型，icon|image|label 可选, 该属性的可选值为 icon / image / label
+  test.each([
+    ['icon', 'icon type should have the proper class'],
+    ['image', 'image type should have the proper class'],
+    ['label', 'label type should have the proper class']
+  ])('type %s', async (type, _description) => {
+    const wrapper = mount(() => <UserHead type={type}></UserHead>)
+    expect(wrapper.find('.tiny-user-head__portrait').classes()).toContain(type)
+  })
 
+  // value 设置type=icon 时为图标类名，type=label时为字体串，type=image时为资源路径
+  test('value', async () => {
+    const tinyIconSmile = iconSmile()
+    const iconWrapper = mount(() => <UserHead type="icon" modelValue={tinyIconSmile}></UserHead>)
+    expect(iconWrapper.find('.tiny-svg').exists()).toBeTruthy()
+
+    const labelWrapper = mount(() => <UserHead type="label" modelValue="文字"></UserHead>)
+    expect(labelWrapper.find('span').exists()).toBeTruthy()
+    expect(labelWrapper.find('span').text()).toBe('文字')
+
+    const imageWrapper = mount(() => <UserHead type="image" modelValue="/static/images/mountain.png"></UserHead>)
+    expect(imageWrapper.find('.tiny-user-head__portrait').attributes().style).contains(
+      'background-image: url(/static/images/mountain.png)'
+    )
+  })
   /**
    * slots
    */
