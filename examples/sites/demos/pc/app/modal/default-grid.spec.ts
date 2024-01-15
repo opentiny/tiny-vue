08f3186f42f6a6fcfbf35f35b30f37635054efec
hwx1309243 <hujm969696@163.com>
2024-01-14 18:52:24 -0800

    fix(dialog-box/modal): tiny-docs

--- examples/sites/demos/pc/app/modal/default-grid.spec.ts
+++ examples/sites/demos/pc/app/modal/default-grid.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test'
 
 test('嵌套grid', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('modal#grids-default')
+  await page.goto('modal#default-grid')
 
   const modal = page.locator('.tiny-modal')
   const grid = modal.locator('.tiny-grid ').first()
-  await page.getByRole('button', { name: '默认插槽' }).click()
+  await page.getByRole('button', { name: /默认插槽/ }).click()
   await expect(grid).toBeVisible()
 })
