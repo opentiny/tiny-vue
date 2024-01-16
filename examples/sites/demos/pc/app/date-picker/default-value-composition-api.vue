f04c7afed7a13b207f01372015c0fc8b46ffa1bd
Kagol <kagol@sina.com>
2024-01-16 15:32:43 +0800

    docs(date-picker): [date-picker] optimize api docs

--- examples/sites/demos/pc/app/date-picker/default-value-composition-api.vue
+++ examples/sites/demos/pc/app/date-picker/default-value-composition-api.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <p>打开日期面板，默认显示的日期</p>
+    <p>打开日期面板，默认显示的日期：</p>
     <div class="demo-date-picker-wrap">
       <tiny-date-picker v-model="value" :default-value="defaultDate"></tiny-date-picker>
     </div>
-    <p>日期时间(范围)，选择日期之后默认显示的时间(范围)</p>
+    <p>日期时间(范围)，选择日期之后默认显示的时间(范围)：</p>
     <div class="demo-date-picker-wrap">
       <tiny-date-picker v-model="dateTimeValue" type="datetime" :default-time="defaultTime"></tiny-date-picker>
       <tiny-date-picker
