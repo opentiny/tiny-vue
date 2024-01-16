9f996556de343dd7ba7ebdac23dc805d9f974ab9
gxuud <igaoxian@foxmail.com>
2024-01-16 23:38:33 +0800

    styles(area): [area] add area types

--- packages/renderless/src/area/vue.ts
+++ packages/renderless/src/area/vue.ts
@@ -11,14 +11,25 @@
  */
 
 import { getRegion, changeOffice, getRep, getOffice, fetchDefaultData, beforeMount } from './index'
+import type {
+  IAreaProps,
+  IAreaApi,
+  IAreaState,
+  ISharedRenderlessParamHooks,
+  ISharedRenderlessParamUtils
+} from '@/types'
 
 export const api = ['state', 'getRep', 'getRegion', 'getOffice', 'changeOffice']
 
-export const renderless = (props, { onBeforeMount, reactive }, { emit, service, refs, nextTick }) => {
-  const api = {}
+export const renderless = (
+  props: IAreaProps,
+  { onBeforeMount, reactive }: ISharedRenderlessParamHooks,
+  { emit, service, refs, nextTick }: ISharedRenderlessParamUtils<never>
+) => {
+  const api: Partial<IAreaApi> = {}
   const { fetchArea } = service || {}
 
-  const state = reactive({
+  const state = reactive<IAreaState>({
     jcr: '',
     region: '',
     rep: '',
@@ -32,14 +43,14 @@ export const renderless = (props, { onBeforeMount, reactive }, { emit, service, 
   Object.assign(api, {
     state,
     changeOffice: changeOffice({ emit, state }),
-    beforeMount: beforeMount({ api, props }),
+    beforeMount: beforeMount({ api: api as IAreaApi, props }),
     getRep: getRep({ emit, fetchArea, nextTick, props, refs, state }),
     getRegion: getRegion({ emit, fetchArea, nextTick, props, refs, state }),
     getOffice: getOffice({ emit, fetchArea, nextTick, props, refs, state }),
     fetchDefaultData: fetchDefaultData({ emit, fetchArea, nextTick, props, refs, state })
   })
 
-  onBeforeMount(api.beforeMount)
+  onBeforeMount((api as IAreaApi).beforeMount)
 
-  return api
+  return api as IAreaApi
 }
