3d1b00a7a8d47fd690e4ad7fbbb28d8fe4de407c
wNing50 <814661179@qq.com>
2024-01-18 19:33:08 +0800

    fix(dialog-box): [dialog-box] cancel E2E commit

--- examples/sites/demos/pc/app/dialog-box/center.spec.ts
+++ examples/sites/demos/pc/app/dialog-box/center.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('dialogBox 头部和底部水平居中', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('dialog-box#center')
-  await page.getByRole('button', { name: '弹出Dialogfalse' }).click()
+  await page.getByRole('button', { name: '弹出 Dialogfalse' }).click()
   await page.getByText('消息').click()
   // 底部水平居中
   await expect(page.locator('.tiny-dialog-box.is-center > .tiny-dialog-box__header')).toHaveCSS('text-align', 'center')
