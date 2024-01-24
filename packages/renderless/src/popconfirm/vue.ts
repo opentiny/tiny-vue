ef4c52248314657181ff7e73c145b58b37255b2c
Floyd <59243597+Floyd-bit@users.noreply.github.com>
2024-01-24 12:20:30 +0800

    style(popconfirm): [popconfirm] improve typescript declaration of components (#1325)

--- packages/renderless/src/popconfirm/vue.ts
+++ packages/renderless/src/popconfirm/vue.ts
@@ -1,15 +1,25 @@
+import type {
+  IPopconfirmApi,
+  IPopconfirmProps,
+  IPopconfirmRenderlessParamUtils,
+  ISharedRenderlessParamHooks
+} from '@/types'
 import { show, hide, confirm, handleEmit } from './index'
 
 export const api = ['state', 'show', 'hide', 'confirm', 'handleEmit']
 
-export const renderless = (props, { computed, reactive }, { emit, constants, designConfig, vm }) => {
-  const api = {}
-  const designIcon = designConfig?.icons?.[props.type]
+export const renderless = (
+  props: IPopconfirmProps,
+  { computed, reactive }: ISharedRenderlessParamHooks,
+  { emit, constants, designConfig, vm }: IPopconfirmRenderlessParamUtils
+): IPopconfirmApi => {
+  const api = {} as IPopconfirmApi
+  const designIcon = designConfig?.icons?.[props.type as string]
   const state = reactive({
     isLock: false,
     showPopover: false,
     getIcon: computed(() =>
-      typeof props.type === 'object' ? props.type : designIcon || constants.ICON_MAP[props.type]
+      typeof props.type === 'object' ? props.type : designIcon || constants.ICON_MAP[props.type as string]
     )
   })
 
---
+++ packages/renderless/types/popconfirm.type.ts
@@ -0,0 +1,30 @@
+import type { ExtractPropTypes } from 'vue'
+import type { popconfirmProps, $constants } from '@/popconfirm/src'
+import type { ISharedRenderlessFunctionParams, ISharedRenderlessParamUtils } from './shared.type'
+import type { show, hide, confirm, handleEmit } from '../src/popconfirm'
+
+export interface IPopconfirmState {
+  isLock: boolean
+  showPopover: boolean
+  getIcon: string
+}
+
+export type IPopconfirmProps = ExtractPropTypes<typeof popconfirmProps>
+
+export type IPopconfirmConstants = typeof $constants
+
+export type IPopconfirmRenderlessParams = ISharedRenderlessFunctionParams<IPopconfirmConstants> & {
+  api: IPopconfirmApi
+  state: IPopconfirmState
+  props: IPopconfirmProps
+}
+
+export interface IPopconfirmApi {
+  state: IPopconfirmState
+  show: ReturnType<typeof show>
+  hide: ReturnType<typeof hide>
+  confirm: ReturnType<typeof confirm>
+  handleEmit: ReturnType<typeof handleEmit>
+}
+
+export type IPopconfirmRenderlessParamUtils = ISharedRenderlessParamUtils<IPopconfirmConstants>
