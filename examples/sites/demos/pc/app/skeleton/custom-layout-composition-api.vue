47c6f176c278f2fcf68c26aa46faea3b261e72ca
xiaoy <1003346758@qq.com>
2024-02-02 15:27:14 +0800

    feat(skeleton): skeleton component (#1345)
    * feat(skeleton): skeleton component
    
    * fix(skeleton): skeleton css style

---
+++ examples/sites/demos/pc/app/skeleton/custom-layout-composition-api.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <tiny-skeleton>
+      <template #placeholder>
+        <tiny-skeleton-item variant="image" style="width: 200px"></tiny-skeleton-item>
+        <br /><br />
+        <tiny-skeleton-item variant="circle" size="small"></tiny-skeleton-item>
+      </template>
+    </tiny-skeleton>
+  </div>
+</template>
+
+<script setup>
+import { Skeleton as TinySkeleton, SkeletonItem as TinySkeletonItem } from '@opentiny/vue'
+</script>
