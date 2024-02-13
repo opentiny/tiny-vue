cf8c0878a4afa9b4af78ad11a0fb776df8f59c44
zhangyanling <2581932570@qq.com>
2024-02-13 17:15:23 +0800

    feat(float-button): [float-button] add float-button

---
+++ examples/sites/demos/pc/app/float-button/icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="parent">
+    <tiny-float-button bottom="-15px" right="100px" trigger="" :icon="IconDel"> </tiny-float-button>
+  </div>
+</template>
+
+<script>
+import { FloatButton as TinyFloatButton } from '@opentiny/vue'
+import { iconDel } from '@opentiny/vue-icon'
+
+export default {
+  components: {
+    TinyFloatButton
+  },
+  data() {
+    return {
+      IconDel: iconDel()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.parent {
+  position: relative;
+}
+
+.tiny-float-button {
+  position: absolute;
+}
+</style>
