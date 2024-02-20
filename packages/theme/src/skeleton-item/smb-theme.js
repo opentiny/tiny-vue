6fd3a0a83376629e385c5946dfc5cecf9dafea18
hwx1309243 <hujm969696@163.com>
2024-02-19 23:22:34 -0800

    fix: add smb-theme

---
+++ packages/theme/src/skeleton-item/smb-theme.js
@@ -0,0 +1,5 @@
+export const tinySkeletonItemSmbTheme = {
+  'ti-skeleton-item-bg-color': 'var(--ti-common-color-bg-disabled)',
+  'ti-skeleton-item-border-radius': '2px',
+  'ti-skeleton-item-image-icon-color': 'var(--ti-common-color-bg-disabled)'
+}
---
+++ packages/theme/src/skeleton/smb-theme.js
@@ -0,0 +1,5 @@
+export const tinySkeletonSmbTheme = {
+  'ti-skeleton-title-width': '20%',
+  'ti-skeleton-title-height': 'var(--ti-common-space-6x)',
+  'ti-skeleton-title-margin-bottom': 'var(--ti-common-space-6x)'
+}
