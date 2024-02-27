1019e574e2f609f1ce694f3455a34dfadb04bae3
张烟铃 <84823288+fanbingbing16@users.noreply.github.com>
2024-02-27 18:49:06 +0800

    feat(float-button): [float-button] add float-button (#1394)
    * feat(float-button): [float-button] add float-button
    
    * fix(float-button):[float-button] remove mobile
    
    * delete token.ts

---
+++ examples/sites/demos/pc/app/float-button/basic-usage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="parent">
+    <tiny-float-button :icon="IconSearch" circle bottom="20px"></tiny-float-button>
+    <tiny-float-button
+      type="primary"
+      :icon="IconEdit"
+      circle
+      bottom="20px"
+      right="60px"
+      shape="square"
+    ></tiny-float-button>
+
+    <tiny-float-button type="info" :icon="IconMail" circle bottom="20px" right="100px "></tiny-float-button>
+    <tiny-float-button type="warning" :icon="IconStarO" circle bottom="20px" right="140px"></tiny-float-button>
+    <tiny-float-button type="danger" :icon="IconDel" circle bottom="20px" right="180px"></tiny-float-button>
+  </div>
+</template>
+
+<script>
+import { FloatButton } from '@opentiny/vue'
+import { IconDel, IconEdit, IconMail, IconStarO, IconSearch } from '@opentiny/vue-icon'
+
+export default {
+  components: {
+    TinyFloatButton: FloatButton
+  },
+  data() {
+    return {
+      IconDel: IconDel(),
+      IconEdit: IconEdit(),
+      IconMail: IconMail(),
+      IconStarO: IconStarO(),
+      IconSearch: IconSearch()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.parent {
+  position: relative;
+  height: 100px;
+}
+
+.tiny-float-button {
+  position: absolute;
+}
+</style>
