943830df5b3546204a470e628a6f26ac8d2eece4
Kagol <kagol@sina.com>
2024-02-28 16:37:39 +0800

    feat(date-picker): quarter-panel support prevYear/nextYear/pickYear

--- packages/renderless/src/quarter-panel/index.ts
+++ packages/renderless/src/quarter-panel/index.ts
@@ -1,3 +1,6 @@
+import { modifyDate, nextYear, prevYear } from '../common/deps/date-util'
+import { DATEPICKER } from '../common'
+
 const QUARTER_MAP = {
   0: 0,
   1: 3,
@@ -6,8 +9,46 @@ const QUARTER_MAP = {
 }
 
 export const handleQuarterTableClick =
-  ({ emit }) =>
+  ({ state, emit }) =>
   (event) => {
-    const currentDate = new Date(2024, QUARTER_MAP[event.target.cellIndex], 1)
+    const currentDate = new Date(state.date.getFullYear(), QUARTER_MAP[event.target.cellIndex], 1)
     emit('pick', currentDate)
+  }
+
+export const showYearPicker =
+  ({ state }) =>
+  () =>
+    (state.currentView = DATEPICKER.Year)
+
+export const cusPrevYear =
+  ({ state }) =>
+  () => {
+    if (state.currentView === DATEPICKER.Year) {
+      state.startYear = state.startYear - DATEPICKER.PanelYearNum
+    } else {
+      state.date = prevYear(state.date)
+    }
+  }
+
+export const cusNextYear =
+  ({ state }) =>
+  () => {
+    if (state.currentView === DATEPICKER.Year) {
+      state.startYear = state.startYear + DATEPICKER.PanelYearNum
+    } else {
+      state.date = nextYear(state.date)
+    }
+  }
+
+export const handleYearPick =
+  ({ state }) =>
+  (value) => {
+    state.currentView = DATEPICKER.Quarter
+    state.date = modifyDate(state.date, value, state.date.getMonth(), state.date.getDate())
+  }
+
+export const getYearLabel =
+  ({ state, t }) =>
+  () => {
+    return state.date.getFullYear()
   }
