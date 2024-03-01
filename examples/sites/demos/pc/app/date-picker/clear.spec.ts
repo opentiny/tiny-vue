e4e91c9c9ebb6763a74346d4a853885a486d38e1
李天佑 <66231260+You-Hw-Y@users.noreply.github.com>
2024-03-01 16:28:38 +0800

    fix(test): [date-picker] update e2e test (#1453)
    * fix(test): [date-picker] update e2e test
    
    * fix(test): [date-picker] update e2e test
    
    * fix(test): [user-link] update e2e test

--- examples/sites/demos/pc/app/date-picker/clear.spec.ts
+++ examples/sites/demos/pc/app/date-picker/clear.spec.ts
@@ -31,6 +31,6 @@ test('[DatePicker] 测试清除输入', async ({ page }) => {
 
   // 测试清除功能
   await expect(datePickerCustomClearIcon).toHaveValue('2023-05-24')
-  await page.locator('.tiny-input__icon-container > svg > .st0').click()
+  await page.locator('#clear').getByRole('img').nth(2).click()
   await expect(datePickerCustomClearIcon).toHaveValue('')
 })
