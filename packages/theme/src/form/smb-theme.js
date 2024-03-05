8cde538415cd7a337477340227372280a2ca6aec
Huangyilin19 <huangyilin16@qq.com>
2024-03-05 11:43:37 +0800

    fix(build:theme): solved running build:theme script error

--- packages/theme/src/form/smb-theme.js
+++ packages/theme/src/form/smb-theme.js
@@ -1,5 +1,7 @@
 export const tinyFormSmbTheme = {
   'ti-form-item-label-top-margin-bottom': '24px',
-  'ti-form-item-label-top-label-line-height': '1.5', // smb要求行高为1.5
-  'ti-form-item-label-top-label-height': 'calc(1.5em + 8px)' // border-box需要加上内边距
+  // smb要求行高为1.5
+  'ti-form-item-label-top-label-line-height': '1.5',
+  // border-box需要加上内边距
+  'ti-form-item-label-top-label-height': 'calc(1.5em + 8px)'
 }
