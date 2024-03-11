90c2eb02ba1cadf60518f16e6a09182193969485
gimmyhehe <975402925@qq.com>
2024-03-11 09:15:04 +0800

    feat(sites): add theme route (#1478)

--- examples/sites/src/views/components/demo.vue
+++ examples/sites/src/views/components/demo.vue
@@ -84,7 +84,7 @@ export default defineComponent({
   },
   setup(props) {
     const { templateModeState } = useTemplateMode()
-    const { currThemeLabel } = useTheme()
+    const { currentThemeKey } = useTheme()
     const isMobileFirst = computed(() => {
       return templateModeState.mode === 'mobile-first'
     })
@@ -179,7 +179,7 @@ export default defineComponent({
       },
       openPlayground(demo, open = true) {
         const cmpId = router.currentRoute.value.params.cmpId
-        const tinyTheme = templateModeState.isSaas ? 'saas' : currThemeLabel.value.split('-')[1]
+        const tinyTheme = templateModeState.isSaas ? 'saas' : currentThemeKey.value.split('-')[1]
         const openModeQuery = open ? '' : '&openMode=preview'
         // TODO: 目前mf只有Options写法，后续再放开compositon
         const url = `${import.meta.env.VITE_PLAYGROUND_URL}?cmpId=${cmpId}&fileName=${
