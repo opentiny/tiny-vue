9bc7db4834c90531205801abb7154c2f65ee0cf4
James-9696 <hujm969696@163.com>
2024-03-18 02:17:04 -0700

    fix: update review

--- packages/theme/src/statistic/smb-theme.js
+++ packages/theme/src/statistic/smb-theme.js
@@ -3,5 +3,4 @@ export const tinyStatisticSmbTheme = {
   'ti-statistic-prefix-font-size': '24px',
   'ti-statistic-font-color': '#191919',
   'ti-statistic-suffix-font-size': 'var(--ti-common-font-size-4)'
-  // 'ti-statistic-suffix-font-weight': 'var(--ti-common-font-weight-3)'
 }
