7dc12dec1d623958bd116ee5d85eb738df7c1e85
张烟铃 <2581932570@qq.com>
2024-03-25 17:59:13 +0800

    fix(float-button): [float-button] nothing to do

--- examples/sites/demos/pc/app/float-button/backTop.spec.ts
+++ examples/sites/demos/pc/app/float-button/backTop.spec.ts
@@ -4,11 +4,9 @@ test('测试按钮回到顶部', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('float-button#backTop')
   const parent = await page.locator('.parent')
-
   await parent.evaluate((el) => {
     el.scrollTop = el.scrollHeight
   })
-
   await expect(page.locator('.tiny-float-button')).toBeVisible()
   await page.locator('.tiny-float-button').click()
   await page.waitForTimeout(1000)
