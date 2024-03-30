ecba505e95835ca96c705488499c7d4603db44fb
James <72028410+James-9696@users.noreply.github.com>
2024-03-30 10:04:06 +0800

    feat(statistic): statistic component  (#1491)
    * fix: add new component statistic
    
    * fix: add statistic renderless
    
    * feat: add statistic theme
    
    * feat: add statistic component
    
    * fix: update review
    
    * fix: update review
    
    * fix: update review

---
+++ examples/sites/demos/pc/app/statistic/basic-usage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <tiny-layout>
+      <tiny-row :flex="true" class="row-bg">
+        <tiny-col :span="3">
+          <tiny-statistic :value="num" :precision="0" title="用户活跃度"></tiny-statistic>
+        </tiny-col>
+        <tiny-col :span="3">
+          <tiny-statistic :value="num" :precision="2" title="消费额度"></tiny-statistic>
+        </tiny-col>
+        <tiny-col :span="3">
+          <tiny-statistic :value="num" :precision="0" title="来访数量" prefix="view:"></tiny-statistic>
+        </tiny-col>
+        <tiny-col :span="3">
+          <tiny-statistic :value="123" :precision="0" title="男女占比例" suffix="/100"></tiny-statistic>
+        </tiny-col>
+      </tiny-row>
+    </tiny-layout>
+  </div>
+</template>
+
+<script>
+import { Statistic, Layout, Row, Col } from '@opentiny/vue'
+
+export default {
+  components: {
+    TinyStatistic: Statistic,
+    TinyLayout: Layout,
+    TinyRow: Row,
+    TinyCol: Col
+  },
+  data() {
+    return {
+      num: 306526.23
+    }
+  }
+}
+</script>
---
+++ packages/renderless/src/statistic/index.ts
@@ -0,0 +1,22 @@
+import { isFunction } from '../common/type'
+
+export const isNumber =
+  ({ props }) =>
+  () => {
+    return typeof props.value === 'number'
+  }
+
+export const getIntegerAndDecimal =
+  ({ props }) =>
+  () => {
+    if (isFunction(props.formatter)) {
+      return props.formatter(props.value)
+    }
+    if (!isNumber(props.value)) {
+      return props.value
+    }
+    let displayValue = props.value ? String(props.value).split('.') : ''
+    let integer = displayValue[0]?.replace(/\B(?=(\d{3})+(?!\d))/g, props.groupSeparator)
+    let decimal = displayValue[1]?.padEnd(props.precision, '0').slice(0, props.precision > 0 ? props.precision : 0)
+    return [integer, decimal].join(decimal ? '.' : '')
+  }
