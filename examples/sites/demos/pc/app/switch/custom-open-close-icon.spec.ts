10be7da5098460bdbb33c0849b503052a3024652
AcWrong02 <3024800887@qq.com>
2024-04-10 22:27:12 +0800

    test(Swtich): [Switch] improve e2e test of costom icon

---
+++ examples/sites/demos/pc/app/switch/custom-open-close-icon.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+test('自定义开关图标', async ({ page }) => {
+  page.on('pageerror', (exception) => expect(exception).toBeNull())
+  await page.goto('switch##custom-open-close-icon')
+
+  const demo = page.locator('#custom-open-close-icon')
+  const switchBtn = demo.locator('.tiny-switch')
+
+  await expect(switchBtn.first()).toHaveAttribute()('class', 'tiny-svg')
+})
