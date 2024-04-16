8362c3a81ccbe8d41bb4a72342347de6d72dc4f9
AcWrong02 <3024800887@qq.com>
2024-04-16 13:42:29 +0800

    docs(Carousel): [Carousel] unified code example and improve the carousel demo style

--- examples/sites/demos/pc/app/carousel/carousel-events.vue
+++ examples/sites/demos/pc/app/carousel/carousel-events.vue
@@ -34,6 +34,15 @@ export default {
 </script>
 
 <style scoped>
+.tiny-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+  font-size: 25px;
+}
+
 .carousel-item-demo:nth-child(2n) {
   background-color: #fafafa;
 }
--- examples/sites/demos/pc/app/carousel/indicator-trigger-composition-api.vue
+++ examples/sites/demos/pc/app/carousel/indicator-trigger-composition-api.vue
@@ -6,11 +6,20 @@
   </tiny-carousel>
 </template>
 
-<script setup lang="jsx">
+<script setup>
 import { Carousel as TinyCarousel, CarouselItem as TinyCarouselItem } from '@opentiny/vue'
 </script>
 
 <style scoped>
+.tiny-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+  font-size: 25px;
+}
+
 .carousel-item-demo:nth-child(2n) {
   background-color: #fafafa;
 }
