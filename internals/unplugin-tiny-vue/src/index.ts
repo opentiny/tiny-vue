180ef51c04259781b016cc17c467fb38011d2126
Kagol <kagol@sina.com>
2024-04-19 09:11:50 +0800

    feat(unplugin-tiny-vue): add TinyVueResolver (#1585)

--- internals/unplugin-tiny-vue/src/index.ts
+++ internals/unplugin-tiny-vue/src/index.ts
@@ -12,22 +12,26 @@ const supportMap = {
   'rspack': AutoRspack
 }
 
+export const TinyVueResolver = (componentName) => {
+  if (componentName.startsWith('Tiny') && !componentName.startsWith('TinyIcon')) {
+    return {
+      name: componentName.slice(4),
+      from: '@opentiny/vue'
+    }
+  }
+}
+
 /** TinyVue 自动导入组件的插件，支持Vite,Webpack,Rollup 等常见的构建工具。
  * 目前不支持Tiny Icon的自动导入
  * @example
  * import autoImportPlugin from '@opentiny/unplugin-tiny-vue'
  * plugins: [autoImportPlugin('vite')]
  */
-export default (name) =>
-  supportMap[name]({
-    resolvers: [
-      (componentName) => {
-        if (componentName.startsWith('Tiny') && !componentName.startsWith('TinyIcon')) {
-          return {
-            name: componentName.slice(4),
-            from: '@opentiny/vue'
-          }
-        }
-      }
-    ]
+export default (name) => {
+  // 兼容webpack/vite的差异
+  const autoPlugin = supportMap[name].default || supportMap[name]
+
+  return autoPlugin({
+    resolvers: [TinyVueResolver]
   })
+}
