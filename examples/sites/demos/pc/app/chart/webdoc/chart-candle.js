8b3931ecde7bf1ff6e9555764605d9d26a208b8b
Davont <davontz@icloud.com>
2024-05-07 17:36:31 +0800

    docs: 修改文档描述

--- examples/sites/demos/pc/app/chart/webdoc/chart-candle.js
+++ examples/sites/demos/pc/app/chart/webdoc/chart-candle.js
@@ -5,49 +5,38 @@ export default {
     {
       'demoId': 'candle-base',
       'name': { 'zh-CN': '基本用法', 'en-US': 'Basic Usage' },
-      'desc': {
-        'zh-CN':
-          '<p>可以流畅的运行在 PC 和移动设备上，兼容当前绝大部分浏览器（IE 9/10/11，Chrome，Firefox，Safari 等）底层依赖轻量级的 Canvas 类库 ZRender，提供直观，生动，可交互，可高度个性化定制的数据可视化图表。</p>\n',
-        'en-US':
-          '<p> can run smoothly on PCs and mobile devices and is compatible with most browsers. (Internet Explorer 9/10/11, Chrome, Firefox, Safari, etc.) The bottom layer relies on the lightweight Canvas class library ZRender to provide intuitive, vivid, interactive, and highly personalized data visualization charts. </p>\n'
-      },
+      'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['candle/base.vue']
     },
     {
       'demoId': 'candle-demo2',
-      'name': { 'zh-CN': '使用简化的数据格式', 'en-US': 'Use simplified data format' },
+      'name': { 'zh-CN': 'K线图显示MA', 'en-US': 'K-line chart shows MA' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['candle/demo2.vue']
     },
     {
       'demoId': 'candle-demo3',
-      'name': { 'zh-CN': '设置数据格式', 'en-US': 'Set the data format' },
+      'name': { 'zh-CN': 'K线图显示volume', 'en-US': 'K-line chart shows volume' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['candle/demo3.vue']
     },
     {
       'demoId': 'candle-demo4',
-      'name': { 'zh-CN': '显示 MA, VOL', 'en-US': 'Display MA, VOL' },
+      'name': { 'zh-CN': ' K线图显示dataZoom', 'en-US': 'K-line chart shows dataZoom' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['candle/demo4.vue']
     },
     {
       'demoId': 'candle-demo5',
-      'name': { 'zh-CN': '修改指标名和图例文字', 'en-US': 'Modify the KQI/KPI Name and Legend Text' },
+      'name': { 'zh-CN': '不显示volume调整位置', 'en-US': 'Do not display the volume adjustment position' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['candle/demo5.vue']
     },
     {
       'demoId': 'candle-demo6',
-      'name': { 'zh-CN': '修改 MA 显示周期', 'en-US': 'Modify the MA display period' },
+      'name': { 'zh-CN': '显示volume调整位置', 'en-US': 'Displaying the volume adjustment position' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['candle/demo6.vue']
-    },
-    {
-      'demoId': 'candle-demo7',
-      'name': { 'zh-CN': '设置 dataRoom 控件', 'en-US': 'Set the dataRoom control' },
-      'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
-      'codeFiles': ['candle/demo7.vue']
     }
   ],
   apis: [{ 'name': 'chart-candle', 'type': 'component', 'props': [], 'events': [], 'slots': [] }],
--- examples/sites/demos/pc/app/chart/webdoc/chart-histogram.js
+++ examples/sites/demos/pc/app/chart/webdoc/chart-histogram.js
@@ -5,29 +5,24 @@ export default {
     {
       'demoId': 'histogram-base',
       'name': { 'zh-CN': '基本用法', 'en-US': 'Basic Usage' },
-      'desc': {
-        'zh-CN':
-          '<p>可以流畅的运行在 PC 和移动设备上，兼容当前绝大部分浏览器（IE 9/10/11，Chrome，Firefox，Safari 等）底层依赖轻量级的 Canvas 类库 ZRender，提供直观，生动，可交互，可高度个性化定制的数据可视化图表。</p>\n',
-        'en-US':
-          '<p> can run smoothly on PCs and mobile devices and is compatible with most browsers. (Internet Explorer 9/10/11, Chrome, Firefox, Safari, etc.) The bottom layer relies on the lightweight Canvas class library ZRender to provide intuitive, vivid, interactive, and highly personalized data visualization charts. </p>\n'
-      },
+      'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/base.vue']
     },
     {
       'demoId': 'histogram-demo2',
-      'name': { 'zh-CN': '设置显示的指标维度', 'en-US': 'Set the KQI/KPI dimension to be displayed.' },
+      'name': { 'zh-CN': '位置与padding', 'en-US': ' Position and padding' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/demo2.vue']
     },
     {
       'demoId': 'histogram-demo3',
-      'name': { 'zh-CN': '设置双 y 轴', 'en-US': 'Set the two y axes' },
+      'name': { 'zh-CN': '数据最小高度', 'en-US': 'Minimum data height' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/demo3.vue']
     },
     {
       'demoId': 'histogram-demo4',
-      'name': { 'zh-CN': '柱状图+折线图', 'en-US': 'Bar chart + Line chart' },
+      'name': { 'zh-CN': '鼠标悬浮提示框', 'en-US': 'Mouse hovering prompt box' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/demo4.vue']
     },
@@ -39,19 +34,19 @@ export default {
     },
     {
       'demoId': 'histogram-demo6',
-      'name': { 'zh-CN': '默认显示柱状图数据', 'en-US': 'By default, bar chart data is displayed.' },
+      'name': { 'zh-CN': '区域缩放轴', 'en-US': 'Region Zoom Axis' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/demo6.vue']
     },
     {
       'demoId': 'histogram-demo7',
-      'name': { 'zh-CN': '修改别名', 'en-US': 'Modify Alias' },
+      'name': { 'zh-CN': '数值显示配置详解', 'en-US': 'Value Display Configuration Details' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/demo7.vue']
     },
     {
       'demoId': 'histogram-demo8',
-      'name': { 'zh-CN': '设置横轴为连续的数值轴', 'en-US': 'Set the horizontal axis to a continuous value axis' },
+      'name': { 'zh-CN': 'x轴文本旋转', 'en-US': 'X-axis text rotation' },
       'desc': { 'zh-CN': '详细用法参考如下示例', 'en-US': 'For details, see the following example.' },
       'codeFiles': ['histogram/demo8.vue']
     }
