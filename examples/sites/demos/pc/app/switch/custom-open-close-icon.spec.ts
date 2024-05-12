a079643eef5a5a89b59921c579008e88e106a689
AcWrong02 <147061401+AcWrong02@users.noreply.github.com>
2024-05-12 11:26:09 +0800

    test(switch): fix swtich custom-open-close-icon e2e test

--- examples/sites/demos/pc/app/switch/custom-open-close-icon.spec.ts
+++ examples/sites/demos/pc/app/switch/custom-open-close-icon.spec.ts
@@ -2,10 +2,14 @@ import { test, expect } from '@playwright/test'
 
 test('自定义开关图标', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('switch##custom-open-close-icon')
+  await page.goto('switch#custom-open-close-icon')
 
   const demo = page.locator('#custom-open-close-icon')
-  const switchBtn = demo.locator('.tiny-switch')
+  const switchContainer = demo.locator('.tiny-switch')
 
-  await expect(switchBtn.first()).toHaveAttribute('class', 'tiny-svg')
+  const switchBtn = switchContainer.locator('.tiny-switch__button').first()
+
+  const switchIcon = switchBtn.locator('.tiny-svg').first()
+
+  await expect(switchIcon).not.toBeNull()
 })
