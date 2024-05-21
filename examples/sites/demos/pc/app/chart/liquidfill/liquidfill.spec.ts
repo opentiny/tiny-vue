f3a912871ad12456fc0bea1975ebb7b808057622
Davont <davontz@icloud.com>
2024-05-21 09:08:28 +0800

    fix: 【Charts】update charts snapshots (#1634)

--- examples/sites/demos/pc/app/chart/liquidfill/liquidfill.spec.ts
+++
@@ -1,5 +0,0 @@
-// test('base', async ({ page }) => {
-//   await page.goto('chart-liquidfill#liquidfill-base')
-//   const chart = page.locator('#liquidfill-base .hui-chart')
-//   await expect(chart).toHaveScreenshot('base.png')
-// })
