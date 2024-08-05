e09599fb00b057a558430467da0e501a803b5b3b
Kagol <kagol@sina.com>
2024-08-05 16:22:55 +0800

    fix(fluent-editor): fix style missing (#1831)

--- packages/vue/src/fluent-editor/src/mobile-first.vue
+++ packages/vue/src/fluent-editor/src/mobile-first.vue
@@ -14,6 +14,7 @@
 import { renderless, api } from '@opentiny/vue-renderless/fluent-editor/vue'
 import { props, $prefix, setup, defineComponent } from '@opentiny/vue-common'
 import FluentEditor from '@opentiny/fluent-editor'
+import '@opentiny/fluent-editor/dist/style.css'
 import {
   IconEditorAlignCenter,
   IconEditorAlignLeft,
