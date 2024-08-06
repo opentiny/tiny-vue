6a1ece881f74d98d006e69b4dfe739368dab6af6
wuyiping0628 <1106773985@qq.com>
2024-08-06 05:42:38 -0700

    feat(nav-menu): [nav-menu] updata nav-menu xdesign

--- packages/theme/src/nav-menu/smb-theme.js
+++ packages/theme/src/nav-menu/smb-theme.js
@@ -1,3 +1,21 @@
 export const tinyNavMenuSmbTheme = {
-  'ti-nav-menu-bg-color': '#282b33'
+  'ti-nav-menu-bg-color': 'var(--ti-common-color-bg-navigation)',
+  'ti-nav-menu-item-text-color': 'var(--ti-common-color-text-primary)',
+  'ti-nav-menu-popmenu-text-color': 'var(--ti-common-color-text-secondary)',
+  'ti-nav-menu-item-hover-bg-color': 'var(--ti-common-color-bg-navigation)',
+  'ti-nav-menu-item-text-font-weight': 'var(--ti-common-font-weight-bold)',
+  'ti-nav-menu-popmenu-selected-font-weight': 'var(--ti-common-font-weight-bold)',
+  'ti-nav-menu-item-selected-underline-height': 'var(--ti-common-border-weight-1)',
+  'ti-nav-menu-item-selected-underline-bg-color': 'var(--ti-common-color-text-primary)',
+  'ti-nav-menu-submenu-group-margin-top': 'var(--ti-common-size-9x)',
+  'ti-nav-menu-popmenu-node-item-padding-top': 'var(--ti-common-space-0)',
+  'ti-nav-menu-popmenu-node-item-padding-button': 'calc(var(--ti-common-space-3x) + 2px)',
+  'ti-nav-menu-submenu-padding-horizontal': 'calc(var(--ti-common-space-base) * 20.5)',
+  'ti-nav-menu-popmenu-node-title-margin-bottom': 'calc(var(--ti-common-space-3x) + 2px)',
+  'ti-nav-menu-popmenu-text-font-size': 'var(--ti-common-font-size-0)',
+  'ti-nav-menu-popmenu-more-item-hover-bg-color': 'var(--ti-common-color-bg-navigation)',
+  'ti-nav-menu-popmenu-more-item-active-bg-color': 'var(--ti-common-color-bg-navigation)',
+  'ti-nav-menu-popmenu-border-color': 'var(--ti-common-color-bg-navigation)',
+  'ti-nav-menu-popmenu-box-shadow': 'var(--ti-common-shadow-2-down)',
+  'ti-nav-menu-item-font-size': 'var(--ti-common-font-size-base)'
 }
