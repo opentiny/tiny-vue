4b3db7b8e77d9174c7c81c187e741711099d4f1c
zzcr <894103554@qq.com>
2024-08-12 17:09:43 +0800

    test(e2e): only test pc template

--- examples/sites/demos/pc/app/alert/basic-usage.vue
+++ examples/sites/demos/pc/app/alert/basic-usage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <tiny-alert description="测试e2e"></tiny-alert>
+    <tiny-alert description="type 为默认值 info"></tiny-alert>
     <tiny-alert type="error" description="type 为 error"></tiny-alert>
     <tiny-alert type="success" description="type 为 success"></tiny-alert>
     <tiny-alert type="warning">
