0a78d68cd8ae26b73862811b5dadee023fa1f6f2
zzcr <894103554@qq.com>
2024-08-13 10:14:15 +0800

    fix: split theme build error

--- packages/theme/src/split/smb-theme.js
+++ packages/theme/src/split/smb-theme.js
@@ -1,4 +1,6 @@
 export const tinySplitSmbTheme = {
-  'ti-split-trigger-button-border-width': '0', // 不显示边框线
-  'ti-split-trigger-button-bg-color': 'var(--ti-base-color-transparent)' // 不显示背景
+  // 不显示边框线
+  'ti-split-trigger-button-border-width': '0',
+  // 不显示背景
+  'ti-split-trigger-button-bg-color': 'var(--ti-base-color-transparent)'
 }
