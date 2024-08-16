3484a2c3eb3d950e843707c73d881a2d4ea77ecc
James-9696 <hujm969696@163.com>
2024-08-16 02:26:43 -0700

    fix(modal): [modal] modify issue 1384 bug

--- examples/sites/demos/pc/app/modal/message-closable-composition-api.vue
+++ examples/sites/demos/pc/app/modal/message-closable-composition-api.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script setup>
-import { Button as TinyButton, Modal } from '@opentiny/vue'
+import { Button as TinyButton, Modal, Notify } from '@opentiny/vue'
 
 function showModal() {
-  Modal.message({ message: '右侧显示关闭按钮', status: 'info', messageClosable: true })
+  Modal.message({
+    message: '右侧显示关闭按钮',
+    status: 'info',
+    messageClosable: true,
+    events: {
+      close: () => {
+        Notify({
+          type: 'info',
+          title: '触发close回调事件',
+          position: 'top-right'
+        })
+      }
+    }
+  })
 }
 </script>
