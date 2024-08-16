c2a48e26a990bf37ea9d2510458fb2c50793fa95
chenxi-20 <76168465+chenxi-20@users.noreply.github.com>
2024-08-15 23:13:05 -0700

    fix(anchor): [anchor] Add Anchor Component Software as a Service Topic (#1920)
    * fix(anchor): 增加anchor组件saas主题
    
    * fix(anchor): 去除onchange

---
+++ examples/sites/demos/mobile-first/app/anchor/is-affix.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrap">
+    <div class="demo-top mb10">
+      <tiny-switch v-model="isAffix"></tiny-switch>
+      <span class="demo-anchor">{{ isAffix ? '固定模式' : '非固定模式' }}</span>
+    </div>
+    <tiny-anchor :links="links" :is-affix="isAffix"></tiny-anchor>
+  </div>
+</template>
+
+<script>
+import { Anchor, Switch } from '@opentiny/vue'
+
+export default {
+  components: {
+    TinyAnchor: Anchor,
+    TinySwitch: Switch
+  },
+  data() {
+    return {
+      isAffix: false,
+      links: [
+        {
+          key: 'demonstrate',
+          link: '#demonstrate',
+          title: '演示',
+          children: [
+            {
+              key: 'basic-usage',
+              link: '#basic-usage',
+              title: '基本用法'
+            },
+            {
+              key: 'is-affix',
+              link: '#is-affix',
+              title: '固定模式'
+            },
+            {
+              key: 'set-container',
+              link: '#set-container',
+              title: '滚动容器'
+            },
+            {
+              key: 'on-change',
+              link: '#change',
+              title: 'change 事件'
+            }
+          ]
+        },
+        {
+          key: 'api',
+          link: '#API',
+          title: 'API'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrap {
+  min-height: 180px;
+}
+.demo-top {
+  display: flex;
+  align-items: center;
+}
+.demo-anchor {
+  margin-left: 8px;
+  font-size: 14px;
+}
+</style>
