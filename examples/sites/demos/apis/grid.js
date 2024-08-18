5e200ffe173d7ea1ec97714feae42394f24e97ea
Gweesin Chan <42909374+gweesin@users.noreply.github.com>
2024-08-18 11:23:55 +0800

    feat(grid): [grid] support resizable config (#1679)
    * fix: fix resizable line pos error
    
    * feat: support resizable config
    
    * refactor: expose params
    
    * docs: add document for resizable config
    
    * test: init resizable limit test
    
    * revert: revert unused modified for transfer docs
    
    This reverts commit 7aa1e1cb849377914dd66e4449d344bcc4a27272.
    
    * docs: add version info for IResizableConfig property

--- examples/sites/demos/apis/grid.js
+++ examples/sites/demos/apis/grid.js
@@ -724,6 +724,22 @@ export default {
           mfDemo: ''
         },
         {
+          name: 'IResizableConfig',
+          typeAnchorName: 'IResizableConfig',
+          type: 'IResizableConfig',
+          defaultValue: '',
+          metaData: {
+            new: '3.19.0'
+          },
+          desc: {
+            'zh-CN': '设置列宽拖拽参数',
+            'en-US': 'Set column width drag parameters'
+          },
+          mode: ['pc'],
+          pcDemo: 'grid-size#size-resizable-config',
+          mfDemo: ''
+        },
+        {
           name: 'row-class-name',
           typeAnchorName: 'IClassNameArgs',
           type: 'string | (args: IClassNameArgs) => string',
@@ -4668,6 +4684,16 @@ interface IResizableChangeArgs {
   columnIndex: number
   // 是否固定列
   fixed: boolean
+}
+      `
+    },
+    {
+      name: 'IResizableConfig',
+      type: 'type',
+      code: `
+interface IResizableConfig {
+  // 拖拽宽度限制函数，field: 当前拖拽的列名，width: 当前拖拽的宽度
+  limit: ({ field: string, width: number }) => number
 }
       `
     },
