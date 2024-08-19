dde583ed2221c5fa12a7c4ee5871ab756e881342
zzcr <894103554@qq.com>
2024-08-19 20:35:13 +0800

    feat(input,autocomplete): [input,autocomplete] update input&autocomplete x-design style

--- packages/theme/src/input/smb-theme.js
+++ packages/theme/src/input/smb-theme.js
@@ -11,7 +11,6 @@ export const tinyInputSmbTheme = {
   'ti-input-count-textlength-color': 'var(--ti-common-color-text-primary)',
   'ti-input-suffix-padding-left': 'var(--ti-common-space-3x)',
   'ti-input-disabled-text-color': 'var(--ti-common-color-text-disabled)',
-  'ti-input-disabled-bg-color': 'rgba(0,0,0,0.05)',
   'ti-input-suffix-icon-width': 'var(--ti-common-font-size-2)',
   'ti-input-suffix-icon-height': 'var(--ti-common-font-size-2)',
   'ti-input-icon-close-text-color': 'var(--ti-base-color-common-2)',
