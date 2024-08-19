fbddd1e87eb6b15f0d840f3b93ed2270a4414015
wuyiping0628 <1106773985@qq.com>
2024-08-19 00:08:45 -0700

    feat(collapse): [collapse] updata collapse xdesign

--- packages/theme/src/collapse-item/smb-theme.js
+++ packages/theme/src/collapse-item/smb-theme.js
@@ -5,8 +5,9 @@ export const tinyCollapseItemSmbTheme = {
   'ti-collapse-item-header-min-height': 'var(--ti-common-size-height-medium)',
   'ti-collapse-item-icon-color': '#808080',
   'ti-collapse-item-border-color': 'transparent',
-  'ti-collapse-item-content-border-color': 'rgba(0,0,0,0.08)',
-  'ti-collapse-item-content-border-color-top': 'rgba(0,0,0,0.08)',
+  'ti-collapse-item-content-border-color': 'var(--ti-common-color-light)',
+  'ti-collapse-item-content-border-color-top': 'var(--ti-common-color-light)',
+  'ti-collapse-item-border-length': 'var(--ti-common-border-radius-0)',
   'ti-collapse-item-content-padding-top': 'var(--ti-common-space-6x)',
   'ti-collapse-item-content-padding-right': 'var(--ti-common-space-5x)',
   'ti-collapse-item-content-padding-bottom': 'var(--ti-common-space-6x)',
@@ -17,5 +18,8 @@ export const tinyCollapseItemSmbTheme = {
   'ti-collapse-item-title-right-fill': '#1476ff',
   'ti-collapse-item-header-bg-color': 'var(--ti-common-color-bg-white-normal)',
   'ti-collapse-item-wrap-bg-color': 'var(--ti-common-bg-minor)',
+  'ti-collapse-item-header-border-color-left': 'var(--ti-common-color-light)',
+  'ti-collapse-item-header-border-color-right': 'var(--ti-common-color-light)',
+  'ti-collapse-item-margin-top': 'calc(var(--ti-common-space-0) - 2px )',
   'ti-collapse-item-arrow-margin-right': 'var(--ti-common-space-3x)'
 }
