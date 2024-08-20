08916543b37d9b25d04154c43a998d7db9b41e4b
chenxi <2465950588@qq.com>
2024-08-20 03:31:21 -0700

    feat(fileupload): [file-upload] 修复上传组件新规范检视 pictrue-card问题

--- packages/theme/src/upload-list/smb-theme.js
+++ packages/theme/src/upload-list/smb-theme.js
@@ -6,10 +6,11 @@ export const tinyUploadListSmbTheme = {
   'ti-upload-list-item-line-height': 'var(--ti-common-line-height-1)',
   'ti-upload-list-item-margin-top': 'var(--ti-common-space-2x)',
   'ti-upload-list-picture-card-item-margin-right': 'var(--ti-common-space-4x)',
-  'ti-upload-list-item-border-radius': 'var(--ti-common-border-radius-1)',
+  'ti-upload-list-item-border-radius': 'var(--ti-common-border-radius-4)',
   'ti-upload-picture-card-border-radius': 'var(--ti-common-border-radius-4)',
   'ti-upload-list-item-hover-background-color': 'var(--ti-common-bg-color-disabled-transparent-5)',
   'ti-upload-list-svg-icon-font-size': 'var(--ti-common-font-size-2)',
   'ti-upload-list-svg-refresh-position-right': 'var(--ti-common-space-8x)',
-  'ti-upload-list-picture-card-item-hover-bg-color': 'var(--ti-common-bg-color-hover-40)'
+  'ti-upload-list-picture-card-item-hover-bg-color': 'var(--ti-common-bg-color-hover-40)',
+  'ti-upload-list-picture-success-border-weight': '0'
 }
