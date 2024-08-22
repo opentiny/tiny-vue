8623577fb47a097ff76bb0d969bb79cd340886eb
18003228289 <149050291+18003228289@users.noreply.github.com>
2024-08-22 15:13:11 +0800

    fix(querySelectorAll): [querySelectorAll] fix querySelectorAll

--- packages/renderless/src/nav-menu/index.ts
+++ packages/renderless/src/nav-menu/index.ts
@@ -24,6 +24,7 @@ import { mapTree } from '../grid/static'
 import { transformTreeData } from '../common/array'
 import { on, off } from '../common/deps/dom'
 import { xss } from '../common/xss'
+import { isBrowser } from '../common/browser'
 
 const { nextZIndex } = PopupManager
 
@@ -483,21 +484,24 @@ export const skip =
 export const getPoint =
   ({ api, parent }: Pick<INavMenuRenderlessParams, 'api' | 'parent'>) =>
   (): number => {
-    const items = parent.$el.querySelectorAll('.menu>li') as NodeListOf<HTMLElement>
-    let index = 0
+    if (!isBrowser) return 0
+    else {
+      const items = parent.$el.querySelectorAll('.menu>li') as NodeListOf<HTMLElement>
+      let index = 0
 
-    if (items) {
-      index = items.length
+      if (items) {
+        index = items.length
 
-      for (let i = 0; i < items.length; i++) {
-        if (api.isHide(items[i])) {
-          index = index - (items.length - i)
-          break
+        for (let i = 0; i < items.length; i++) {
+          if (api.isHide(items[i])) {
+            index = index - (items.length - i)
+            break
+          }
         }
       }
-    }
 
-    return index
+      return index
+    }
   }
 
 export const classify =
