5730b1b35d2aefabea9897eb175c0d89e088ebdd
zhanyini <zhanyini666@163.com>
2024-08-22 10:34:58 -0700

    feat(popeditor): [popeditor] Modify test cases

--- examples/sites/demos/pc/app/popeditor/slot.spec.ts
+++ examples/sites/demos/pc/app/popeditor/slot.spec.ts
@@ -4,13 +4,15 @@ test('PopEditor 组件查询条件插槽', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('popeditor#slot')
 
-  const preview = page.locator('#slot')
-  const textBox = preview.getByRole('textbox')
+  const wrap = page.locator('#slot')
+  const textBox = wrap.getByRole('textbox')
   const searchWrap = page.locator('.tiny-dialog-box__body > .tiny-popeditor-top')
   const defaultSearch = searchWrap.locator('.tiny-popeditor__search-lists')
   const slotItem = searchWrap.locator('.item')
+  const buttons = searchWrap.locator('.buttons button')
 
   await textBox.click()
   await expect(defaultSearch).toHaveCount(0)
-  await expect(slotItem).toHaveCount(3)
+  await expect(slotItem).toHaveCount(2)
+  await expect(buttons).toHaveCount(2)
 })
