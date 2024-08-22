ca20fdeb6b0245de32ddd58a51de6ba6b12b1cb7
zhanyini <zhanyini666@163.com>
2024-08-22 12:50:55 -0700

    feat(image): [image] Modify test cases

--- examples/sites/demos/pc/app/image/keep-style.spec.ts
+++ examples/sites/demos/pc/app/image/keep-style.spec.ts
@@ -18,7 +18,7 @@ test('测试样式属性保持', async ({ page }) => {
   await expect(viewerImage).toHaveCount(1)
   await expect(canvas).toHaveCount(1)
   await expect(mask).toHaveCSS('position', 'absolute')
-  await expect(mask).toHaveCSS('background', 'rgba(0, 0, 0, 0.5)')
+  await expect(mask).toHaveCSS('background-color', 'rgba(0, 0, 0, 0.5)')
   await expect(canvas).toHaveCSS('transform', 'matrix(1, 0, 0, 1, 0, 0)')
 
   // 点击缩小
