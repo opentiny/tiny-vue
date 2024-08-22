90dd30b430a02fc9c18cb9b1d0f6d2d5c03d90e4
zhanyini <zhanyini666@163.com>
2024-08-22 15:25:05 -0700

    feat(slider): [slider] Adapt slider component smb theme

--- packages/theme/src/slider/smb-theme.js
+++ packages/theme/src/slider/smb-theme.js
@@ -1,5 +1,5 @@
 export const tinySliderSmbTheme = {
-  'ti-slider-border-radius': 'var(--ti-common-border-radius-1)',
+  'ti-slider-border-radius': 'var(--ti-common-border-radius-2)',
   'ti-slider-bg-color': 'var(--ti-common-color-bg-normal)',
   'ti-slider-height': 'var(--ti-common-space-2x)',
   'ti-slider-margin-left': 'calc(var(--ti-common-space-base) * -1)',
@@ -8,18 +8,22 @@ export const tinySliderSmbTheme = {
   'ti-slider-range-hover-bg-color': 'var(--ti-common-color-data-1)',
   'ti-slider-range-height': 'var(--ti-common-space-2x)',
   'ti-slider-range-margin-top': '0',
-  'ti-slider-range-border-radius': 'var(--ti-common-border-radius-1)',
+  'ti-slider-range-border-radius': 'var(--ti-common-border-radius-2)',
   'ti-slider-range-disabled-bg-color': 'var(--ti-common-color-bg-secondary)',
 
-  'ti-slider-handle-width': 'var(--ti-common-space-4x)',
+  'ti-slider-handle-width': 'var(--ti-common-space-6x)',
   'ti-slider-handle-height': 'var(--ti-common-space-6x)',
-  'ti-slider-handle-border-color': 'var(--ti-common-color-data-1)',
+  'ti-slider-handle-border-color': 'var(--ti-common-color-line-disabled)',
   'ti-slider-handle-border-color-hover': 'var(--ti-common-color-data-1)',
+  'ti-slider-handle-box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.08)', // 没有这个变量
   'ti-slider-handle-margin-top': 'calc(var(--ti-common-space-2x) * -1)',
   'ti-slider-handle-icon-fill-color': 'var(--ti-common-color-data-1)',
-  'ti-slider-handle-border-radius': 'var(--ti-common-border-radius-4)',
+  'ti-slider-handle-border-radius': 'var(--ti-common-border-radius-5)',
   'ti-slider-handle-icon-disabled-fill-color': 'var(--ti-common-color-icon-disabled)',
   'ti-slider-handle-disabled-border-color': 'var(--ti-common-color-icon-disabled)',
+  'ti-slider-handle-icon-display': 'none',
+  'ti-slider-handle-bg-color': 'var(--ti-common-color-bg-white-normal)',
+  'ti-slider-handle-border-weight-hover': 'var(--ti-common-border-weight-1)',
 
   'ti-slider-tips-text-color': 'var(--ti-common-color-dark)',
   'ti-slider-tips-box-shadow': 'var(--ti-common-box-shadow-popover)',
@@ -29,5 +33,7 @@ export const tinySliderSmbTheme = {
   'ti-slider-input-text-align': 'left',
 
   'ti-slider-mark-label-font-size': 'var(--ti-common-font-size-1)',
-  'ti-slider-mark-label-text-color': '#999999'
+  'ti-slider-mark-label-text-color': 'var(--ti-common-color-text-weaken)',
+
+  'ti-slider-input-unit-margin-left': 'var(--ti-common-space-base)'
 }
