ad7bfcb67c056d77d330d5518396489f4a3fdcbf
You-Hw-Y <1664677472@qq.com>
2024-08-23 00:13:06 -0700

    feat(radio): [radio] Adapting to the SMB theme

--- packages/theme/src/radio-button/smb-theme.js
+++ packages/theme/src/radio-button/smb-theme.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2022 - present TinyVue Authors.
+ * Copyright (c) 2022 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
+
 export const tinyRadioButtonSmbTheme = {
   'ti-radio-button-checked-normal-bg-color': '#1476FF',
   'ti-radio-button-checked-normal-border-color': '#1476FF',
@@ -13,5 +25,11 @@ export const tinyRadioButtonSmbTheme = {
   'ti-radio-button-child-right-border-radius': 'var(--ti-common-space-6)',
   'ti-radio-button-padding-verticals': '9px',
   'ti-radio-button-active-disabled-color': 'var(--ti-common-color-text-weaken)',
-  'ti-radio-button-active-disabled-bg-color': 'var(--ti-common-color-bg-dark-disabled)'
+  'ti-radio-button-active-disabled-bg-color': 'var(--ti-common-color-bg-dark-disabled)',
+  'ti-radio-button-checked-disabled-border-color': 'unset',
+  'ti-radio-button-bg-color': 'var(--ti-common-color-bg-normal)',
+  'ti-radio-button-checked-disabled-bg-color': 'var(--ti-common-color-bg-dark-disabled)',
+  'ti-radio-button-disabled-bg-color': 'var(--ti-common-color-bg-disabled)',
+  'ti-radio-button-disabled-checked-text-color': 'var(--ti-common-color-text-weaken)',
+  'ti-radio-button-text-color': 'var(--ti-common-color-text-secondary)'
 }
