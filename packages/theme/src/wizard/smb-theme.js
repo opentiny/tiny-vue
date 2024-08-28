563a64eada7936acf1a64c6c8b2de538be95249f
chenxi <2465950588@qq.com>
2024-08-27 20:05:31 -0700

    feat(wizard): 增加流程图新规范的基本用法模式

---
+++ packages/theme/src/wizard/smb-theme.js
@@ -0,0 +1,19 @@
+export const tinyWizardSmbTheme = {
+  'ti-wizard-icon-bg-color': 'var(--ti-common-color-light)',
+  'ti-wizard-icon-color': 'var(--ti-common-color-bg-emphasize)',
+  'ti-wizard-node-size': 'var(--ti-common-font-size-6)',
+  'ti-wizard-lines-height': 'var(--ti-common-border-weight-normal)',
+  'ti-wizard-lines-width': 'var(--ti-common-size-width-minor)',
+  'ti-wizard-lines-margin-horizontal': 'var(--ti-common-dropdown-gap)',
+  'ti-wizard-first-lines-bg-color': 'var(--ti-common-color-transparent)',
+  'ti-wizard-last-lines-bg-color': 'var(--ti-common-color-transparent)',
+  'ti-wizard-right-lines-doing-bg-color': 'var(--ti-common-color-line-disabled)',
+  'ti-wizard-lines-normal-bg-color': 'var(--ti-common-color-line-disabled)',
+  'ti-wizard-chart-icon-bg-color': 'var(--ti-common-color-bg-light-normal)',
+  'ti-wizard-lines-normal-border-weight': 'var(--ti-common-border-weight-normal)',
+  'ti-wizard-normal-icon-color': 'var(--ti-common-color-text-disabled)',
+  'ti-wizard-node-doing-font-weight': 'var(--ti-common-font-weight-bold)',
+  'ti-wizard-node-title-margin-top': 'var(--ti-common-dropdown-gap)',
+  'ti-wizard-button-group-margin-top': 'calc(var(--ti-common-space-base) * 9)',
+  'ti-wizard-button-group-text-align': 'left'
+}
