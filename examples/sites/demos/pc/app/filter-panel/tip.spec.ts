5e9600904938cb78ef3e5104dd5ad4c541a9e7bb
chenxi <2465950588@qq.com>
2024-09-02 19:15:11 -0700

    feat(filter-panel): 测试filter-panel

--- examples/sites/demos/pc/app/filter-panel/tip.spec.ts
+++ examples/sites/demos/pc/app/filter-panel/tip.spec.ts
@@ -4,9 +4,10 @@ test('tip提示', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('filter-panel#tip')
 
-  const panel = page.locator('.tiny-filter-panel')
+  const container = page.locator('#tip')
+  const panel = container.locator('.tiny-filter-panel')
   const help = panel.locator('.tiny-svg.tiny-tooltip')
-  const tooltip = page.getByRole('tooltip', { name: '请选择物品数量' })
+  const tooltip = container.getByRole('tooltip', { name: '请选择物品数量' })
 
   await help.hover()
   await tooltip.isVisible()
