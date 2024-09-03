713a191f5b2da7014fb872bc3413037cf493867a
shenjunjian <40288193@qq.com>
2024-09-03 00:00:18 -0700

    fix(theme0swap): fix

--- internals/automate/src/theme-swap/index.ts
+++ internals/automate/src/theme-swap/index.ts
@@ -1,1 +1,11 @@
-//
+import fg from 'fast-glob'
+import path from 'node:path'
+
+const ignoreFolds = ['base', 'theme', 'images', 'mixins', 'svgs', 'transitions', 'vars.less']
+const srcFold = path.join(process.cwd(), '../../packages/theme/src')
+
+// vars.less的所有文件夹， 类似  [ 'action-menu/vars.less',  'alert/vars.less',.........]
+const varsFiles = fg.sync(['**/vars.less'], { cwd: srcFold, ignore: ignoreFolds })
+// console.log(varsFiles)
+
+const _folds = varsFiles.map((file) => file.split('/')[0])
