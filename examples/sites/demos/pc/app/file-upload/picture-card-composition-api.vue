b29e30da97a748a5bab151e905d5d28635bb67d1
chenxi <2465950588@qq.com>
2024-09-05 23:22:38 -0700

    fix(file-upload): 状态更新为success

--- examples/sites/demos/pc/app/file-upload/picture-card-composition-api.vue
+++ examples/sites/demos/pc/app/file-upload/picture-card-composition-api.vue
@@ -27,6 +27,11 @@ const fileList = ref([
   {
     name: 'fruit',
     url: `${import.meta.env.VITE_APP_BUILD_BASE_URL}static/images/fruit.jpg`,
+    status: 'success'
+  },
+  {
+    name: 'fruit',
+    url: `${import.meta.env.VITE_APP_BUILD_BASE_URL}static/images/fruit.jpg`,
     status: 'uploading',
     percentage: 50
   },
