ebb5f40b19f539f1f551f464d1edbac09fb76126
18003228289 <149050291+18003228289@users.noreply.github.com>
2024-09-06 14:22:41 +0800

    fix(dropdown): [dropdown] fix the issue with the dropdown box position (#2049)
    * feat(tiny-vue-nuxt): [tiny-vue-nuxt] add tiny-vue-nuxt module and add some components to module
    
    * feat(tiny-vue-nuxt): [tiny-vue-nuxt] finish importing all components to nuxt module
    
    * fix(dropdown): [dropdown] fix the issue with the dropdown box position
    
    * fix(dropdown): [dropdown] fix the issue with the dropdown box position

--- examples/nuxt/playground/pages/Badge.vue
+++ examples/nuxt/playground/pages/Badge.vue
@@ -1,15 +1,20 @@
 <template>
-  <tiny-badge :value="2" badge-class="custom-badge-class">我的待办</tiny-badge>
+  <div class="badge-demo">
+    <tiny-badge :value="2">默认类型</tiny-badge>
+    <tiny-badge :value="2" type="danger">danger 类型</tiny-badge>
+    <tiny-badge :value="2" type="primary">primary 类型</tiny-badge>
+    <tiny-badge :value="2" type="success">success 类型</tiny-badge>
+    <tiny-badge :value="2" type="warning">warning 类型</tiny-badge>
+    <tiny-badge :value="2" type="info">info 类型</tiny-badge>
+  </div>
 </template>
 
 <script setup>
 import { Badge as TinyBadge } from '@opentiny/vue'
 </script>
 
-<style>
-.custom-badge-class {
-  background-color: #f2f2f3;
-  color: #7a7e87;
-  font-weight: normal;
+<style scoped>
+.badge-demo .tiny-badge__wrapper {
+  margin-bottom: 12px;
 }
 </style>
