6a50e17d529169883a13de6511dc4b69dcf0621c
You-Hw-Y <1664677472@qq.com>
2024-09-09 20:45:46 -0700

    feat(badge): [badge] Adapting to the SMB theme

--- examples/sites/demos/pc/app/badge/offset.vue
+++ examples/sites/demos/pc/app/badge/offset.vue
@@ -2,11 +2,11 @@
   <div>
     <p>使用字符串：</p>
     <br />
-    <tiny-badge :value="2" :offset="['0', '-20%']">我的待办</tiny-badge>
+    <tiny-badge :value="2" :offset="['0', '-50%']">我的待办</tiny-badge>
     <br /><br />
     <p>使用数字：</p>
     <br />
-    <tiny-badge :value="2" :offset="[0, -6]">我的待办</tiny-badge>
+    <tiny-badge :value="2" :offset="[0, -10]">我的待办</tiny-badge>
   </div>
 </template>
 
