37398f33626beeae4bd7ba0dc45da743bb0b7da1
chenxi <2465950588@qq.com>
2024-09-10 01:08:47 -0700

    feat(search): 迷你模式增加无边框示例

--- examples/sites/demos/pc/app/search/mini-mode.vue
+++ examples/sites/demos/pc/app/search/mini-mode.vue
@@ -1,5 +1,8 @@
 <template>
-  <tiny-search mini></tiny-search>
+  <div>
+    <tiny-search mini></tiny-search>
+    <tiny-search mini class="mt24 custom-search"></tiny-search>
+  </div>
 </template>
 
 <script lang="jsx">
@@ -11,3 +14,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+:deep(.tiny-search.custom-search.collapse .tiny-search__line) {
+  border: none;
+}
+</style>
