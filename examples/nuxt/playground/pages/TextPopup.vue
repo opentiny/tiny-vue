479c85121fb0027383bea28f8814bb958d185d8b
18003228289 <149050291+18003228289@users.noreply.github.com>
2024-09-11 16:40:54 +0800

    fix(actionMenu): [actionMenu] fix actionMenu SSR style error (#2082)
    * fix(actionMenu): [actionMenu] fix actionMenu SSR style error
    
    * fix(actionMenu): [actionMenu] fix actionMenu SSR style error

--- examples/nuxt/playground/pages/TextPopup.vue
+++ examples/nuxt/playground/pages/TextPopup.vue
@@ -1,7 +1,13 @@
 <template>
-  <tiny-text-popup></tiny-text-popup>
+  <div>
+    <tiny-text-popup v-model="value"></tiny-text-popup>
+    <span style="margin-left: 14px">{{ value }}</span>
+  </div>
 </template>
 
 <script setup lang="jsx">
+import { ref } from 'vue'
 import { TextPopup as TinyTextPopup } from '@opentiny/vue'
+
+const value = ref('我是 TextPopup 组件')
 </script>
