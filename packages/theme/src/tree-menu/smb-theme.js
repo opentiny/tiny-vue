b734129b560e7c9b866d6974b9e12ae7739050da
wuyiping0628 <42107997+wuyiping0628@users.noreply.github.com>
2024-09-10 20:33:02 -0700

    feat(tree-menu): [tree-menu] updata tree-menu xdesign (#2095)

--- packages/theme/src/tree-menu/smb-theme.js
+++ packages/theme/src/tree-menu/smb-theme.js
@@ -4,6 +4,9 @@ export const tinyTreeMenuSmbTheme = {
   'ti-tree-menu-node-current-text-color': 'var(--ti-common-color-text-primary)',
   'ti-tree-menu-square-left-border-color': 'var(--ti-common-color-icon-active)',
   'ti-tree-menu-node-number-font-size': 'var(--ti-common-font-size-0)',
+  'ti-tree-menu-padding-top': 'var(--ti-common-size-2x)',
   'ti-tree-menu-border-color': 'var(--ti-common-color-light)',
+  'ti-tree-menu-collapse-icon-margin-right': 'var(--ti-common-size-5x)',
+  'ti-common-color-light': 'var(--ti-common-color-line-dividing)',
   'ti-tree-menu-node-number-color': 'var(--ti-common-color-text-weaken)'
 }
