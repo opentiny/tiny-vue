a4a36aba99c1d367f6c688ec36a89ff91ba33b84
You-Hw-Y <1664677472@qq.com>
2024-09-12 05:54:12 -0700

    feat(switch): [switch] Adapting to the SMB theme

---
+++ examples/sites/demos/pc/app/switch/loading-composition-api.vue
@@ -0,0 +1,24 @@
+<template>
+  <tiny-switch v-model="value" :loading="loading"></tiny-switch>
+  <tiny-switch v-model="value2" class="demo-switch" :loading="loading"></tiny-switch>
+  <tiny-button size="mini" @click="change()"> changeValue </tiny-button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Switch as TinySwitch, Button as TinyButton } from '@opentiny/vue'
+
+const value = ref(true)
+const value2 = ref(false)
+const loading = ref(true)
+
+function change() {
+  loading.value = !loading.value
+}
+</script>
+
+<style scoped>
+.demo-switch {
+  margin-right: 40px;
+}
+</style>
