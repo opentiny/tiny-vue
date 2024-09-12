761f1fd22154877741da7723ec304a30ca1e3a25
GweesinChan <gweesin2000@gmail.com>
2024-09-12 16:35:38 +0800

    fix: fix build dts error
    reuse tsup to generate dts

--- packages/renderless/build.config.ts
+++ packages/renderless/build.config.ts
@@ -1,10 +1,16 @@
 import { defineBuildConfig } from 'unbuild'
+import { version } from './package.json'
 
 export default defineBuildConfig({
   entries: [
     {
       builder: 'mkdist',
-      input: './src/'
+      input: './src/',
+      esbuild: {
+        define: {
+          'process.env.RUNTIME_VERSION': JSON.stringify(version)
+        }
+      }
     }
   ],
   clean: true
