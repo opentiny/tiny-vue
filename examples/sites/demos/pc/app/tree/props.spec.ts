71b1eaed08279c2320aaa3fc432f103c0364d651
申君健 <40288193@qq.com>
2024-09-14 14:28:17 +0800

    fix(theme):　[tree,tooltip,transfer, cascader] update unsolved smb components (#2124)
    * fix(theme): remove after's class
    
    * fix(tooltip): refactor tooltip's arrow style by  rotate a div
    
    * fix(theme): update cascader style
    
    * fix(theme): update tooltip/transfer style
    
    * fix(theme): update unresolved smb style
    
    * fix(tree): fix e2e error

--- examples/sites/demos/pc/app/tree/props.spec.ts
+++ examples/sites/demos/pc/app/tree/props.spec.ts
@@ -8,5 +8,5 @@ test('测试自定义映射列', async ({ page }) => {
   await expect(tree.getByText('数据 1-1-1')).toHaveCount(1)
 
   // 测试disabled
-  await expect(tree.getByText('数据 2-2')).toHaveCSS('color', 'rgb(173, 176, 184)')
+  await expect(tree.getByText('数据 2-2')).toHaveCSS('color', 'rgb(194, 194, 194)')
 })
