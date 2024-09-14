b59f14ab05233d0f7b341f8ac81cac262a1cfe5b
You-Hw-Y <1664677472@qq.com>
2024-09-13 23:51:48 -0700

    feat(checkbox): [checkbox] Adapting to the SMB  theme

--- examples/sites/demos/pc/app/checkbox/checkbox-button.spec.ts
+++ examples/sites/demos/pc/app/checkbox/checkbox-button.spec.ts
@@ -7,12 +7,12 @@ test('复选框按钮', async ({ page }) => {
   const demo = page.locator('#checkbox-button')
   const checkboxButton = demo.locator('.tiny-checkbox-button .tiny-checkbox-button__inner')
 
-  await expect(checkboxButton.first()).toHaveCSS('background-color', 'rgb(94, 124, 224)')
-  await expect(checkboxButton.first()).toHaveCSS('border-bottom-color', 'rgb(94, 124, 224)')
-  await expect(checkboxButton.nth(1)).toHaveCSS('background-color', 'rgb(233, 237, 250)')
-  await expect(checkboxButton.nth(1)).toHaveCSS('border-bottom-color', 'rgb(233, 237, 250)')
+  await expect(checkboxButton.first()).toHaveCSS('background-color', 'rgb(255, 255, 255)')
+  await expect(checkboxButton.first()).toHaveCSS('border-bottom-color', 'rgb(20, 118, 255)')
+  await expect(checkboxButton.nth(1)).toHaveCSS('background-color', 'rgb(255, 255, 255)')
+  await expect(checkboxButton.nth(1)).toHaveCSS('border-bottom-color', 'rgb(194, 194, 194)')
 
   await checkboxButton.nth(1).click()
-  await expect(checkboxButton.nth(1)).toHaveCSS('background-color', 'rgb(94, 124, 224)')
-  await expect(checkboxButton.nth(1)).toHaveCSS('border-bottom-color', 'rgb(94, 124, 224)')
+  await expect(checkboxButton.nth(1)).toHaveCSS('background-color', 'rgb(255, 255, 255)')
+  await expect(checkboxButton.nth(1)).toHaveCSS('border-bottom-color', 'rgb(20, 118, 255)')
 })
