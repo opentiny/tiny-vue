e7bd9638dd8950386e684502861c0523c2f1bfd8
Gimmy <975402925@qq.com>
2024-09-19 19:30:21 +0800

    test(grid): [grid] grid test case fix

--- examples/sites/demos/pc/app/grid/renderer/inner-renderer.spec.js
+++ examples/sites/demos/pc/app/grid/renderer/inner-renderer.spec.js
@@ -4,5 +4,5 @@ test('内置渲染器', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-renderer#renderer-inner-renderer')
   const cell = page.getByRole('cell', { name: '90.0%' }).locator('.tiny-grid__rate-chart')
-  await expect(cell).toHaveCSS('background-color', 'rgb(80, 212, 171)')
+  await expect(cell).toHaveCSS('background-color', 'rgb(92, 179, 0)')
 })
