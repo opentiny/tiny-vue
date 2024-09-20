e863c4aab2a0ce1e4a030afc995e8447b3832082
gimmyhehe <975402925@qq.com>
2024-09-20 09:30:18 +0800

    feat(grid): [grid] grid custom panel adapt to x-design (#2152)
    * feat(grid): [grid] grid custom panel adapt to x-design
    
    * test(grid): [grid] grid test case fix

--- examples/sites/demos/pc/app/grid/tree-table/tree-grid-index.spec.js
+++ examples/sites/demos/pc/app/grid/tree-table/tree-grid-index.spec.js
@@ -3,21 +3,17 @@ import { test, expect } from '@playwright/test'
 test('展开行序号列配置', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-tree-table#tree-table-tree-grid-index')
-  await page
-    .getByRole('row', { name: '1 GFD科技YX公司 华东区 800' })
-    .locator('.tiny-grid-tree__node-btn')
-    .first()
-    .click()
+  await page.getByRole('row', { name: '1 GFD科技YX公司 华东区 800' }).locator('.tiny-grid-tree-wrapper').first().click()
   await page
     .getByRole('row', { name: '2 WWWW科技YX公司 华南区 500' })
-    .locator('.tiny-grid-tree__node-btn')
+    .locator('.tiny-grid-tree-wrapper')
     .first()
     .click()
-  await page.getByRole('row', { name: '3 TGBYX公司 华南区 360' }).locator('.tiny-grid-tree__node-btn').first().click()
+  await page.getByRole('row', { name: '3 TGBYX公司 华南区 360' }).locator('.tiny-grid-tree-wrapper').first().click()
   await page.getByText('3.1').first().click()
   await page
     .getByRole('row', { name: '4 康康物业YX公司 华南区 400' })
-    .locator('.tiny-grid-tree__node-btn')
+    .locator('.tiny-grid-tree-wrapper')
     .first()
     .click()
 
