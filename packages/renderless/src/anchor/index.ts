1d6b6bd7de2cad4e40117430952c70508607fab1
gimmyhehe <975402925@qq.com>
2024-09-20 09:29:38 +0800

    fix(anchor): [anchor] fix anchor error when links is empty (#2155)
    * fix(anchor): [anchor] fix anchor error when links is empty
    
    * fix(anchor): [anchor] fix anchor error when links is empty

--- packages/renderless/src/anchor/index.ts
+++ packages/renderless/src/anchor/index.ts
@@ -124,6 +124,9 @@ export const handleScroll = (state: IAnchorRenderlessParams['state']) => () => {
 
 // 设置滚动偏移量
 const setChildOffsetTop = ({ state, props }: Pick<IAnchorRenderlessParams, 'state' | 'props'>) => {
+  if (!props.links?.length) {
+    return
+  }
   state.offsetTop = document.querySelector(props.links[0].link)?.offsetTop || 0
 }
 
