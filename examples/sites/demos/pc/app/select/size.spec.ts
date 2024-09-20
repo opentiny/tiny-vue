e78b27480b4dd0fc2fc54d540992e6b84c0a1b65
zhanyini <zhanyini666@163.com>
2024-09-20 10:39:10 -0700

    feat(test): [dropdown,select,slider] Modify test cases

--- examples/sites/demos/pc/app/select/size.spec.ts
+++ examples/sites/demos/pc/app/select/size.spec.ts
@@ -9,7 +9,7 @@ test('默认尺寸', async ({ page }) => {
   const input = select.locator('.tiny-input')
   const tag = select.locator('.tiny-tag')
 
-  await expect(input.locator('.tiny-input__inner')).toHaveCSS('height', '28px')
+  await expect(input.locator('.tiny-input__inner')).toHaveCSS('height', '32px')
   await expect(tag.nth(0)).toHaveClass(/tiny-tag--light/)
 })
 
@@ -23,6 +23,7 @@ test('medium 尺寸', async ({ page }) => {
   const tag = select.locator('.tiny-tag')
 
   await expect(input).toHaveClass(/tiny-input-medium/)
+  // TODO: 此处继承input 尺寸的话，应该是32
   await expect(input.locator('.tiny-input__inner')).toHaveCSS('height', '40px')
   await expect(tag.nth(0)).toHaveClass(/tiny-tag--medium tiny-tag--light/)
 })
@@ -39,7 +40,7 @@ test('small 尺寸', async ({ page }) => {
 
   await expect(input).toHaveClass(/tiny-input-small/)
   await expect(tag.nth(0)).toHaveClass(/tiny-tag--small tiny-tag--light/)
-  expect(height).toBeCloseTo(32, 1)
+  expect(height).toBeCloseTo(28, 1)
 })
 
 test('mini 尺寸', async ({ page }) => {
