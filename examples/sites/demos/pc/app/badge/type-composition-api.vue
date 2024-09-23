d697b6ab61468cfe76bc62191bdfbbf3958cf049
东风使命必达 <1664677472@qq.com>
2024-09-23 05:21:48 -0700

    feat(badge): [badge]: Adapting to the SMB theme (#2167)

--- examples/sites/demos/pc/app/badge/type-composition-api.vue
+++ examples/sites/demos/pc/app/badge/type-composition-api.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="badge-demo">
     <tiny-badge :value="2">默认类型</tiny-badge>
-    <tiny-badge :value="2" type="danger">danger 类型</tiny-badge>
-    <tiny-badge :value="2" type="primary">primary 类型</tiny-badge>
     <tiny-badge :value="2" type="success">success 类型</tiny-badge>
     <tiny-badge :value="2" type="warning">warning 类型</tiny-badge>
     <tiny-badge :value="2" type="info">info 类型</tiny-badge>
+    <tiny-badge :value="2" type="danger">danger 类型</tiny-badge>
+    <tiny-badge :value="2" type="primary">primary 类型</tiny-badge>
   </div>
 </template>
 
 <script setup>
 import { Badge as TinyBadge } from '@opentiny/vue'
 </script>
+
 <style scoped>
 .badge-demo .tiny-badge__wrapper {
   margin-bottom: 12px;
