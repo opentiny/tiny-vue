4c16cf879dfd25fab8338ba31be4434337dd6f9f
东风使命必达 <1664677472@qq.com>
2024-09-24 04:30:37 -0700

    feat(popconfirm): [popconfirm] Adapting to the SMB theme (#2168)

--- examples/sites/demos/pc/app/popconfirm/type-composition-api.vue
+++ examples/sites/demos/pc/app/popconfirm/type-composition-api.vue
@@ -1,42 +1,44 @@
 <template>
   <div>
-    <tiny-popconfirm :title="title" type="info">
+    <tiny-popconfirm :title="title" type="info" :message="message">
       <template #reference>
         <tiny-button>info</tiny-button>
       </template>
     </tiny-popconfirm>
 
-    <tiny-popconfirm :title="title" type="error">
+    <tiny-popconfirm :title="title" type="error" :message="message">
       <template #reference>
         <tiny-button>error</tiny-button>
       </template>
     </tiny-popconfirm>
 
-    <tiny-popconfirm :title="title" type="warning">
+    <tiny-popconfirm :title="title" type="warning" :message="message">
       <template #reference>
         <tiny-button>warning</tiny-button>
       </template>
     </tiny-popconfirm>
 
-    <tiny-popconfirm :title="title" type="success">
+    <tiny-popconfirm :title="title" type="success" :message="message">
       <template #reference>
         <tiny-button>success</tiny-button>
       </template>
     </tiny-popconfirm>
 
-    <tiny-popconfirm :title="title" :type="TinyIconDel">
+    <tiny-popconfirm :title="title" :type="TinyIconDel" :message="message">
       <template #reference>
         <tiny-button>自定义</tiny-button>
       </template>
     </tiny-popconfirm>
   </div>
 </template>
+
 <script setup>
 import { ref } from 'vue'
 import { Popconfirm as TinyPopconfirm, Button as TinyButton } from '@opentiny/vue'
 import { iconDel } from '@opentiny/vue-icon'
 
-const title = ref('确定要删除该安全组规则吗?')
+const title = ref('提示标题')
+const message = ref('安全组规则是推荐的，确定要删除该安全组规则吗?')
 const TinyIconDel = iconDel()
 </script>
 
