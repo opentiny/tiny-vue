ba63fad6623b948cfc45ca03281ce4b310e09e3d
Gimmy <975402925@qq.com>
2024-09-24 19:02:15 +0800

    refactor(form): change css var prefix

--- packages/theme/src/form/aurora-theme.js
+++ packages/theme/src/form/aurora-theme.js
@@ -1,6 +1,6 @@
 export const tinyFormAuroraTheme = {
-  'ti-form-valid-padding-vertical': '7px',
-  'ti-form-valid-padding-horizontal': '4px',
-  'ti-form-item-error-border-color': '#f5222d',
-  'ti-form-item-error-bg-color': '#ffffff'
+  'ti-Form-valid-padding-vertical': '7px',
+  'ti-Form-valid-padding-horizontal': '4px',
+  'ti-Form-item-error-border-color': '#f5222d',
+  'ti-Form-item-error-bg-color': '#ffffff'
 }
--- packages/theme/src/form/old-theme.js
+++ packages/theme/src/form/old-theme.js
@@ -1,6 +1,6 @@
 export const tinyFormOldTheme = {
-  'ti-form-display-label-padding-bottom': 'var(--ti-common-space-2x, 8px)',
-  'ti-form-label-top-label-height': 'var(--ti-common-size-height-normal, 28px)',
-  'ti-form-label-top-label-line-height': 'var(--ti-common-size-height-normal, 28px)',
-  'ti-form-label-top-margin-bottom': 'var(--ti-common-space-4x, 16px)'
+  'ti-Form-display-label-padding-bottom': 'var(--ti-common-space-2x, 8px)',
+  'ti-Form-label-top-label-height': 'var(--ti-common-size-height-normal, 28px)',
+  'ti-Form-label-top-label-line-height': 'var(--ti-common-size-height-normal, 28px)',
+  'ti-Form-label-top-margin-bottom': 'var(--ti-common-space-4x, 16px)'
 }
