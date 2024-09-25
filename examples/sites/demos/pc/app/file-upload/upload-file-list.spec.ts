c3f5e83d59e6c6b5ce4213701f248551e918285d
chenxi <2465950588@qq.com>
2024-09-24 23:44:25 -0700

    fix(e2e): 修复e2e测试用例报错

--- examples/sites/demos/pc/app/file-upload/upload-file-list.spec.ts
+++ examples/sites/demos/pc/app/file-upload/upload-file-list.spec.ts
@@ -4,8 +4,9 @@ test('文件列表', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('file-upload#upload-file-list')
 
-  const upload = page.getByRole('button', { name: '点击上传' })
-  const items = page.locator('.tiny-upload-list__item')
+  const container = page.locator('#upload-file-list')
+  const upload = container.getByRole('button', { name: '点击上传' })
+  const items = container.locator('.tiny-upload-list__item')
   const [fileChooser] = await Promise.all([page.waitForEvent('filechooser'), upload.click()])
   const { width, height } = await items.nth(0).boundingBox()
 
@@ -14,10 +15,10 @@ test('文件列表', async ({ page }) => {
   const currentPath = path.resolve(__dirname, '测试.jpg')
 
   await expect(width).toBeGreaterThanOrEqual(height)
-  await expect(height).toBeGreaterThanOrEqual(25, 0)
+  await expect(height).toBeGreaterThanOrEqual(22, 0)
   await expect(items).toHaveCount(2)
   await expect(items).toHaveText([/test1/, /test2/])
   await fileChooser.setFiles(currentPath)
-  await page.getByText('测试.jpg').isVisible()
+  await container.getByText('测试.jpg').isVisible()
   await expect(items).toHaveText([/test1/, /test2/])
 })
