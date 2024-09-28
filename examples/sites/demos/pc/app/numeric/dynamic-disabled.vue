0175947eae99a290a3bc99952e6ff01443e23084
ajaxzheng <894103554@qq.com>
2024-09-28 10:39:07 +0800

    refactor(numeric): refactor numeric theme vars

--- examples/sites/demos/pc/app/numeric/dynamic-disabled.vue
+++ examples/sites/demos/pc/app/numeric/dynamic-disabled.vue
@@ -2,6 +2,9 @@
   <div class="f-r f-box-center">
     <span>全部禁用：</span>
     <tiny-numeric v-model="value1" :disabled="disabled" class="mr20"></tiny-numeric>
+    <tiny-numeric v-model="value1" :disabled="disabled" unit="kg" class="mr20"></tiny-numeric>
+    <tiny-numeric v-model="value1" :disabled="disabled" :controls="false" show-left class="mr20"></tiny-numeric>
+    <tiny-numeric v-model="value1" :disabled="disabled" controls-position="right" class="mr20"></tiny-numeric>
     <span>部分禁用：</span>
     <tiny-numeric v-model="value2" :min="min" :max="max"></tiny-numeric>
   </div>
@@ -25,3 +28,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.mr20 {
+  margin-right: 20px;
+}
+</style>
