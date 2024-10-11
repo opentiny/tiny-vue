fa1ef8bacc4023cdf35ff1cb0ac1993e7b37008f
ajaxzheng <894103554@qq.com>
2024-10-10 19:05:05 -0700

    refactor(theme): [ip-address] refactor ip-address theme vars (#2222)
    * refactor(theme): [ip-address] refactor ip-address theme vars
    
    * refactor(theme): [ip-address] refactor ip-address theme vars

--- examples/sites/demos/pc/app/ip-address/size.spec.ts
+++ examples/sites/demos/pc/app/ip-address/size.spec.ts
@@ -7,7 +7,7 @@ test('ipAddress 输入框大小', async ({ page }) => {
   const demo = page.locator('#size')
   const ipAddress = demo.locator('.tiny-ip-address__input')
 
-  await expect(ipAddress.first()).toHaveCSS('height', '42px')
+  await expect(ipAddress.first()).toHaveCSS('height', '40px')
   await expect(ipAddress.nth(1)).toHaveCSS('height', '32px')
   await expect(ipAddress.nth(2)).toHaveCSS('height', '28px')
   await expect(ipAddress.nth(3)).toHaveCSS('height', '24px')
