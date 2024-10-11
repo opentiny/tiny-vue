ff7bd0d116bfec7125f2b841e8832659602c1223
chenxi-20 <2465950588@qq.com>
2024-10-10 18:59:03 -0700

    refactor(progress): 重构progress主题 (#2234)

--- packages/theme/src/progress/old-theme.js
+++ packages/theme/src/progress/old-theme.js
@@ -1,6 +1,6 @@
 export const tinyProgressOldTheme = {
-  'ti-progress-exception-bg-color': 'var(--ti-common-bg-primary, #c7000b)',
-  'ti-progress-bar-text-margin-left': 'var(--ti-common-space-2x, 8px)',
-  'ti-progress-bar-inner-bg-color': 'var(--ti-common-color-bg-emphasize, #5e7ce0)',
-  'ti-progress-bar-outer-bg-color': 'var(--ti-common-color-bg-disabled, #f5f5f6)'
+  'tv-Progress-bg-color-exception': 'var(--ti-common-bg-primary, #c7000b)',
+  'tv-Progress-right-text-margin-left': 'var(--ti-common-space-2x, 8px)',
+  'tv-Progress-inner-bg-color': 'var(--ti-common-color-bg-emphasize, #5e7ce0)',
+  'tv-Progress-outer-bg-color': 'var(--ti-common-color-bg-disabled, #f5f5f6)'
 }
