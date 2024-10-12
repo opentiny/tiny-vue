7ebf52a75997b9636e45c19f82e212c6e929778a
zhanyini <zhanyini666@163.com>
2024-10-11 18:08:55 -0700

    refactor(select): [select] refactor vue template for select

--- packages/vue/src/select/src/pc.vue
+++ packages/vue/src/select/src/pc.vue
@@ -361,6 +361,7 @@
           </template>
         </tiny-input>
       </slot>
+
       <transition name="tiny-zoom-in-top" @before-enter="handleMenuEnter" @after-leave="doDestroy">
         <tiny-select-dropdown
           ref="popper"
@@ -378,7 +379,6 @@
           >
             <tiny-input
               ref="input"
-              type="text"
               v-model="state.query"
               :placeholder="placeholder"
               @input="debouncedQueryChange"
@@ -429,9 +429,10 @@
             @node-click="treeNodeClick"
             v-bind="treeOp"
           ></tiny-tree>
-          <!-- tiny 新增 可搜索的输入框 -->
+          <!-- tiny 新增 面板搜索 -->
           <tiny-input
             v-if="searchable"
+            input-box-type="underline"
             v-model="state.query"
             :placeholder="t('ui.search.placeholder')"
             class="tiny-select-dropdown__search"
@@ -484,6 +485,7 @@
               </tiny-recycle-scroller>
             </div>
           </template>
+
           <tiny-scrollbar
             v-if="!optimization && !~['grid', 'tree'].indexOf(renderType)"
             ref="scrollbar"
@@ -566,7 +568,7 @@
             "
           >
             <!-- tiny 新增 showEmptyImage功能 -->
-            <div v-if="loadingText || slots.empty">
+            <div v-if="loadingText || slots.empty" class="tiny-select-dropdown__empty-wrap">
               <slot name="empty" v-if="slots.empty"></slot>
               <span v-else-if="showEmptyImage" class="tiny-select-dropdown__empty-images"></span>
               <p class="tiny-select-dropdown__empty" v-else>
@@ -574,15 +576,15 @@
               </p>
             </div>
             <div v-else class="tiny-select-dropdown__loading" :class="{ 'show-loading-icon': loading }">
-              <template v-if="!loading">
-                <span v-if="showEmptyImage" class="tiny-select-dropdown__empty-images"></span>
-                <span v-else class="tiny-select-dropdown__empty"> {{ state.emptyText }}</span>
-              </template>
-
               <component
+                v-if="loading"
                 class="circular"
                 :is="state.designConfig.icons.loadingIcon || 'icon-loading-shadow'"
               ></component>
+              <template v-else>
+                <span v-if="showEmptyImage" class="tiny-select-dropdown__empty-images"></span>
+                <span v-else class="tiny-select-dropdown__empty"> {{ state.emptyText }}</span>
+              </template>
             </div>
           </template>
           <!-- tiny 新增 footer插槽 -->
