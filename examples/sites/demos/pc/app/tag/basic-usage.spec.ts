e4719847d2f04122c1b3e5041115b835171298ca
shenjunjian <40288193@qq.com>
2024-10-15 00:11:25 -0700

    fix(tag): fix e2e

--- examples/sites/demos/pc/app/tag/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/tag/basic-usage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('配置式标签', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).not.toBeNull())
-  await page.goto('tag#content')
+  await page.goto('tag#basic-usage')
 
   const tag = page.locator('.all-demos-container').getByText('配置式标签')
 
