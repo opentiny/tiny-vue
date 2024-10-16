3b48111de055e82799818b865f966ebffb700de4
ajaxzheng <894103554@qq.com>
2024-10-16 14:26:12 +0800

    feat: delete useless files (#2269)

--- packages/theme/src/action-menu/aurora-theme.js
+++
@@ -1,4 +0,0 @@
-export const tinyActionMenuAuroraTheme = {
-  'ti-action-menu-text-color': 'var(--ti-common-color-text-highlight)',
-  'ti-action-menu-item-hover-text-color': 'var(--ti-common-color-text-link-hover)'
-}
--- packages/theme/src/form/aurora-theme.js
+++
@@ -1,6 +0,0 @@
-export const tinyFormAuroraTheme = {
-  'ti-Form-valid-padding-vertical': '7px',
-  'ti-Form-valid-padding-horizontal': '4px',
-  'ti-Form-item-error-border-color': '#f5222d',
-  'ti-Form-item-error-bg-color': '#ffffff'
-}
