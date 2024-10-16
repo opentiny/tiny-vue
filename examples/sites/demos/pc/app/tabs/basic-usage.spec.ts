a8fccf62bd1fdc2e64b1d2401a458ff0123461c4
chenxi-20 <2465950588@qq.com>
2024-10-15 20:45:36 -0700

    refactor(tabs): [tabs] refactor the Tabs Theme (#2270)
    * refactor(tabs): [tabs] 重构tabs主题
    
    * refactor(tabs): [tabs] 修复测试用例报错

--- examples/sites/demos/pc/app/tabs/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/tabs/basic-usage.spec.ts
@@ -15,13 +15,13 @@ test('基本用法', async ({ page }) => {
   await expect(tabItems).toHaveCount(4)
   await expect(tabItems).toHaveClass([/is-top/, /is-top is-active/, /is-top/, /is-top/])
   await expect(content).toHaveText(/数据组件/)
-  await expect(activeBar).toHaveCSS('width', '56px')
+  await expect(activeBar).toHaveCSS('width', '64px')
   await expect(activeBar).toHaveCSS('transform', 'matrix(1, 0, 0, 1, 96, 0)')
   await item1.click()
   await expect(item1).toHaveClass(/is-active/)
-  await expect(activeBar).toHaveCSS('width', '56px')
+  await expect(activeBar).toHaveCSS('width', '64px')
   await expect(activeBar).toHaveCSS('transform', 'matrix(1, 0, 0, 1, 0, 0)')
-  await expect(item1).toHaveCSS('color', 'rgb(94, 124, 224)')
+  await expect(item1).toHaveCSS('color', 'rgb(25, 25, 25)')
   await expect(content).toHaveText(/表单组件/)
 
   // 禁用
