e4ffe3cad29a8a4eee2db53efc5f7b3ab051706f
James-9696 <hujm969696@163.com>
2024-10-24 00:40:13 -0700

    fix(dialog-box): [dialog-box] modify dialog-box of design

--- examples/sites/demos/pc/app/dialog-box/hidden-header.vue
+++ examples/sites/demos/pc/app/dialog-box/hidden-header.vue
@@ -2,7 +2,7 @@
   <div>
     <tiny-button @click="boxVisibility = true">无标题</tiny-button>
     <tiny-dialog-box :show-header="false" v-model:visible="boxVisibility" title="消息" width="30%">
-      <span>隐藏弹窗的标题部分</span>
+      <div class="body-class">隐藏弹窗的标题部分</div>
       <template #footer>
         <tiny-button type="primary" @click="boxVisibility = false">确 定</tiny-button>
       </template>
@@ -25,3 +25,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.body-class {
+  margin-top: 32px;
+}
+</style>
