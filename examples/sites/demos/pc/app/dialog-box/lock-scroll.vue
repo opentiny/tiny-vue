405a2781b15e7a92b7896aba74d3b60b4df222bd
James-9696 <hujm969696@163.com>
2024-10-24 00:47:07 -0700

    fix: modify demo

--- examples/sites/demos/pc/app/dialog-box/lock-scroll.vue
+++ examples/sites/demos/pc/app/dialog-box/lock-scroll.vue
@@ -2,7 +2,7 @@
   <div>
     <tiny-button @click="openDlg(true)" type="info">允许滚动背景</tiny-button>
     <tiny-button @click="openDlg(false)">不允许滚动背景</tiny-button>
-    <tiny-dialog-box :lock-scroll="false" v-model:visible="visible1" title="消息" width="30%">
+    <tiny-dialog-box :lock-scroll="false" v-model:visible="visible1" title="消息" width="30%" @close="closeDlg(true)">
       <span>允许被遮罩内容的滚动</span>
       <template #footer>
         <tiny-button type="primary" @click="closeDlg(true)">确 定</tiny-button>
@@ -35,21 +35,21 @@ export default {
     openDlg(isScroll) {
       if (isScroll) {
         document.body.style.overflow = 'auto'
-        document.body.style.height = '200vh'
-        this.visible1 = true
+        document.body.style.height = '130vh'
+        visible1.value = true
       } else {
-        document.body.style.height = '200vh'
-        this.visible2 = true
+        document.body.style.overflow = 'hidden'
+        document.body.style.height = '100vh'
+        visible2.value = true
       }
     },
     closeDlg(isScroll) {
       if (isScroll) {
         document.body.style.overflow = 'hidden'
         document.body.style.height = '100vh'
-        this.visible1 = false
+        visible1.value = false
       } else {
-        document.body.style.height = '100vh'
-        this.visible2 = false
+        visible2.value = false
       }
     }
   }
