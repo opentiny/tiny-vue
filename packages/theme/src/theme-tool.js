445402079a1f9ca4088b3012a0fd02970b8a2134
申君健 <40288193@qq.com>
2024-10-24 16:48:03 +0800

    refactor(theme-tool): Reimplementing the theme-tool method (#2369)
    * fix(theme): fix
    
    * refactor(theme-tool): refactor theme-tool Class, adopt style by adoptedStyleSheets
    
    * fix(theme): fix
    
    * refactor(theme-tool): refactor theme-tool done
    
    * fix(theme): fix
    
    * fix(theme-tool): support multi target

--- packages/theme/src/theme-tool.js
+++ packages/theme/src/theme-tool.js
@@ -1,1 +1,40 @@
-export default class TinyThemeTool {}
+import OldTheme from './old-theme-index.js'
+
+export { OldTheme }
+
+/**
+ * 动态切换文档或影子根节点的样式类
+ */
+export default class TinyThemeTool {
+  constructor(theme) {
+    this.loaded = {} // 缓存已加载的样式
+    if (theme) {
+      this.changeTheme(theme)
+    }
+  }
+
+  changeTheme(theme, target = document) {
+    let loadedSheet = this.loaded[target]
+    if (!loadedSheet) {
+      loadedSheet = new CSSStyleSheet()
+      target.adoptedStyleSheets = [...target.adoptedStyleSheets, loadedSheet]
+      this.loaded[target] = loadedSheet
+    }
+
+    if (theme && (theme.data || theme.css)) {
+      let cssContent = ''
+      if (theme.data && typeof theme.data === 'object') {
+        cssContent = Object.keys(theme.data)
+          .map((key) => `--${key}: ${theme.data[key]}; `)
+          .join('')
+
+        cssContent = `:root{${cssContent}}`
+      }
+      if (theme.css && typeof theme.css === 'string') {
+        cssContent += theme.css
+      }
+
+      loadedSheet.replaceSync(cssContent)
+    }
+  }
+}
