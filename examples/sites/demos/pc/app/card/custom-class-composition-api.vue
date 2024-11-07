247c07371a905d38e2b003879d2d12dac99f243e
You-Hw-Y <1664677472@qq.com>
2024-11-06 22:58:15 -0800

    feat(card): [card] Simplification and optimization of card components

--- examples/sites/demos/pc/app/card/custom-class-composition-api.vue
+++ examples/sites/demos/pc/app/card/custom-class-composition-api.vue
@@ -1,13 +1,6 @@
 <template>
-  <div class="card-wrap">
-    <tiny-card title="这是卡片标题" custom-class="my-card">
-      <p>
-        这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，
-        这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，
-      </p>
-    </tiny-card>
-    <br />
-    <tiny-card title="这是卡片标题" :options="options" height="100px">
+  <div>
+    <tiny-card title="这是卡片标题" custom-class="demo-card-custom-class">
       <p>
         这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，
         这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，这是一段长文本内容，
@@ -18,37 +11,11 @@
 
 <script setup>
 import { TinyCard } from '@opentiny/vue'
-import { IconDel, IconWriting, IconAscending, IconShare } from '@opentiny/vue-icon'
-import { ref } from 'vue'
-
-const options = ref([
-  {
-    text: '删除列表',
-    icon: IconDel()
-  },
-  {
-    text: '编辑',
-    icon: IconWriting(),
-    disabled: true
-  },
-  {
-    text: '排序',
-    icon: IconAscending()
-  },
-  {
-    text: '分享',
-    icon: IconShare(),
-    disabled: true
-  }
-])
 </script>
 
 <style scoped>
-.card-wrap {
-  background: #f5f5f5;
-  padding: 16px;
-}
-.my-card {
-  background-color: rgba(0, 0, 0, 0.1);
+.demo-card-custom-class {
+  padding: 20px;
+  background-color: #f0f0f0;
 }
 </style>
