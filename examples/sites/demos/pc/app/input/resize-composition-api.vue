c6e57b5fd447c0db94ed1a8cf42d7b3b85642943
ajaxzheng <894103554@qq.com>
2024-12-02 16:42:33 +0800

    test(e2e): fix input、link、popeditor error e2e test

--- examples/sites/demos/pc/app/input/resize-composition-api.vue
+++ examples/sites/demos/pc/app/input/resize-composition-api.vue
@@ -5,14 +5,16 @@
     <tiny-input type="textarea" v-model="input" resize="both" placeholder="resize = both"></tiny-input>
     <tiny-input type="textarea" v-model="input" resize="horizontal" placeholder="resize = horizontal"></tiny-input>
     <tiny-input type="textarea" v-model="input" resize="vertical" placeholder="resize = vertical"></tiny-input>
-    <p>autosize</p>
-    <tiny-input
-      type="textarea"
-      v-model="textarea"
-      placeholder="autosize = { minRows: 2, maxRows: 3 }"
-      :autosize="{ minRows: 2, maxRows: 3 }"
-    ></tiny-input>
-    <tiny-input type="textarea" v-model="textarea" placeholder="autosize" autosize></tiny-input>
+    <div class="auto-size">
+      <p>autosize</p>
+      <tiny-input
+        type="textarea"
+        v-model="textarea"
+        placeholder="autosize = { minRows: 2, maxRows: 3 }"
+        :autosize="{ minRows: 2, maxRows: 3 }"
+      ></tiny-input>
+      <tiny-input type="textarea" v-model="textarea" placeholder="autosize" autosize></tiny-input>
+    </div>
     <p>hover-expand</p>
     <tiny-input
       class="expand"
