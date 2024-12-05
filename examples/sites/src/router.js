e81f11cf80bd1ad7d6e9bbcdc9934030fc136c87
ajaxzheng <894103554@qq.com>
2024-12-05 11:43:05 +0800

    feat: change default router link (#2613)
    * feat: change default router link
    
    * feat: change default router link

--- examples/sites/src/router.js
+++ examples/sites/src/router.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/views/layout/layout.vue'
-import { LANG_KEY, LANG_PATH_MAP, ZH_CN_LANG, CURRENT_THEME_KEY, THEME_ROUTE_MAP, SMB_THEME } from './const'
-import { $local } from './tools/storage'
+import { LANG_PATH_MAP, ZH_CN_LANG, THEME_ROUTE_MAP, DEFAULT_THEME } from './const'
 
 const Components = () => import('@/views/components/components.vue')
 const Docs = () => import('@/views/docs/docs.vue')
@@ -35,10 +34,8 @@ let routes = [
   {
     path: '/:pathMatch(.*)*',
     redirect: () => {
-      const lang = $local[LANG_KEY]
-      const langPath = LANG_PATH_MAP[lang] || LANG_PATH_MAP[ZH_CN_LANG]
-      const themeKey = localStorage.getItem(CURRENT_THEME_KEY)
-      const theme = THEME_ROUTE_MAP[themeKey] || THEME_ROUTE_MAP[SMB_THEME]
+      const langPath = LANG_PATH_MAP[ZH_CN_LANG]
+      const theme = THEME_ROUTE_MAP[DEFAULT_THEME]
       return { path: `${context}${langPath}/${theme}/overview` }
     }
   }
