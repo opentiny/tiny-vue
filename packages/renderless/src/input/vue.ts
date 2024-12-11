fcf3016e91dfa8e6da68798232b5a7803a79a4b6
申君健 <40288193@qq.com>
2024-12-11 11:23:21 +0800

    fix(input):  fix input's autofocus not work (#2615)

--- packages/renderless/src/input/vue.ts
+++ packages/renderless/src/input/vue.ts
@@ -416,6 +416,10 @@ export const renderless = (
       api.setShowMoreBtn(true)
       on(window, 'resize', api.setShowMoreBtn)
     }
+
+    if (vm.$attrs.autofocus) {
+      api.focus()
+    }
   })
 
   onBeforeUnmount(() => {
