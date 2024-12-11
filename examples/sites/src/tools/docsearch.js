c9991b2c54829ef42d8437d07d4053fb2496364a
ajaxzheng <894103554@qq.com>
2024-12-11 11:52:36 +0800

    feat: support global search shortcut keys to search multiple websites (#2637)

--- examples/sites/src/tools/docsearch.js
+++ examples/sites/src/tools/docsearch.js
@@ -1,26 +1,13 @@
-const HIT_CLASS = 'DocSearch-Hit'
-
-const findUrlLink = (target) => {
-  if (target?.nodeName?.toLocaleLowerCase?.() === 'a') {
-    return target.getAttribute('href')
-  } else if (target?.parentElement) {
-    return findUrlLink(target.parentElement)
-  }
-}
-
-const isAlgoliaHitDom = (dom) =>
-  dom?.className?.includes?.(HIT_CLASS) || dom?.parentElement?.className?.includes?.(HIT_CLASS)
+const OPEN_TINY_URL = 'https://opentiny.design'
 
 export const doSearchEverySite = () => {
-  window.addEventListener('click', (event) => {
-    const target = event.target
-    if (isAlgoliaHitDom(target)) {
-      const openUrl = findUrlLink(target)
-      if (openUrl) {
-        const urlObj = new URL(openUrl)
-        event.preventDefault()
-        window.location.href = openUrl.replace(urlObj.origin, window.location.origin)
+  window.handleGlobalSearchData = (resolve) => {
+    return (data) => {
+      if (typeof data.content === 'string') {
+        data.content = data.content.replaceAll(OPEN_TINY_URL, window.location.origin)
       }
+
+      resolve(data)
     }
-  })
+  }
 }
