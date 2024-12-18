e1b7f0a0dc3467a3c60018b836c4ee9e1114bc24
shenjunjian <40288193@qq.com>
2024-12-17 23:35:02 -0800

    fix(unplugin-tiny-vue): fix error TinyVueResolver name

--- internals/unplugin-tiny-vue/example/vite.config.js
+++ internals/unplugin-tiny-vue/example/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import Inspect from 'vite-plugin-inspect'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { TinyVueAutoImportResolver } from '../dist/index.js'
+import { TinyVueResolver } from '../dist/index.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +11,10 @@ export default defineConfig({
     vue(),
     Inspect(),
     Components({
-      resolvers: [TinyVueAutoImportResolver]
+      resolvers: [TinyVueResolver]
     }),
     AutoImport({
-      resolvers: [TinyVueAutoImportResolver]
+      resolvers: [TinyVueResolver]
     })
   ],
   define: {
