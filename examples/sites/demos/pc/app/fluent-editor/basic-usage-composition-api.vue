0908b0f4345aec9f42feaf0c8510a3c92169fa91
Kagol <kagol@sina.com>
2024-12-27 16:34:04 +0800

    docs(fluent-editor): fixed text exceeding problem caused by too long content in the fluent-editor component demo

--- examples/sites/demos/pc/app/fluent-editor/basic-usage-composition-api.vue
+++ examples/sites/demos/pc/app/fluent-editor/basic-usage-composition-api.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <tiny-fluent-editor v-model="content"></tiny-fluent-editor>
-    内容：<br />
-    {{ content }}
+    <div class="fluent-editor-demo__basic-usage__content">
+      内容：<br />
+      {{ content }}
+    </div>
   </div>
 </template>
 
@@ -12,3 +14,11 @@ import { TinyFluentEditor } from '@opentiny/vue'
 
 const content = ref('{"ops":[{"insert":"Hello "},{"attributes":{"bold":true},"insert":"FluentEditor"},{"insert":"!"}]}')
 </script>
+
+<style scoped>
+.fluent-editor-demo__basic-usage__content {
+  margin: 16px 0;
+  line-height: 1.5;
+  word-break: break-word;
+}
+</style>
--- examples/sites/demos/pc/app/fluent-editor/basic-usage.vue
+++ examples/sites/demos/pc/app/fluent-editor/basic-usage.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <tiny-fluent-editor v-model="content"></tiny-fluent-editor>
-    内容：<br />
-    {{ content }}
+    <div class="fluent-editor-demo__basic-usage__content">
+      内容：<br />
+      {{ content }}
+    </div>
   </div>
 </template>
 
@@ -20,3 +22,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.fluent-editor-demo__basic-usage__content {
+  margin: 16px 0;
+  line-height: 1.5;
+  word-break: break-word;
+}
+</style>
