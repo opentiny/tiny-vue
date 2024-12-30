6a40844657379c5345f78f71ec52a3369aa24f52
James-9696 <hujm969696@163.com>
2024-12-29 22:59:50 -0800

    fix: modify e2e test error

--- examples/sites/demos/pc/app/statistic/statistic-slot.spec.ts
+++ examples/sites/demos/pc/app/statistic/statistic-slot.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('插槽用法', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('statistic#statistic-slot')
-  const desc = page.locator('.tiny-statistic__description')
+  const desc = page.locator('.tiny-statistic__description-margin')
   await page
     .locator('div')
     .filter({ hasText: /^存储总量已使用容量\(GB\)10,010,258GB$/ })
