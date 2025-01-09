bc9bc4789c2cd9f8172fed9a29c4bd6fec2a8c84
James-9696 <hujm969696@163.com>
2025-01-08 18:17:27 -0800

    fix: modify e2e test cases for the divider component

--- examples/sites/demos/pc/app/divider/divider-type.spec.ts
+++ examples/sites/demos/pc/app/divider/divider-type.spec.ts
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test'
 test('分割线类型', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('divider#divider-type')
-  const dividerText = page.locator('.tiny-divider__text')
-  await expect(dividerText.first()).toHaveText('solid线')
-  await expect(dividerText.nth(1)).toHaveText('dashed线')
-  await expect(dividerText.nth(2)).toHaveText('dotted线')
+  const dividerText = page.locator('.tiny-divider--default')
+  await expect(dividerText.first()).toHaveCSS('border-top-style', 'solid')
+  await expect(dividerText.nth(1)).toHaveCSS('border-top-style', 'dashed')
+  await expect(dividerText.nth(2)).toHaveCSS('border-top-style', 'dotted')
 })
