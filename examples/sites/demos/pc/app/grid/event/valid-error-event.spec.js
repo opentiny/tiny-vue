f2fb3b42890ad9680d30d5728f0361449fbb74c4
Gimmy <975402925@qq.com>
2025-01-13 19:55:26 +0800

    test(grid): fix grid e2e test case

--- examples/sites/demos/pc/app/grid/event/valid-error-event.spec.js
+++ examples/sites/demos/pc/app/grid/event/valid-error-event.spec.js
@@ -4,12 +4,7 @@ test('校验不通过事件', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-event#event-valid-error-event')
   await page.getByText('GFD 科技 YX 公司').first().click()
-  await page
-    .getByRole('row', {
-      name: '1 华东区 福州 公司技术和研发实力雄厚，是国家 863 项目的参与者，并被政府认定为“高新技术企业”。'
-    })
-    .getByRole('textbox')
-    .clear()
+  await page.locator('.tiny-grid-default-input').clear()
   await page.waitForTimeout(400)
   await expect(page.getByText('触发校验不通过事件').first()).toBeVisible()
 })
--- examples/sites/demos/pc/app/time-picker/format.spec.ts
+++ examples/sites/demos/pc/app/time-picker/format.spec.ts
@@ -12,21 +12,21 @@ test('时间格式化', async ({ page }) => {
 
   await page.getByRole('textbox', { name: '19:40:00 pm' }).click()
   await page.waitForTimeout(100)
-  await page.getByRole('listitem').filter({ hasText: '08 pm' }).first().locator('span').click()
+  await page.locator('.tiny-time-panel[visible=true]').getByText('08 pm').click()
   await page.getByRole('button', { name: '确定' }).click()
   await page.waitForTimeout(100)
   await expect(page.getByRole('textbox', { name: '20:40:00 pm' })).toBeVisible()
 
   await page.getByRole('textbox', { name: '8:40:0 PM' }).click()
   await page.waitForTimeout(100)
-  await page.getByRole('listitem').filter({ hasText: '09 PM' }).locator('span').click()
+  await page.locator('.tiny-time-panel[visible=true]').getByText('09 PM').click()
   await page.getByRole('button', { name: '确定' }).click()
   await page.waitForTimeout(100)
   await expect(page.getByRole('textbox', { name: '9:40:0 PM' })).toBeVisible()
 
   await page.getByRole('textbox', { name: '21:40:0 PM' }).click()
   await page.waitForTimeout(100)
-  await page.getByRole('listitem').filter({ hasText: '10 PM' }).locator('span').click()
+  await page.locator('.tiny-time-panel[visible=true]').getByText('10 PM').click()
   await page.getByRole('button', { name: '确定' }).click()
   await page.waitForTimeout(100)
   await expect(page.getByRole('textbox', { name: '22:40:0 PM' })).toBeVisible()
@@ -34,14 +34,14 @@ test('时间格式化', async ({ page }) => {
   // value-format: 选中值的格式
   await page.getByRole('textbox', { name: '18:30:00' }).click()
   await page.waitForTimeout(100)
-  await page.getByRole('listitem').filter({ hasText: '19' }).first().click()
+  await page.locator('.tiny-time-panel[visible=true]').getByText('19').first().click()
   await page.getByRole('button', { name: '确定' }).click()
   await page.waitForTimeout(100)
   await expect(page.getByRole('textbox', { name: '19:30:00' })).toBeVisible()
 
   // picker-options.format: 下拉框中显示的格式
-  const timePickerHour = page.getByRole('listitem').filter({ hasText: '18' }).first()
-  const timePickerMinute = page.getByRole('listitem').filter({ hasText: '40' }).first()
+  const timePickerHour = page.locator('.tiny-time-panel[visible=true]').getByText('18').first()
+  const timePickerMinute = page.locator('.tiny-time-panel[visible=true]').getByText('40').first()
   const timePickerSecond = page.locator('.of-hidden > div:nth-child(12) .tiny-scrollbar').nth(2)
   await page.getByRole('textbox', { name: '18:40:00' }).click()
   await page.waitForTimeout(200)
