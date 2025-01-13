0a9eabd50fa9ead10f210e1a24409c1a85bbd04b
gimmyhehe <975402925@qq.com>
2025-01-13 20:01:28 +0800

    test: update @playwright/test to 1.49.x and fix e2e case (#2792)
    * test: update @playwright/test to 1.49.x
    
    * test(grid): fix grid e2e test case
    
    * test(grid): fix grid e2e test case

--- examples/sites/demos/pc/app/grid/edit/status-of-editing.spec.js
+++ examples/sites/demos/pc/app/grid/edit/status-of-editing.spec.js
@@ -4,12 +4,7 @@ test('开启编辑状态', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-edit#edit-status-of-editing')
   await page.getByText('GFD 科技 YX 公司').first().click()
-  await page
-    .getByRole('row', {
-      name: '1 华东区 福州 公司技术和研发实力雄厚，是国家 863 项目的参与者，并被政府认定为“高新技术企业”。'
-    })
-    .getByRole('textbox')
-    .fill('sdfdf')
+  await page.locator('.tiny-grid-default-input').fill('sdfdf')
   await page.getByRole('heading', { name: '开启编辑状态：' }).click()
   await expect(page.getByRole('cell', { name: 'sdfdf' }).nth(0)).toHaveClass(/col__valid-success/)
   await expect(page.getByRole('cell', { name: 'sdfdf' }).nth(1)).not.toHaveClass(/col__valid-success/)
--- examples/sites/demos/pc/app/grid/edit/trigger-mode-hm-editing.spec.js
+++ examples/sites/demos/pc/app/grid/edit/trigger-mode-hm-editing.spec.js
@@ -2,18 +2,12 @@ import { test, expect } from '@playwright/test'
 
 test('手动触发编辑', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
+  const demo = page.locator('#edit-trigger-mode-hm-editing')
   await page.goto('grid-edit#edit-trigger-mode-hm-editing')
   await page.getByText('GFD 科技 YX 公司').first().click()
   await expect(page.getByText('GFD 科技 YX 公司').first()).toBeVisible()
-  await page
-    .getByRole('row', {
-      name: '1 GFD 科技 YX 公司 华东区 福州 公司技术和研发实力雄厚，是国家 863 项目的参与者，并被政府认定为“高新技术企业”。 编辑'
-    })
-    .getByRole('button', { name: '编辑' })
-    .click()
-  await expect(
-    page.getByRole('row', { name: '1 华东区 福州 公司技术和研发实力雄厚，是国家 863' }).getByRole('textbox')
-  ).toBeVisible()
+  await demo.locator('.tiny-grid-body__row').first().getByRole('button', { name: '编辑' }).click()
+  await expect(page.locator('.tiny-grid-default-input')).toBeVisible()
 
   await page.getByRole('row', { name: '3 RFV' }).getByRole('button').click()
   await expect(page.getByRole('combobox')).toBeVisible()
