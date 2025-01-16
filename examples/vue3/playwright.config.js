6531347a5abb0290bd10016cd55132ffc6698f52
ajaxzheng <894103554@qq.com>
2025-01-16 11:20:05 +0800

    ci(workflows): the full test case pipeline adds the ability to configure URL addresses

--- examples/vue3/playwright.config.js
+++ examples/vue3/playwright.config.js
@@ -4,9 +4,9 @@ import Config from '@opentiny-internal/playwright-config'
  *  本地开发环境链接：'http://localhost:7130/pc/'
  *  本地官网链接：'http://localhost:3101'
  */
-const origin = 'http://localhost:3101'
+const origin = 'http://localhost:3101/tiny-vue'
 
-const baseURL = `${origin}/tiny-vue/zh-CN/os-theme/components/`
+const baseURL = `${origin}/zh-CN/os-theme/components/`
 const devServerCommon = 'pnpm run -w site'
 
 export default Config({
---
+++ internals/cli/src/commands/release/releaseE2EConfig.ts
@@ -0,0 +1,13 @@
+import fs from 'fs-extra'
+import { pathFromWorkspaceRoot } from '../../config/vite'
+
+const pathFromExamples = (...args) => pathFromWorkspaceRoot('examples', ...args)
+
+const playWrightConfigPath = pathFromExamples('vue3/playwright.config.js')
+
+export const releaseE2EConfig = ({ origin }) => {
+  const content = fs.readFileSync(playWrightConfigPath).toString('UTF-8' as BufferEncoding)
+  const result = origin ? content.replace('http://localhost:3101/tiny-vue', origin) : content
+
+  fs.writeFileSync(playWrightConfigPath, result)
+}
