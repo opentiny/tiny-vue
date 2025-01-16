892dba7f7e630d999a83eaea68c71a57104bf554
ajaxzheng <894103554@qq.com>
2025-01-16 14:21:29 +0800

    fix(e2e): fix all e2e test error

--- internals/cli/src/commands/release/releaseE2EConfig.ts
+++ internals/cli/src/commands/release/releaseE2EConfig.ts
@@ -7,7 +7,9 @@ const playWrightConfigPath = pathFromExamples('vue3/playwright.config.js')
 
 export const releaseE2EConfig = ({ origin }) => {
   const content = fs.readFileSync(playWrightConfigPath).toString('UTF-8' as BufferEncoding)
-  const result = origin ? content.replace('http://localhost:3101/tiny-vue', origin) : content
+  const result = origin
+    ? content.replace('http://localhost:3101/tiny-vue', origin).replace('pnpm run -w site', '')
+    : content
 
   fs.writeFileSync(playWrightConfigPath, result)
 }
