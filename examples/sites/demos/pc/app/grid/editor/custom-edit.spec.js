ccdef5516561eb285e83af398c7d1a2c77338cf0
shenjunjian <40288193@qq.com>
2025-01-16 00:38:22 -0800

    fix(test): fix

--- examples/sites/demos/pc/app/grid/editor/custom-edit.spec.js
+++ examples/sites/demos/pc/app/grid/editor/custom-edit.spec.js
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test'
 test('多行编辑', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-editor#editor-custom-edit')
-  await expect(page.getByRole('textbox', { name: 'GFD 科技有限公司' })).toBeVisible()
+  await expect(page.getByRole('textbox', { name: 'GFD 科技有限公司', exact: true })).toBeVisible()
 })
