dde891003cf90d79f060e9ca7d690e26462dfb15
ajaxzheng <894103554@qq.com>
2025-01-16 17:15:01 +0800

    fix(grid): [grid] fix grid error e2e test

--- examples/sites/demos/pc/app/grid/editor/custom-edit.spec.js
+++ examples/sites/demos/pc/app/grid/editor/custom-edit.spec.js
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test'
 test('多行编辑', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-editor#editor-custom-edit')
-  await expect(page.getByRole('textbox', { name: 'GFD 科技有限公司', exact: true })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'GFD 科技有限公司' }).getByRole('textbox')).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'WWWW 科技有限公司' }).getByRole('textbox')).toBeVisible()
 })
