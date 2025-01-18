44a146828504037518aacc3d496ea91d2afd9528
wuyiping0628 <1106773985@qq.com>
2025-01-18 01:42:44 -0800

    feat(utils): extract the functions from the type to the tils package

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,11 +1,15 @@
 import xss from './xss'
 import log from './log'
 import crypt from './crypt'
+import global from './global'
+import * as type from './type'
 
-export { xss, log, crypt }
+export { xss, log, crypt, global, type }
 
 export default {
   xss,
   log,
-  crypt
+  crypt,
+  global,
+  type
 }
