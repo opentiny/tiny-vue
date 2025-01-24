e6a412cf30ee478f4cd04b942f7600756de1cc99
shenjunjian <40288193@qq.com>
2025-01-24 01:29:27 -0800

    fix(renderless): fix

--- packages/renderless/src/common/deps/debounce.ts
+++ packages/renderless/src/common/deps/debounce.ts
@@ -12,6 +12,6 @@
 
 import throttle from './throttle'
 
-export function debounce(delay, atBegin, callback?: Function) {
+export default function (delay, atBegin, callback?: Function) {
   return callback === undefined ? throttle(delay, atBegin, false) : throttle(delay, callback, atBegin !== false)
 }
