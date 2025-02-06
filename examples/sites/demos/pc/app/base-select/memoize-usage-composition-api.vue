54e95a72a68436f66958186c609d310906480ee8
shenjunjian <40288193@qq.com>
2025-02-06 04:13:31 -0800

    fix(utils): 替换路径

--- examples/sites/demos/pc/app/base-select/memoize-usage-composition-api.vue
+++ examples/sites/demos/pc/app/base-select/memoize-usage-composition-api.vue
@@ -19,7 +19,7 @@
 <script setup>
 import { ref } from 'vue'
 import { TinyBaseSelect, TinyOption } from '@opentiny/vue'
-import Memorize from '@opentiny/vue-renderless/common/deps/memorize'
+import { Memorize } from '@opentiny/utils'
 
 const MemorizeInstance = new Memorize({ key: 'test456' })
 
