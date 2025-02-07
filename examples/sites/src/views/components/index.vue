a1476bace1c57e1fd407b57e398124dd0920c58a
Gimmy <975402925@qq.com>
2025-02-07 17:34:03 +0800

    fix: optimize components

--- examples/sites/src/views/components/index.vue
+++ examples/sites/src/views/components/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <ComponentDocs />
+  <component :is="componentDocs" />
 </template>
+
 <script setup>
-import ComponentDocs from './components.vue'
+import Saas from './saas.vue'
+import Mobile from './mobile.vue'
+import Pc from './pc.vue'
+
+let componentDocs
+const mode = import.meta.env.VITE_APP_MODE
+const theme = import.meta.env.VITE_TINY_THEME
+if (mode === 'mobile') {
+  componentDocs = Mobile
+} else if (mode === 'pc' && theme === 'saas') {
+  componentDocs = Saas
+} else {
+  componentDocs = Pc
+}
 </script>
---
+++ examples/sites/src/views/components/pc.vue
@@ -0,0 +1,7 @@
+<template>
+  <ComponentDocs />
+</template>
+
+<script setup>
+import ComponentDocs from './components.vue'
+</script>
