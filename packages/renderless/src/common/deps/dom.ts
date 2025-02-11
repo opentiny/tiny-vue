7d8150e748279c82af32fb51b16b24f3a643d30a
shenjunjian <40288193@qq.com>
2025-02-10 18:47:25 -0800

    fix(renderless): restore dom into renderless/common

---
+++ packages/renderless/src/common/deps/dom.ts
@@ -0,0 +1,20 @@
+export {
+  on,
+  off,
+  once,
+  hasClass,
+  addClass,
+  removeClass,
+  getStyle,
+  setStyle,
+  isScroll,
+  getScrollContainer,
+  isInContainer,
+  getDomNode,
+  getScrollTop,
+  stopPropagation,
+  preventDefault,
+  getScrollParent,
+  useScrollParent,
+  isDisplayNone
+} from '@opentiny/utils'
