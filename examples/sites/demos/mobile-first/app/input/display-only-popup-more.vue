2df0c8b49ed247e54dcf5a2510cc6c8707267dd6
Gimmy <975402925@qq.com>
2025-02-13 11:21:19 +0800

    fix(input): [input] fix textarea word wrap style

---
+++ examples/sites/demos/mobile-first/app/input/display-only-popup-more.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo-input">
+    <tiny-input type="textarea" v-model="input" display-only popup-more></tiny-input>
+  </div>
+</template>
+
+<script>
+import { TinyInput } from '@opentiny/vue'
+
+export default {
+  components: {
+    TinyInput
+  },
+  data() {
+    return {
+      input:
+        '表单文本域开启只读模式，\n文本内容超过 5 行自动隐藏。\n表单文本域开启只读模式，\n文本内容超过 5 行自动隐藏。\n表单文本域开启只读模式，\n文本内容超过 5 行自动隐藏。\n表单文本域开启只读模式，\n文本内容超过 5 行自动隐藏。\n表单文本域开启只读模式，\n文本内容超过 5 行自动隐藏'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.demo-input .tiny-textarea {
+  width: 260px;
+}
+</style>
