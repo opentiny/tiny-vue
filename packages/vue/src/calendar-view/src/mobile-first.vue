6ef887296959bb55e1fef6d4bdff72f41e0c590e
James-9696 <hujm969696@163.com>
2025-02-20 00:07:04 -0800

    fix: revise the title to include line breaks in both Chinese and English

--- packages/vue/src/calendar-view/src/mobile-first.vue
+++ packages/vue/src/calendar-view/src/mobile-first.vue
@@ -292,7 +292,7 @@
                 :key="idx"
                 class="py-1.5 h-auto border border-color-border-separator rounded mb-2 shadow-sm"
               >
-                <div class="px-1.5 mb-1.5 border-l-2 border-color-brand">{{ event.title }}</div>
+                <div class="px-1.5 mb-1.5 border-l-2 border-color-brand break-all">{{ event.title }}</div>
                 <div class="mb-1.5 px-2 text-color-text-placeholder">
                   {{ getEventShowTime('start', event, date.value) }} - {{ getEventShowTime('end', event, date.value) }}
                 </div>
