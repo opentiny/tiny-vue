795236e8342a19a350d9f4f37c2ffcd202995c09
James-9696 <hujm969696@163.com>
2025-02-27 01:06:36 -0800

    fix: modifying form validation sorting issues in asynchronous situations

--- packages/renderless/src/form/index.ts
+++ packages/renderless/src/form/index.ts
@@ -179,8 +179,12 @@ export const validate =
           Object.keys(field).forEach((item) => invalidFieldArr.push(item))
         }
 
+        // 排序
+        const sortField = sortFields(state.fields, invalidFields)
+        const sortFieldArr = sortFields(state.fields, invalidFieldArr)
+
         if (typeof callback === 'function' && ++count === state.fields.length) {
-          callback(valid, invalidFields, invalidFieldArr)
+          callback(valid, sortField, sortFieldArr)
         }
       })
     })
@@ -189,6 +193,23 @@ export const validate =
       return promise
     }
   }
+
+const sortFields = (fileds, val) => {
+  const arrField = fileds.map((item) => item.prop)
+  if (Object.prototype.toString.call(val) === '[object Object]') {
+    const keys = Object.keys(val)
+    const sortKeys = keys.sort((a, b) => arrField.indexOf(a) - arrField.indexOf(b))
+    const sortedObject = sortKeys.reduce((acc, index) => {
+      acc[index] = val[index]
+      return acc
+    }, {})
+    return sortedObject
+  }
+  if (Array.isArray(val)) {
+    val.sort((x, y) => arrField.indexOf(x) - arrField.indexOf(y))
+    return val
+  }
+}
 
 export const validateField =
   (state: IFormRenderlessParams['state']) =>
