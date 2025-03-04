b43df41454d7855663a8d11c33e5809bdd9bb93a
You-Hw-Y <1664677472@qq.com>
2025-03-03 17:42:12 -0800

    feat(checkbox): [checkbox] Modify test cases

--- examples/sites/demos/pc/app/checkbox/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/checkbox/basic-usage.spec.ts
@@ -6,7 +6,6 @@ test('基础用法', async ({ page }) => {
   const demo = page.locator('#basic-usage')
   const checkbox = demo.locator('.tiny-checkbox')
   await expect(checkbox).toHaveClass(/is-checked/)
-  await expect(checkbox.locator('.tiny-checkbox__inner svg')).toHaveCSS('fill', 'rgb(20, 118, 255)')
   await checkbox.click()
   await expect(checkbox).not.toHaveClass(/is-checked/)
 })
