88e44e6c145d2757ed73b372392d5b66ea829690
wuyiping0628 <1106773985@qq.com>
2025-03-18 20:18:38 -0700

    fix(text-popup): [text-popup] update dark theme

--- examples/sites/demos/pc/app/text-popup/clear-value.vue
+++ examples/sites/demos/pc/app/text-popup/clear-value.vue
@@ -3,6 +3,8 @@
     <div>value 值：{{ value }}</div>
     <br />
     <tiny-button @click="click">点击清除</tiny-button>
+    <br />
+    <br />
     <tiny-text-popup v-model="value"></tiny-text-popup>
   </div>
 </template>
