05c7bcf93b6a056ce565bfcfbb435f47cde160d9
ajaxzheng <894103554@qq.com>
2025-03-26 17:49:58 +0800

    fix(calendar-view): 调整日历视图工具提示内容的最大高度和溢出处理，以优化显示效果

--- packages/vue/src/calendar-view/src/mobile-first.vue
+++ packages/vue/src/calendar-view/src/mobile-first.vue
@@ -3,13 +3,13 @@
     <tiny-tooltip
       ref="tooltip"
       v-model="state.eventTipVisible"
-      popper-class="absolute max-w-[theme(spacing.80)] max-h-[80vh] overflow-auto"
+      popper-class="absolute max-w-[theme(spacing.80)]"
       :manual="true"
       effect="light"
       placement="right"
     >
       <template #content>
-        <div class="p-2">
+        <div class="p-2 max-h-[80vh] overflow-auto">
           <div class="px-1.5 mb-1.5 border-l-2 border-color-brand">{{ state.eventTipContent.title }}</div>
           <div class="mb-1.5 px-2 text-color-text-placeholder">
             {{ state.eventTipContent.startDay }} {{ state.eventTipContent.startTime }} ~
