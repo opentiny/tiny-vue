6f526a865af6b024fd842b8b556ad52e37cc5e57
James-9696 <hujm969696@163.com>
2025-03-26 20:40:54 -0700

    fix: modify test-e2e

--- examples/sites/demos/pc/app/divider/content-position.spec.ts
+++ examples/sites/demos/pc/app/divider/content-position.spec.ts
@@ -3,7 +3,8 @@ import { test, expect } from '@playwright/test'
 test('分割线文案位置', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('divider#content-position')
-  const left = page.locator('.tiny-divider__text')
+  const demo = page.locator('#content-position')
+  const left = demo.locator('.tiny-divider__text')
   await expect(left.first()).toHaveText('左侧且偏移20%')
   await expect(left.nth(1)).toHaveText('中间')
   await expect(left.nth(2)).toHaveCSS('right', '80px')
