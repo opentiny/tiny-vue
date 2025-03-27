09bcbfd74ea77d4dc2f2b00580c2939bab66c708
wuyiping0628 <1106773985@qq.com>
2025-03-27 05:55:36 -0700

    fix(grid): [grid] update dark theme

--- examples/sites/demos/pc/app/grid/validation/bubbling-composition-api.vue
+++ examples/sites/demos/pc/app/grid/validation/bubbling-composition-api.vue
@@ -101,7 +101,7 @@ function asteriskMethod({ column }) {
 .demo-body {
   height: 400px;
   overflow: scroll;
-  background: var(---tv-color-warn-bg-light, #ffebd1);
+  background: var(--tv-color-warn-bg-light, #ffebd1);
 }
 .demo-body > .demo-container {
   height: 3000px;
