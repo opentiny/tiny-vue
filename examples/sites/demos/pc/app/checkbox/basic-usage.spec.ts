42b14f05801b8d47faf199a7e53dfed87227721e
discreted66 <953831480@qq.com>
2025-04-01 15:10:54 -0700

    fix:用例修复以及图标修改

--- examples/sites/demos/pc/app/checkbox/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/checkbox/basic-usage.spec.ts
@@ -3,8 +3,7 @@ import { test, expect } from '@playwright/test'
 test('基础用法', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('checkbox#basic-usage')
-  const demo = page.locator('#basic-usage')
-  const checkbox = demo.locator('.tiny-checkbox')
+  const checkbox = await page.locator('.tiny-checkbox').first()
   await expect(checkbox).toHaveClass(/is-checked/)
   await expect(checkbox.locator('.tiny-checkbox__inner svg path').first()).toHaveCSS('fill', 'rgb(20, 118, 255)')
   await checkbox.click()
