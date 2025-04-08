64466204e36a5adc80939298a07e3a7a4566cd24
zhanyini <zhanyini666@163.com>
2025-04-07 17:23:16 -0700

    fix(checkbox): [checkbox] fix test

--- examples/sites/demos/pc/app/checkbox/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/checkbox/basic-usage.spec.ts
@@ -5,7 +5,7 @@ test('基础用法', async ({ page }) => {
   await page.goto('checkbox#basic-usage')
   const checkbox = await page.locator('.tiny-checkbox').first()
   await expect(checkbox).toHaveClass(/is-checked/)
-  await expect(checkbox.locator('.tiny-checkbox__inner svg path').nth(1)).toHaveCSS('fill', 'rgb(20, 118, 255)')
+  await expect(checkbox.locator('.tiny-checkbox__inner svg path').nth(0)).toHaveCSS('fill', 'rgb(20, 118, 255)')
   await checkbox.click()
   await expect(checkbox).not.toHaveClass(/is-checked/)
 })
