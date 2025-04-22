03121e02bf1f60c5dad427721e84349450e6eb11
ajaxzheng <894103554@qq.com>
2025-04-22 14:40:24 +0800

    chore: 还原测试数据

--- examples/sites/demos/pc/app/autocomplete/basic-usage-composition-api.vue
+++ examples/sites/demos/pc/app/autocomplete/basic-usage-composition-api.vue
@@ -28,7 +28,7 @@ function createFilter(queryString) {
 function loadAll() {
   return [
     {
-      value: 'GFD 科技 YX 公司1',
+      value: 'GFD 科技 YX 公司',
       address: '福州'
     },
     {
--- packages/vue/src/alert/src/index.ts
+++ packages/vue/src/alert/src/index.ts
@@ -26,7 +26,7 @@ export const $constants = {
     info: 'ui.alert.info',
     warning: 'ui.alert.warning'
   },
-  CONTENT_MAXHEUGHT: 250
+  CONTENT_MAXHEUGHT: 252
 }
 
 export const alertProps = {
