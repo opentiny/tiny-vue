326afe48619dd68b6036cbd2faab0fd1a851dc40
szxc <shuzhuxvchuang@163.com>
2025-05-22 14:21:04 +0800

    fix(text-popup): 更新测试用例中的URL路径为modelValue

--- examples/sites/demos/pc/app/text-popup/modelValue.spec.ts
+++ examples/sites/demos/pc/app/text-popup/modelValue.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('Textpopup 基本用法', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
-  await page.goto('text-popup#value')
+  await page.goto('text-popup#modelValue')
 
   const preview = page.locator('#modelValue')
   const small = preview.locator('input')
