56ea1a25b68e483320802bec6d179b31c2389454
shenjunjian <shenjunjian5@huawei.com>
2025-06-03 10:21:19 +0800

    fix(site): fix 检视意见

--- examples/sites/src/views/components-doc/composition/DifyModelProvider.ts
+++ examples/sites/src/views/components-doc/composition/DifyModelProvider.ts
@@ -29,7 +29,6 @@ export class DifyModelProvider extends BaseModelProvider {
 
     try {
       // 验证请求的messages属性，必须是数组，且每个消息必须有role\content属性
-      // const { onData, onDone, onError } = handler
       const lastMessage = request.messages[request.messages.length - 1].content
 
       // 模拟异步流式响应
@@ -47,7 +46,6 @@ export class DifyModelProvider extends BaseModelProvider {
           inputs: {
             sessionId: window.$sessionId
           }
-          // conversation_id:''
         })
       })
 
