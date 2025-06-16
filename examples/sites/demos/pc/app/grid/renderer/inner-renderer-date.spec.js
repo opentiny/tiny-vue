10345e903ceb58d9d41ba9fb333863e65f77ec13
Gimmy <975402925@qq.com>
2025-06-16 20:45:25 +0800

    docs(grid): fix demo date format error

--- examples/sites/demos/pc/app/grid/renderer/inner-renderer-date.spec.js
+++ examples/sites/demos/pc/app/grid/renderer/inner-renderer-date.spec.js
@@ -4,6 +4,6 @@ test('日期渲染器', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-renderer#renderer-inner-renderer-date')
   await expect(page.getByText('-07-02').first()).toBeVisible()
-  await expect(page.getByText('-02-15').first()).toBeVisible()
-  await expect(page.getByText('-02-15').nth(1)).toBeVisible()
+  await expect(page.getByText('02/07/2024').first()).toBeVisible()
+  await expect(page.getByText('02/15/2024').first()).toBeVisible()
 })
