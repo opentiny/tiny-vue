51b04bd79564d763ea9a072385ccc90faada35d0
Kagol <kagol@sina.com>
2025-06-18 17:10:07 +0800

    fix: fix typo error

--- packages/vue-icon/src/performance/index.ts
+++ packages/vue-icon/src/performance/index.ts
@@ -10,6 +10,6 @@
  *
  */
 import { svg } from '@opentiny/vue-common'
-import Performance from '@opentiny/vue-theme/svgs/performanc.svg'
+import Performance from '@opentiny/vue-theme/svgs/performance.svg'
 
 export default () => svg({ name: 'IconPerformance', component: Performance })()
