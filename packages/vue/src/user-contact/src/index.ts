a8066737f6a71ffbb467321c4d92aa55276f6234
shenjunjian <shenjunjian5@huawei.com>
2025-07-28 15:13:07 +0800

    fix(PropType):  fix import of PropType

--- packages/vue/src/user-contact/src/index.ts
+++ packages/vue/src/user-contact/src/index.ts
@@ -10,7 +10,7 @@
  *
  */
 
-import { $props, $setup, $prefix, defineComponent } from '@opentiny/vue-common'
+import { $props, $setup, $prefix, defineComponent, type PropType } from '@opentiny/vue-common'
 import template from 'virtual-template?pc'
 
 export const userContactProps = {
