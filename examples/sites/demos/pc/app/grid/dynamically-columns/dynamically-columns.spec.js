7a077e0bf6d780571c4bfae83a276258b5613d99
Gimmy <975402925@qq.com>
2025-07-30 18:49:35 +0800

    test: fix e2e test case

--- examples/sites/demos/pc/app/grid/dynamically-columns/dynamically-columns.spec.js
+++ examples/sites/demos/pc/app/grid/dynamically-columns/dynamically-columns.spec.js
@@ -3,7 +3,8 @@ import { test, expect } from '@playwright/test'
 test('根据日期动态生成列', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-dynamically-columns#dynamically-columns-dynamically-columns')
-  await page.getByRole('textbox').nth(1).click()
+  const demo = page.locator('#dynamically-columns-dynamically-columns')
+  await demo.locator('.tiny-picker.tiny-date-container').click()
   await page.getByText('12 月').first().click()
   await page.getByText('2 月').nth(2).click()
   await expect(page.getByRole('cell', { name: '12' }).first()).toBeVisible()
