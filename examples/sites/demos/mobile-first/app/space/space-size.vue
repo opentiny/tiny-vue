9275afa9596ce8309751d5ee57d4dd86bef422f8
ynnnny <xiachichi6242@gmail.com>
2025-08-04 01:38:32 +0800

    feat: space组件基础功能

--- examples/sites/demos/mobile-first/app/space/space-size.vue
+++ examples/sites/demos/mobile-first/app/space/space-size.vue
@@ -14,7 +14,7 @@
     </div>
 
     <!-- 使用 dynamic direction 值 -->
-    <tiny-space :size="[rowValue, columnValue]" wrap="true" :direction="direction">
+    <tiny-space :size="[rowValue, columnValue]" :wrap="true">
       <tiny-button style="margin: 0" v-for="n in 15" :key="n">按钮 {{ n }}</tiny-button>
     </tiny-space>
   </div>
