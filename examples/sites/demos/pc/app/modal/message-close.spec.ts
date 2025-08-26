19062bd99b14cb151b7bf26254b1f7e09e8fb45c
James-9696 <hujm969696@163.com>
2025-08-25 21:01:42 -0700

    fix: update e2e-test

--- examples/sites/demos/pc/app/modal/message-close.spec.ts
+++ examples/sites/demos/pc/app/modal/message-close.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('消息的关闭和延时', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('modal#message-close')
-  const content = page.locator('.tiny-modal__text')
   await page.getByRole('button', { name: '消息可关闭' }).click()
-  await expect(content).toHaveText(/5s 后得自动关闭/)
+  const info = page.locator('div').filter({ hasText: '自定义消息的内容可关闭，5s 后得自动关闭' })
+  await expect(info.nth(1)).toHaveText(/5s 后得自动关闭/)
 })
