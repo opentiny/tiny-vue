53b54e42737c7760f5e8bcad7fa622d5f62120ad
ynnnny <xiachichi6242@gmail.com>
2025-09-26 22:08:15 +0800

    feat: fix test

--- packages/vue/src/space/src/mobile-first.vue
+++ packages/vue/src/space/src/mobile-first.vue
@@ -1,39 +1,3 @@
-<!-- <template>
-  <div
-    data-tag="tiny-space"
-    :style="[
-      state.gapStyle,
-      {
-        display: 'flex',
-        flexDirection: direction || 'row',
-        alignItems: align || 'flex-start',
-        justifyContent: justify || 'flex-start',
-        flexWrap: wrap ? 'wrap' : 'nowrap',
-        ...customStyle
-      }
-    ]"
-    :class="customClass"
-  >
-    <template v-for="(child, idx) in orderedChildren" :key="child.key || idx">
-      <component :is="child" />
-    </template>
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent, props, setup } from '@opentiny/vue-common'
-import { renderless, api } from '@opentiny/vue-renderless/space/vue'
-import type { ISpaceApi } from '@opentiny/vue-renderless/types/space.type'
-
-export default defineComponent({
-  name: 'TinySpace',
-  props: [...props, 'size', 'direction', 'align', 'justify', 'wrap', 'order', 'customClass', 'customStyle'],
-  setup(props, context) {
-    return setup({ props, context, renderless, api }) as unknown as ISpaceApi
-  }
-})
-</script> -->
-
 <script lang="ts">
 import { defineComponent, props, setup, h } from '@opentiny/vue-common'
 import { renderless, api } from '@opentiny/vue-renderless/space/vue'
