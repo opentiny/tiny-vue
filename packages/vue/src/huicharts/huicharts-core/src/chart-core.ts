7855e1b8b966dee86a1d860bb8a868a676db11f1
Davont <davontz@icloud.com>
2025-10-14 09:52:59 +0800

    fix: 添加chart实例置空顺序

--- packages/vue/src/huicharts/huicharts-core/src/chart-core.ts
+++ packages/vue/src/huicharts/huicharts-core/src/chart-core.ts
@@ -579,8 +579,8 @@ export default {
     this.afterSetOptionOnce && this.afterSetOptionOnce(this.integrateChart.echartsIns)
   },
   beforeUnmount() {
-    this.integrateChart = null
     if (this.integrateChart && this.integrateChart.uninstall) this.integrateChart.uninstall()
+    this.integrateChart = null
     this.watchToPropsEchartOptions.forEach((unwatch) => {
       unwatch && unwatch()
     })
