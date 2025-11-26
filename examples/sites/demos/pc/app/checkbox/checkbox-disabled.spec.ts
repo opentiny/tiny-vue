f7aec6aa6cc8002680caa88a64fb0ce7a813a1b8
unknown <1987250181@qq.com>
2025-11-26 21:53:27 +0800

    fix(checkbox): [checkbox] fix disabled state style and improve disabled checked behavior test

--- examples/sites/demos/pc/app/checkbox/checkbox-disabled.spec.ts
+++ examples/sites/demos/pc/app/checkbox/checkbox-disabled.spec.ts
@@ -3,10 +3,14 @@ import { test, expect } from '@playwright/test'
 test('禁用复选框', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('checkbox#disabled')
-  const checkbox = await page.locator('.tiny-checkbox').first()
+  const checkbox = page.locator('.tiny-checkbox').first()
+  const inner = checkbox.locator('.tiny-checkbox__inner')
+
   await expect(checkbox).toHaveClass(/is-disabled/)
-  await expect(checkbox.locator('.tiny-checkbox__inner')).toHaveCSS('background-color', 'rgb(245, 245, 245)')
-  await expect(checkbox.locator('.tiny-checkbox__inner')).toHaveCSS('border-color', 'rgb(223, 223, 223)')
+  await expect(checkbox).toHaveClass(/is-checked/)
+
+  await expect(inner).toHaveCSS('background-color', 'rgb(245, 245, 245)')
+  await expect(inner).toHaveCSS('border-color', 'rgb(223, 223, 223)')
   await checkbox.click()
   await expect(checkbox).toHaveClass(/is-checked/)
 })
