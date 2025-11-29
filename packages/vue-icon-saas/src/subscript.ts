5393b72d505ab400e32e06f0d965412d3ec92380
Kagol <kagol@sina.com>
2025-11-29 11:46:54 +0800

    fix: fix build:saas

---
+++ packages/vue-icon-saas/src/subscript.ts
@@ -0,0 +1,5 @@
+import { svg } from '@opentiny/vue-common'
+import Subscript from '@opentiny/vue-theme-saas/svgs/sub-script.svg'
+import SubscriptFilled from '@opentiny/vue-theme-saas/svgs/sub-script-filled.svg'
+
+export default () => svg({ name: 'IconSubscript', component: Subscript, filledComponent: SubscriptFilled })()
