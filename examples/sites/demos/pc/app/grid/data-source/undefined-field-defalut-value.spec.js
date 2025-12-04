dfd14173cf8007cd005a0cdfe8ba2f38395402ba
cenguanming <975402925@qq.com>
2025-12-04 18:46:27 +0800

    fix(grid):  optimze e2e test

--- examples/sites/demos/pc/app/grid/data-source/undefined-field-defalut-value.spec.js
+++ examples/sites/demos/pc/app/grid/data-source/undefined-field-defalut-value.spec.js
@@ -8,6 +8,7 @@ test('缺省数据默认值', async ({ page }) => {
 
   const firstRow = demo.locator('.tiny-grid-body__row:visible').nth(0)
 
+  // 缺省值的修改能够正常显示角标
   await firstRow.locator('td').nth(1).click()
   await firstRow.locator('.tiny-input__inner').click()
   await firstRow.locator('.tiny-input__inner').fill('1')
@@ -18,7 +19,7 @@ test('缺省数据默认值', async ({ page }) => {
   await firstRow.locator('td').nth(1).click()
   await expect(firstRow.locator('td').nth(2)).toHaveClass(/col__valid-success/)
 
-  // 新增行
+  // 新增行能成功进入编辑态
   await demo.locator('.tiny-button').click()
   await firstRow.locator('td').nth(1).click()
   await firstRow.locator('.tiny-input__inner').click()
