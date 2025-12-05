d2bb0c4de02bec42f5d53051438c98c3fe49326a
James-9696 <hujm969696@163.com>
2025-12-04 17:19:10 -0800

    fix: increase the demonstration function of screens of different sizes in the training field

--- examples/sites/saas/main.ts
+++
@@ -1,9 +0,0 @@
-import '../../../packages/theme-saas/src/index.less'
-import '../../../packages/theme-saas/theme/theme.json'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app = createApp(App)
-app.config.globalProperties.tiny_mode = 'mobile-first'
-app.mount('#app')
