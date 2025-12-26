f85d0f76748d8e4cc9dae3fa037c34dc2eb52ee1
discreted66 <953831480@qq.com>
2025-12-26 10:38:20 -0800

    fix:fixE2E

--- examples/sites/demos/pc/app/time-picker/step.spec.ts
+++ examples/sites/demos/pc/app/time-picker/step.spec.ts
@@ -4,11 +4,11 @@ test('步长', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('time-picker#step')
 
-  await page.getByRole('textbox', { name: '18:40:00' }).click()
+  await page.getByLabel('任意时间点').click()
   await page.waitForTimeout(100)
   await page.locator('li').filter({ hasText: '20' }).first().click()
   await page.waitForTimeout(100)
   await page.getByRole('button', { name: '确定' }).click()
   await page.waitForTimeout(100)
-  await expect(page.getByRole('textbox', { name: '20:40:00' })).toBeVisible()
+  await expect(page.getByLabel('任意时间点')).toHaveValue('20:40:00')
 })
