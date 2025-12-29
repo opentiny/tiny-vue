1e219838b8ff52a0fd796c4549d95e8d4c36ed97
James-9696 <hujm969696@163.com>
2025-12-29 00:34:56 -0800

    fix(dialog-box): [dialog-box] add aria-* attribute

--- examples/sites/demos/pc/app/dialog-box/basic-usage.spec.ts
+++ examples/sites/demos/pc/app/dialog-box/basic-usage.spec.ts
@@ -5,22 +5,22 @@ test('dialogBox 基础用法', async ({ page }) => {
   await page.goto('dialog-box#basic-usage')
   const demo = page.locator('#basic-usage')
 
-  await page.getByRole('button', { name: /Dialog/ }).click()
+  await demo.getByRole('button', { name: /Dialog/ }).click()
   await demo.getByRole('button', { name: '确 定' }).click()
 
-  await page.getByRole('button', { name: /Dialog/ }).click()
+  await demo.getByRole('button', { name: /Dialog/ }).click()
   await demo.getByRole('button', { name: '取 消' }).click()
 
   // 点击 x 图标
-  await page.getByRole('button', { name: /Dialog/ }).click()
+  await demo.getByRole('button', { name: /Dialog/ }).click()
   await demo.getByLabel('Close').click()
 
   // 点击遮罩时，关闭 dialogBox
-  await page.getByRole('button', { name: /Dialog/ }).click()
+  await demo.getByRole('button', { name: /Dialog/ }).click()
   await demo.locator('.tiny-dialog-box__wrapper').click()
 
   // 按 ESC 键，关闭 dialogBox
-  await page.getByRole('button', { name: /Dialog/ }).click()
+  await demo.getByRole('button', { name: /Dialog/ }).click()
   await page.locator('body').press('Escape')
   expect(demo.locator('.tiny-dialog-box.is-center > .tiny-dialog-box__header')).toBeHidden()
 })
