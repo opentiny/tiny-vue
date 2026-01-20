3e1858a581fd48c857efaa169a143ade2e2620e7
shenjunjian <40288193@qq.com>
2026-01-20 10:28:36 +0800

    fix(icon): 再次调整重命名文件的格式, 并升级版本号

--- packages/vue-icon/index.ts
+++ packages/vue-icon/index.ts
@@ -533,6 +533,7 @@ import IconYes from './src/yes'
 import IconZipType from './src/zip-type'
 import IconZoomIn from './src/zoom-in'
 import IconZoomOut from './src/zoom-out'
+import IconSubScript from './src/sub-script'
 
 // 双图标
 export { IconEditorEraser, IconEditorEraser as iconEditorEraser }
@@ -1072,10 +1073,7 @@ export { IconYes, IconYes as iconYes }
 export { IconZipType, IconZipType as iconZipType }
 export { IconZoomIn, IconZoomIn as iconZoomIn }
 export { IconZoomOut, IconZoomOut as iconZoomOut }
-
-// 重命名导出
-export const IconSubScript = () => ({ ...IconSubscript(), name: 'IconSubScript', deprecatedBy: 'IconSubscript' })
-export const iconSubScript = IconSubScript
+export { IconSubScript, IconSubScript as iconSubScript }
 
 export default {
   IconEditorEraser,
