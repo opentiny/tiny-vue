0899efccce83220e49f206213ff74f107e8931f1
James-9696 <hujm969696@163.com>
2026-01-20 22:55:11 -0800

    fix: modify e2e test

--- examples/sites/demos/pc/app/popconfirm/slot-message.spec.ts
+++ examples/sites/demos/pc/app/popconfirm/slot-message.spec.ts
@@ -4,9 +4,16 @@ test('自定义消息内容', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('popconfirm#slot-message')
 
+  // 等待页面加载完成
+  await page.waitForLoadState('networkidle')
   const demo = page.locator('#slot-message')
-  const popConfirm = demo.locator('.tiny-popconfirm .tiny-popconfirm__reference .tiny-button')
-  await popConfirm.hover()
-  const popConfirmPopover = page.locator('.tiny-popconfirm-popover__content')
-  await expect(popConfirmPopover).toContainText('这是气泡确认框自定义提示内容文本描述。')
+  const triggerButton = demo.locator('.tiny-popconfirm .tiny-popconfirm__reference .tiny-button')
+  await triggerButton.hover()
+  const popover = page.locator('.tiny-popconfirm-popover[aria-hidden="false"]')
+
+  await expect(popover).toBeVisible()
+  // 验证内容
+  await expect(popover.locator('.tiny-popconfirm-popover__content')).toContainText(
+    '这是气泡确认框自定义提示内容文本描述'
+  )
 })
