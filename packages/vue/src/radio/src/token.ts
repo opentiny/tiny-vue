7c4c413812504f1a0f2d71f346ae43b6a83721bc
James-9696 <hujm969696@163.com>
2026-01-22 03:12:56 -0800

    fix: modify radio mobile display issues

--- packages/vue/src/radio/src/token.ts
+++ packages/vue/src/radio/src/token.ts
@@ -1,5 +1,5 @@
 export const classes = {
-  'radio-default': 'radio inline-flex items-center  leading-4 cursor-pointer sm:flex-row py-px sm:py-0 h-fit',
+  'radio-default': 'radio inline-flex items-center leading-4 cursor-pointer sm:flex-row py-px sm:py-0 h-fit',
   'radio-label-common': 'relative text-center w-7 h-7 mr-2 sm:mr-0',
   'radio-label-size-common': 'sm:w-4 sm:h-4',
   'radio-label-size-medium': 'sm:w-6 sm:h-6',
@@ -38,7 +38,7 @@ export const classes = {
   'pc-show': 'hidden sm:block',
   'mobile-show': 'block sm:hidden',
   'readonly-is-checked': 'sm:m-0 cursor-default',
-  'readonly-is-not-checked': 'sm:hidden cursor-default',
+  'readonly-is-not-checked': 'hidden cursor-default',
   'not-readonly-common': 'mr-5 align-middle sm:[&:last-child]:mr-0',
   'hidden-radio': 'sm:hidden',
   'not-readly-common-label': 'sm:py-0 sm:pl-2 -ml-0.5 sm:ml-0',
