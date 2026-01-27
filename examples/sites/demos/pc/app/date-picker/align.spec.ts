b45d3d107d52c2d693a54032e033d82608198a08
shenjunjian <40288193@qq.com>
2026-01-27 14:30:35 +0800

    fix(e2e): fix date-picker's e2e error

--- examples/sites/demos/pc/app/date-picker/align.spec.ts
+++ examples/sites/demos/pc/app/date-picker/align.spec.ts
@@ -6,38 +6,35 @@ test.use({
 test('[DatePicker] 测试对齐方式', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('date-picker#align')
-  await page.locator('.settings-btn').first().click()
-  await page.locator('label').filter({ hasText: '单示例' }).click()
-  await page.waitForSelector('.demo-date-picker-wrap', { state: 'visible' })
-
+  const demo = page.locator('#align')
   // 左对齐
-  const leftDateInputDom = page.getByRole('textbox').nth(1)
-  const leftDatePanelDom = page.locator('.tiny-picker-panel').nth(2)
+  const leftDateInputDom = demo.locator('.tiny-input__inner').nth(0)
+  const leftDatePanelDom = page.locator('body > .tiny-picker-panel').nth(0)
   const leftClosePanel = page.getByText('左对齐：')
   await page.waitForTimeout(200)
 
   await leftDateInputDom.click()
   await page.waitForTimeout(200)
-  await expect(leftDatePanelDom).toHaveAttribute('x-placement', /bottom-start|top-start/)
+  await expect(leftDatePanelDom).toHaveAttribute('x-placement', 'bottom-start')
   await leftClosePanel.click()
 
   // 居中对齐
-  const centerDateInputDom = page.getByRole('textbox').nth(2)
-  const centerDatePanelDom = page.locator('.tiny-picker-panel').nth(2)
+  const centerDateInputDom = demo.locator('.tiny-input__inner').nth(1)
+  const centerDatePanelDom = page.locator('body > .tiny-picker-panel').nth(1)
   const centerClosePanel = page.getByText('居中对齐：')
 
   await centerDateInputDom.click()
   await page.waitForTimeout(200)
-  await expect(centerDatePanelDom).toHaveAttribute('x-placement', /bottom|top/)
+  await expect(centerDatePanelDom).toHaveAttribute('x-placement', /bottom/)
   await centerClosePanel.click()
 
   // 右对齐
-  const rightDateInputDom = page.getByRole('textbox').nth(3)
-  const rightDatePanelDom = page.locator('.tiny-picker-panel').nth(2)
+  const rightDateInputDom = demo.locator('.tiny-input__inner').nth(2)
+  const rightDatePanelDom = page.locator('body > .tiny-picker-panel').nth(2)
   const rightClosePanel = page.getByText('右对齐：')
 
   await rightDateInputDom.click()
   await page.waitForTimeout(200)
-  await expect(rightDatePanelDom).toHaveAttribute('x-placement', /bottom-end|top-end/)
+  await expect(rightDatePanelDom).toHaveAttribute('x-placement', /bottom-end/)
   await rightClosePanel.click()
 })
