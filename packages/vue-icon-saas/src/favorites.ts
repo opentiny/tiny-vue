2ec52196a00a50b0d28ebc78714eb6e99a41d5f4
shenjunjian <40288193@qq.com>
2026-01-28 14:15:48 +0800

    fix(icon): 构建脚本，生成新图标的js

---
+++ packages/vue-icon-saas/src/favorites.ts
@@ -0,0 +1,5 @@
+import { svg } from '@opentiny/vue-common'
+import Favorites from '@opentiny/vue-theme-saas/svgs/favorites.svg'
+import FavoritesFilled from '@opentiny/vue-theme-saas/svgs/favorites-filled.svg'
+
+export default () => svg({ name: 'IconFavorites', component: Favorites, filledComponent: FavoritesFilled })()
