30cb53c03c43724a9979a7c7ede69ea103ba382c
shenjunjian <40288193@qq.com>
2026-02-10 14:33:02 +0800

    fix(tag-group): fix e2e test of tag-group

--- examples/sites/demos/pc/app/tag-group/basic-usage.spec.js
+++ examples/sites/demos/pc/app/tag-group/basic-usage.spec.js
@@ -6,23 +6,23 @@ test('TagGroup 基础用法', async ({ page }) => {
 
   const tagGroup = page.locator('.tiny-tag-group')
 
-  const tag1 = tagGroup.getByText('标签一')
+  const tag1 = tagGroup.locator('.tiny-tag').nth(0)
   await expect(tag1).toHaveCSS('background-color', 'rgb(245, 245, 245)')
   await expect(tag1).toHaveCSS('color', 'rgb(25, 25, 25)')
 
-  const tag2 = tagGroup.getByText('标签二')
+  const tag2 = tagGroup.locator('.tiny-tag').nth(1)
   await expect(tag2).toHaveCSS('background-color', 'rgb(222, 236, 255)')
   await expect(tag2).toHaveCSS('color', 'rgb(20, 118, 255)')
 
-  const tag3 = tagGroup.getByText('标签三')
+  const tag3 = tagGroup.locator('.tiny-tag').nth(2)
   await expect(tag3).toHaveCSS('background-color', 'rgb(230, 242, 213)')
   await expect(tag3).toHaveCSS('color', 'rgb(92, 179, 0)')
 
-  const tag4 = tagGroup.getByText('标签四')
+  const tag4 = tagGroup.locator('.tiny-tag').nth(3)
   await expect(tag4).toHaveCSS('background-color', 'rgb(255, 235, 209)')
   await expect(tag4).toHaveCSS('color', 'rgb(255, 136, 0)')
 
-  const tag5 = tagGroup.getByText('标签五')
+  const tag5 = tagGroup.locator('.tiny-tag').nth(4)
   await expect(tag5).toHaveCSS('background-color', 'rgb(252, 227, 225)')
   await expect(tag5).toHaveCSS('color', 'rgb(242, 48, 48)')
 })
