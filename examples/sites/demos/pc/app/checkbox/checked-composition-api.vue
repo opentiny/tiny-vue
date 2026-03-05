1d99d624f8ec22fe376264c260a6ea6e0c25f597
James-9696 <hujm969696@163.com>
2026-03-05 00:57:46 -0800

    fix(checkbox): optimized checkbox

--- examples/sites/demos/pc/app/checkbox/checked-composition-api.vue
+++ examples/sites/demos/pc/app/checkbox/checked-composition-api.vue
@@ -1,14 +1,15 @@
 <template>
   <tiny-checkbox-group v-model="checkedData">
-    <tiny-checkbox label="复选框 1" name="name1"></tiny-checkbox>
-    <tiny-checkbox label="复选框 2" name="name2" checked></tiny-checkbox>
-    <tiny-checkbox label="复选框 3" name="name3"></tiny-checkbox>
+    <tiny-checkbox label="复选框 1" name="name1" style="margin-bottom: 8px"></tiny-checkbox>
+    <tiny-checkbox label="复选框 2" name="name2" checked style="margin-bottom: 8px"></tiny-checkbox>
+    <tiny-checkbox label="复选框 3" name="name3" style="margin-bottom: 8px"></tiny-checkbox>
   </tiny-checkbox-group>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { TinyCheckbox, TinyCheckboxGroup } from '@opentiny/vue'
+import '../../../../../../packages/theme/src/adaptation-index.less'
 
 const checkedData = ref(['复选框 1'])
 </script>
