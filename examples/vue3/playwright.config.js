0431df54a8d1f8663a661e02147f54dd5384c6c8
ajaxzheng <894103554@qq.com>
2026-04-01 14:36:56 +0800

    Revert "fix(playwright): modify the test case viewport configuration (#4157)"
    This reverts commit 1e9af22e06a0043a7a686371ab62ef18cd2258fc.

--- examples/vue3/playwright.config.js
+++ examples/vue3/playwright.config.js
@@ -1,5 +1,4 @@
 import Config from '@opentiny-internal/playwright-config'
-import { defineConfig } from '@playwright/test'
 
 /**
  *  本地开发环境链接：'http://localhost:7130/pc/'
@@ -7,42 +6,13 @@ import { defineConfig } from '@playwright/test'
  *  外部测试官网链接：'https://opentiny.github.io/tiny-vue-web-doc'
  */
 const origin = 'http://localhost:3101/tiny-vue'
+
 const baseURL = `${origin}/zh-CN/os-theme/components/`
 const devServerCommon = 'pnpm run -w site'
 
-const baseConfig = Config({
+export default Config({
   testDir: '../sites/demos/pc',
   baseURL,
-  devServerCommon
-})
-
-// 合并，确保关键字段不丢失
-export default defineConfig({
-  // 保留 baseConfig
-  ...baseConfig,
-
-  // 显式覆盖 projects（完全替换）
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        browserName: 'chromium',
-        viewport: { width: 1920, height: 1080 },
-        launchOptions: {
-          args: ['--window-size=1920,1080']
-        }
-      }
-    }
-  ],
-
-  // 合并 use
-  use: {
-    ...baseConfig.use, // 保留 baseConfig 的 use 配置
-    viewport: { width: 1920, height: 1080 },
-    actionTimeout: 10000
-  },
-
-  // 合并 storageState
   storageState: {
     origins: [
       {
@@ -54,5 +24,6 @@ export default defineConfig({
         ]
       }
     ]
-  }
+  },
+  devServerCommon
 })
