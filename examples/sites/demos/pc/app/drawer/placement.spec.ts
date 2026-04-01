abe3bd52a372a33106bdaeccd804b35cc8614d26
James-9696 <hujm969696@163.com>
2026-03-31 19:19:47 -0700

    fix: modify e2e-test

--- examples/sites/demos/pc/app/drawer/placement.spec.ts
+++ examples/sites/demos/pc/app/drawer/placement.spec.ts
@@ -8,17 +8,17 @@ test('抽屉方向', async ({ page }) => {
 
   await demo.getByRole('button', { name: 'left' }).click()
   await expect(drawer).toHaveCSS('left', '0px')
-  await page.locator('.tiny-drawer__headerbtn').click()
+  await page.locator('.tiny-drawer__mask').click()
 
   await demo.getByRole('button', { name: 'right' }).click()
   await expect(drawer).toHaveCSS('right', '0px')
-  await page.locator('.tiny-drawer__headerbtn').click()
+  await page.locator('.tiny-drawer__mask').click()
 
   await demo.getByRole('button', { name: 'top' }).click()
   await expect(drawer).toHaveCSS('top', '0px')
-  await page.locator('.tiny-drawer__headerbtn').click()
+  await page.locator('.tiny-drawer__mask').click()
 
   await demo.getByRole('button', { name: 'bottom' }).click()
   await expect(drawer).toHaveCSS('bottom', '0px')
-  await page.locator('.tiny-drawer__headerbtn').click()
+  await page.locator('.tiny-drawer__mask').click()
 })
