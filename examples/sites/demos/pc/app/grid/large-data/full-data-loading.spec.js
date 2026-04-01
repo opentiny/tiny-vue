75e88ea33f24e1bb0429eceb41972869234bcaaf
cenguanming <975402925@qq.com>
2026-04-02 00:23:22 +0800

    test(grid): optimize scrolling method in full-data-loading and virtual-rolling specs

--- examples/sites/demos/pc/app/grid/large-data/full-data-loading.spec.js
+++ examples/sites/demos/pc/app/grid/large-data/full-data-loading.spec.js
@@ -8,16 +8,11 @@ test('全量加载', async ({ page }) => {
     height: 2500
   })
   await page.waitForTimeout(1000)
-  await page.locator('.tiny-grid__body-wrapper').hover()
-  await page.locator('.tiny-grid__body-wrapper').click()
-  // 先滚动 1000px
-  await page.mouse.wheel(0, 1000)
+  const scrollContainer = page.locator('.tiny-grid__body-wrapper')
+  await scrollContainer.evaluate((el) => (el.scrollTop = 1000))
   await expect(page.getByRole('cell', { name: '28' })).toBeVisible()
   await page.waitForTimeout(500)
-  await page.locator('.tiny-grid__body-wrapper').hover()
-  await page.locator('.tiny-grid__body-wrapper').click()
-  // 先滚动 4000px
-  await page.mouse.wheel(0, 5000)
+  await scrollContainer.evaluate((el) => (el.scrollTop = 6000))
   await page.waitForTimeout(200)
   await expect(page.getByRole('cell', { name: '129' })).toBeVisible()
 })
