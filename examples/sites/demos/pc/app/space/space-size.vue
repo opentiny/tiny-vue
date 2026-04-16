cae4ed1f3a9f329df007a1f11e090389e73497e8
James-9696 <hujm969696@163.com>
2026-04-15 18:03:22 -0700

    fix: modify the space components

--- examples/sites/demos/pc/app/space/space-size.vue
+++ examples/sites/demos/pc/app/space/space-size.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <!-- 选择行或列的按钮 -->
-    <div>
+    <div style="margin-bottom: 20px">
       <strong>当前 间距: [{{ rowValue }}, {{ columnValue }}]</strong>
     </div>
     <div>
-      <tiny-button type="primary">行</tiny-button>
+      <tiny-button type="primary">列</tiny-button>
       <tiny-slider v-model="rowValue" :min="0" :max="50" :step="2" style="width: 300px; margin-bottom: 20px" />
     </div>
     <div>
-      <tiny-button type="success">列</tiny-button>
+      <tiny-button type="success">行</tiny-button>
       <tiny-slider v-model="columnValue" :min="0" :max="50" :step="2" style="width: 300px; margin-bottom: 20px" />
     </div>
 
     <!-- 使用 dynamic direction 值 -->
     <tiny-space class="tiny-space" :size="[rowValue, columnValue]" :wrap="true">
-      <tiny-button v-for="n in 15" :key="n">按钮 {{ n }}</tiny-button>
+      <tiny-button v-for="n in 15" :key="n" style="width: 100px">按钮 {{ n }}</tiny-button>
     </tiny-space>
   </div>
 </template>
