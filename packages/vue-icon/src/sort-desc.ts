441d72b39c5789d63ce7b24a3e0b5fe90b3ec1fb
zzcr <894103554@qq.com>
2026-07-21 19:36:00 +0800

    fix(sort-desc): update import paths for SVG components and refactor export to use arrow function

--- packages/vue-icon/src/sort-desc.ts
+++ packages/vue-icon/src/sort-desc.ts
@@ -1,5 +1,5 @@
 import { svg } from '@opentiny/vue-common'
-import SortDesc from '@opentiny/theme/svgs/sort-desc.svg'
-import SortDescFilled from '@opentiny/theme/svgs/sort-desc-filled.svg'
+import SortDesc from '@opentiny/vue-theme/svgs/sort-desc.svg'
+import SortDescFilled from '@opentiny/vue-theme/svgs/sort-desc-filled.svg'
 
-export default svg({ name: 'IconSortDesc', component: SortDesc, filledComponent: SortDescFilled })
+export default () => svg({ name: 'IconSortDesc', component: SortDesc, filledComponent: SortDescFilled })()
