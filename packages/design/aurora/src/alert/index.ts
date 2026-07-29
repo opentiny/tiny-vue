0a214e4cf742ba1ebe66ce23c1a4628cbb006aa0
shenjunjian <40288193@qq.com>
2026-07-29 15:08:00 +0800

    fix: remove wrong code from alert

--- packages/design/aurora/src/alert/index.ts
+++ packages/design/aurora/src/alert/index.ts
@@ -3,13 +3,5 @@ import { iconWarning } from '@opentiny/vue-icon'
 export default {
   icons: {
     warning: iconWarning()
-  },
-  renderless: (props, hooks, { emit }, api) => {
-    const state = api.state
-
-    return {
-      // AUI的场景不允许点击header容器点击折叠， 只能点击title才折叠。所以屏蔽header容器点击
-      handleHeaderContainerClick: () => {}
-    }
   }
 }
