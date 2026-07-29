ab050bf5495841b027bb4bfb111869b52ddc21b4
shenjunjian <40288193@qq.com>
2026-07-29 15:04:00 +0800

    fix: 重新更新collapse-item的点击事件

--- packages/design/aurora/src/collapse-item/index.ts
+++ packages/design/aurora/src/collapse-item/index.ts
@@ -1,7 +1,15 @@
-import { iconArrowBottom } from '@opentiny/vue-icon'
+import { iconWarning } from '@opentiny/vue-icon'
 
 export default {
   icons: {
-    arrowIcon: iconArrowBottom()
+    warning: iconWarning()
+  },
+  renderless: (props, hooks, { emit }, api) => {
+    const state = api.state
+
+    return {
+      // AUI的场景不允许点击header容器点击折叠， 只能点击title才折叠。所以屏蔽header容器点击
+      handleHeaderContainerClick: () => {}
+    }
   }
 }
