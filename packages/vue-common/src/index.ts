67139c3e371fd24cc5b88ac03c37e2fa66ebe097
shenjunjian <40288193@qq.com>
2026-08-04 19:18:09 +0800

    fix(common): 为子组件注入design添加兜底变量

--- packages/vue-common/src/index.ts
+++ packages/vue-common/src/index.ts
@@ -169,7 +169,8 @@ export const customDesignConfig: CustomDesignConfig = {
 
 const getDesignConfig = () => {
   // 获取组件级配置和全局配置（inject需要带有默认值，否则控制台会报警告）
-  let globalDesignConfig: DesignConfig = customDesignConfig.designConfig || hooks.inject(design.configKey, {})
+  let globalDesignConfig: DesignConfig =
+    customDesignConfig.designConfig || hooks.inject(design.configKey, null) || design.configInstance
 
   // globalDesignConfig 可能是响应式对象，比如 computed
   globalDesignConfig = globalDesignConfig?.value || globalDesignConfig || {}
