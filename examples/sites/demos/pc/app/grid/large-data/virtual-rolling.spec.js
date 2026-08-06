37e79d652cc73caea243182ab4df6407fb523170
zzcr <894103554@qq.com>
2026-08-06 11:28:40 +0800

    fix: 修复检视意见

--- examples/sites/demos/pc/app/grid/large-data/virtual-rolling.spec.js
+++ examples/sites/demos/pc/app/grid/large-data/virtual-rolling.spec.js
@@ -3,20 +3,22 @@ import { test, expect } from '@playwright/test'
 test('虚拟滚动', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('grid-large-data#large-data-virtual-rolling')
+  const demo = page.locator('#large-data-virtual-rolling')
   await page.setViewportSize({
     width: 1400,
     height: 2500
   })
   await page.waitForTimeout(1000)
-  await page.locator('.tiny-grid__body-wrapper').hover()
-  await page.locator('.tiny-grid__body-wrapper').click()
+  const bodyWrapper = demo.locator('.tiny-grid__body-wrapper')
+  await bodyWrapper.hover()
+  await bodyWrapper.click()
   // 先滚动 1000px
   await page.mouse.wheel(0, 1000)
-  await expect(page.getByRole('cell', { name: '24' })).toBeVisible()
+  await expect(demo.getByRole('cell', { name: '24' })).toBeVisible()
   await page.waitForTimeout(500)
-  await page.locator('.tiny-grid__body-wrapper').hover()
-  await page.locator('.tiny-grid__body-wrapper').click()
+  await bodyWrapper.hover()
+  await bodyWrapper.click()
   // 先滚动 4000px
   await page.mouse.wheel(0, 5000)
-  await expect(page.getByRole('cell', { name: '124' })).toBeVisible()
+  await expect(demo.getByRole('cell', { name: '124' })).toBeVisible()
 })
